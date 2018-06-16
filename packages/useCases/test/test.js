e3ec163aa00ba6f92b252837942e3b87d33a610f
Curran <curran.kelleher@gmail.com>
2018-06-16 22:09:38 +0530

    Got mocha to work with esm

--- packages/useCases/test/test.js
+++ packages/useCases/test/test.js
@@ -1,5 +1,5 @@
+import assert from 'assert';
 
-var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
