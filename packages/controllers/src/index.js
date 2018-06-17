09a8653ac44c71dca2958bcce53cd30627a800e4
Curran <curran.kelleher@gmail.com>
2018-06-17 19:26:20 +0530

    Rename controlers to controller

--- packages/controllers/src/index.js
+++
@@ -1,9 +0,0 @@
-import jsonDiff from 'json0-ot-diff';
-import diffMatchPatch from 'diff-match-patch';
-
-export const Controllers = gateway => ({
-  changeDocument: (oldDocument, newDocument) => {
-    const ops = jsonDiff(oldDocument, newDocument, diffMatchPatch);
-    gateway.changeDocument(ops);
-  }
-});
