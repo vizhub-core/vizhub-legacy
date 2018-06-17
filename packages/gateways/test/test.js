1e112e21b8b6d9e6201082da5820551fa0edc885
Curran <curran.kelleher@gmail.com>
2018-06-17 14:15:28 +0530

    Add should invoke db if success test

--- packages/gateways/test/test.js
+++ packages/gateways/test/test.js
@@ -4,11 +4,24 @@ import { VisualizationGateway } from '../src';
 
 describe('Visualization Gateway', () => {
   describe('createVisualization', () => {
+
     it('should resolve to an error if no owner specified.', done => {
       VisualizationGateway().createVisualization({}).catch(error => {
         assert.equal(error.message, i18n('errorNoOwner'))
         done();
       });
     });
+
+    it('should invoke db if success.', done => {
+      const db = {
+        createVisualization: data => Promise.resolve({ id: '123' })
+      };
+      VisualizationGateway(db).createVisualization({ owner: 'bob' })
+        .then(({id}) => {
+          assert.equal(id, '123');
+          done();
+        });
+    });
+
   });
 });
