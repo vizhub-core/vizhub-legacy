0497029a9fa6df008b1e631709c0e00f2101d4ad
Curran <curran.kelleher@gmail.com>
2018-06-26 15:29:44 +0530

    Fix CSS + SASS bundling issue

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,10 +1,17 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config')
 const { ANALYZE } = process.env
 
 module.exports = withSass(withCSS({
   webpack: (config, { dev }) => {
+
+    // Enable use of both .sass and .css.
+    // Should be a temporary workaround.
+    // See https://github.com/zeit/next-plugins/issues/127
+    config = commonsChunkConfig(config, /\.(sass|scss|css)$/);
+
     if (ANALYZE) {
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'server',
@@ -12,6 +19,7 @@ module.exports = withSass(withCSS({
         openAnalyzer: true
       }))
     }
+
     return config
   }
 }));
