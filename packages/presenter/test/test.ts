37ffd7df7b81d833787a3dcc4fe00faf7067b30f
Curran <curran.kelleher@gmail.com>
2018-07-01 14:10:49 +0530

    Clean tests

--- packages/presenter/test/test.ts
+++ packages/presenter/test/test.ts
@@ -1,19 +1,24 @@
 import * as assert from 'assert';
-import { presentVisualization } from '../src';
+import { computeReferences } from '../src/computeReferences';
+//import { presentVisualization } from '../src';
 
 describe('Presenter', () => {
-  describe('presentVisualization', () => {
-
+  describe('computeReferences', () => {
     it('should compute references', async () => {
-      //const gateway = {
-      //  fetchDocument: id => Promise.resolve({}),
-      //  fetchUser: id => Promise.resolve({})
-      //}; 
-      //const presenter = Presenter(gateway);
-
-      const { references } = await presentVisualization()
-
+      const references = await computeReferences()
       assert.deepEqual(references, [ 'd3-selection', './bar' ]);
     });
   });
+
+  //describe('presentVisualization', () => {
+  //  it('should compute references', async () => {
+  //    //const gateway = {
+  //    //  fetchDocument: id => Promise.resolve({}),
+  //    //  fetchUser: id => Promise.resolve({})
+  //    //}; 
+  //    //const presenter = Presenter(gateway);
+  //    const { references } = await presentVisualization()
+  //    assert.deepEqual(references, [ 'd3-selection', './bar' ]);
+  //  });
+  //});
 });
