06956b5a7db164857a19ea4d0f860a1a66c27ba3
Curran <curran.kelleher@gmail.com>
2018-07-06 11:44:38 +0530

    Test Web

--- packages/web/test/test.js
+++ packages/web/test/test.js
@@ -3,18 +3,33 @@ import puppeteer from 'puppeteer';
 
 // Testing technique inspired by https://medium.com/@dpark/ui-testing-with-puppeteer-and-mocha-8a5c6feb3407
 
+// Use this for magic.
+// const puppeteerOptions = { slowMo: 500, headless: false };
+
+// Use this for speed.
+const puppeteerOptions = {};
+
+const retry = (fn, ms) => new Promise(resolve => {
+  fn()
+    .then(resolve)
+    .catch(() => {
+      setTimeout(() => {
+        console.log('retrying...');
+        retry(fn, ms).then(resolve);
+      }, ms);
+    })
+});
+
+
 describe('Web', () => {
   let browser;
   let page;
 
   describe('set up', () => {
     it('should open page', async () => {
-      browser = await puppeteer.launch({ headless: false });
+      browser = await puppeteer.launch(puppeteerOptions);
       page = await browser.newPage();
-      const response = await page.goto('http://localhost:3000', {
-        timeout: 0,
-        waitUntil: 'domcontentloaded'
-      });
+      const response = await retry(() => page.goto('http://localhost:3000'), 1000);
       assert.equal(response.status(), 200);
     });
   });
@@ -32,7 +47,6 @@ describe('Web', () => {
 
   describe('tear down', () => {
     it('should close', async () => {
-      await new Promise(resolve => setTimeout(resolve, 5000))
       await browser.close();
     });
   });
