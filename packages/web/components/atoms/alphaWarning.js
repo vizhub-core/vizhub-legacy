92d81c84b879a3df77e300c1f93d0235de810d1a
Curran <curran.kelleher@gmail.com>
2018-07-18 12:48:27 +0530

    Add notice re: alpha software. Closes #107

---
+++ packages/web/components/atoms/alphaWarning.js
@@ -0,0 +1,10 @@
+export const AlphaWarning = () => (
+  <nav className='is-inverted' style={{
+    padding: '10px',
+    backgroundColor: '#c00000',
+    color: 'white',
+    textAlign: 'center'
+  }}>
+    <strong style={{ color: 'white' }}>Warning!</strong> This site is alpha software. Your data will not be permanently saved.
+  </nav>
+)
