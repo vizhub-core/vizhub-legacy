daae37b18e69028c89e63553b8d5daee348a656f
curran <curran.kelleher@gmail.com>
2018-07-19 12:00:05 +0530

    Tweak URL for edit page. Closes #111.

--- packages/web/routes/index.js
+++ packages/web/routes/index.js
@@ -4,8 +4,8 @@ export const routes = NextRoutes()
   .add('auth/callback', '/auth/callback')
   .add('auth/error', '/auth/error')
   .add('auth', '/auth')
-  .add('edit-visualization', '/edit/:id')
+  .add('edit-visualization', '/:userName/:id/edit')
   .add('view-visualization', '/:userName/:id');
 
-export const edit = ({id}) => `/edit/${id}`;
+export const edit = ({userName, id}) => `/${userName}/${id}/edit`;
 export const viewVisualization = ({userName, id}) => `/${userName}/${id}`;
