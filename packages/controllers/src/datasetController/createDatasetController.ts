99576af4a3c7e5d69bb4f50d809403fd66723979
Curran <curran.kelleher@gmail.com>
2018-07-31 19:24:41 +0530

    Finalize controller for create dataset

--- packages/controllers/src/datasetController/createDatasetController.ts
+++ packages/controllers/src/datasetController/createDatasetController.ts
@@ -4,20 +4,15 @@ import { userIdFromReq } from '../userIdFromReq';
 export const createDatasetController = (expressApp, datasetGateway) => {
   const createDataset = new CreateDataset({ datasetGateway });
 
-  expressApp.get('/api/dataset/create', async (req, res) => {
-    console.log(req.body.dataset);
-    const owner = userIdFromReq(req);
-    const { title, file } = req.body;
-    //try {
-    //  const requestModel = {
-    //    owner,
-    //    title,
-    //    file
-    //  };
-    //  const responseModel = await createDataset.execute(requestModel);
-    //  res.json(responseModel);
-    //} catch (error) {
-    //  res.json({ error: error.message });
-    //}
+  expressApp.post('/api/dataset/create', async (req, res) => {
+    try {
+      const owner = userIdFromReq(req);
+      const { title, file } = req.body;
+      const requestModel = { owner, title, file };
+      const responseModel = await createDataset.execute(requestModel);
+      res.json(responseModel);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
   });
 }
