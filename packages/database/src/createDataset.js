10fa0d5ee53be4cfd041979623bcceb76b1a31c7
Curran <curran.kelleher@gmail.com>
2018-07-31 19:30:29 +0530

    Add database implementation for create dataset

---
+++ packages/database/src/createDataset.js
@@ -0,0 +1,15 @@
+import { DOCUMENT_INFO, DOCUMENT_CONTENT } from './collectionName';
+
+export const createDataset = connection => dataset => {
+  const id = dataset.id;
+  return new Promise(resolve => {
+
+    // TODO handle errors here.
+    connection.get(DOCUMENT_INFO, id).create(dataset.info);
+    connection.get(DOCUMENT_CONTENT, id).create(dataset.content);
+
+    // TODO only resolve after document created,
+    // to avoid race conditions.
+    resolve({ id });
+  });
+}
--- packages/database/src/index.js
+++ packages/database/src/index.js
@@ -1,9 +1,11 @@
 import { createVisualization } from './createVisualization';
 import { getVisualization } from './getVisualization';
 import { saveVisualization } from './saveVisualization';
+import { createDataset } from './createDataset';
 
 export const Database = connection => ({
   createVisualization: createVisualization(connection),
   getVisualization: getVisualization(connection),
-  saveVisualization: saveVisualization(connection)
+  saveVisualization: saveVisualization(connection),
+  createDataset: createDataset(connection),
 });
