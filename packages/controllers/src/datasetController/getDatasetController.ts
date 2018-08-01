7b640df04fef6141dea32f093498a3ad671990ba
Curran <curran.kelleher@gmail.com>
2018-08-01 19:18:47 +0530

    Got dataset download link to work. Closes #21

--- packages/controllers/src/datasetController/getDatasetController.ts
+++ packages/controllers/src/datasetController/getDatasetController.ts
@@ -16,4 +16,20 @@ export const getDatasetController = (expressApp, datasetGateway) => {
       });
     }
   });
+
+  expressApp.get('/:userName/datasets/:slug.:format', async (req, res) => {
+    try {
+      const requestModel = { slug: req.params.slug };
+      const responseModel = await getDataset.execute(requestModel);
+      res.setHeader('Content-Type', 'text/csv');
+      res.send(responseModel.dataset.content.text);
+    } catch (error) {
+      // TODO unify error handling across the codebase.
+      res.json({
+        error: {
+          message: error.message
+        }
+      });
+    }
+  });
 }
