48b5809a4be131a717923f8f352e0a74bab8bd5c
Curran <curran.kelleher@gmail.com>
2018-08-20 22:21:04 +0530

    Fix scrolling issues. Fix feedback overlapping with code issue.

--- packages/web/components/atoms/commentLink.js
+++ packages/web/components/atoms/commentLink.js
@@ -1,5 +1,5 @@
 export const CommentLink = () => (
   <div className='comment-link'>
-    Frustrated? <a target="_blank" href='https://github.com/datavis-tech/vizhub-ui/issues/52'>Comment here!</a>
+    <a target="_blank" href='https://github.com/datavis-tech/vizhub-ui/issues/52'>Feedback</a>
   </div>
 );
