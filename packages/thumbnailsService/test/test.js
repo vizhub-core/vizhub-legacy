8904c9f2f47ca4373d2ac8418d1f22a131244b0b
Curran <curran.kelleher@gmail.com>
2018-09-29 19:06:12 +0530

    Add more tests for aspect ratios of thumbnails

--- packages/thumbnailsService/test/test.js
+++ packages/thumbnailsService/test/test.js
@@ -9,15 +9,33 @@ const expectedThumbnail = 'fdsa';
 const expectedPreview = 'fdsa';
 
 describe('Thumbnails Service', () => {
-  it('should compute correct thumbnail dimensions', async () => {
+  it('should compute correct thumbnail dimensions for same aspect ratio', async () => {
     const { width, height } = computeImageDimensions({
       actual: { width: 960, height : 500 },
       desired: { width: 230, height: 120 }
     });
-    console.log({width, height});
     assert.equal(width, 230);
     assert.equal(height, 120);
   });
+
+  it('should compute correct thumbnail dimensions for taller visualization', async () => {
+    const { width, height } = computeImageDimensions({
+      actual: { width: 960, height : 800 },
+      desired: { width: 230, height: 120 }
+    });
+    assert.equal(width, 230);
+    assert.equal(height, 276);
+  });
+
+  it('should compute correct thumbnail dimensions for shorter visualization', async () => {
+    const { width, height } = computeImageDimensions({
+      actual: { width: 960, height : 200 },
+      desired: { width: 230, height: 120 }
+    });
+    assert.equal(width, 576);
+    assert.equal(height, 120);
+  });
+
   it('should make a thumbnail for a visualization', async () => {
     const images = await generateImages(visualization);
     console.log(images.thumbnail);
