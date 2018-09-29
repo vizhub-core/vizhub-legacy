44ded0cc356ddd2ada06fe225b79e965eab71487
Curran <curran.kelleher@gmail.com>
2018-09-29 14:52:36 +0530

    Scaffold thumbnails service

--- packages/thumbnailsService/src/index.js
+++ packages/thumbnailsService/src/index.js
@@ -1,7 +1,1 @@
-//import { thumbnailsService } from './thumbnailsService';
-//thumbnailsService(gateways);
-
-export const thumbnailsService = gateways => {
-  console.log('Start thumbnail service');
-  console.log('Start thumbnail service');
-};
+console.log('Start thumbnail service');
