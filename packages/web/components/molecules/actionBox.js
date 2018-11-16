c6596dcbc2dcfb5dace6c534f89610fc5dc05e44
curran <curran.kelleher@gmail.com>
2018-11-16 13:45:17 +0530

    Add templates

--- packages/web/components/molecules/actionBox.js
+++ packages/web/components/molecules/actionBox.js
@@ -1,7 +1,7 @@
 export const ActionBox = ({title, children}) => (
   <div
     className='container section'
-    style={{maxWidth: '600px'}}
+    style={{maxWidth: '1500px'}}
   >
     <div className='box has-text-centered'>
       <h1 className='title'>{title}</h1>
--- packages/web/pages/create-visualization/body.js
+++ packages/web/pages/create-visualization/body.js
@@ -19,9 +19,6 @@ export const BodyAuthenticated = ({onFromScratchClick, templates}) => {
           Create a visualization by forking one of these starter templates.
         </div>
         <div style={{ marginTop: '1.25rem' }} >
-          <a className='button' href='https://vizhub.com/curran/86a75dc8bdbe4965ba353a79d4bd44c8'>
-            Hello World
-          </a>
           {
             templates.map(info =>
               <VisualizationPreview key={info.id} info={info} userName='curran'/>
