bdaba97ce3f23c85870d77ada88820c75e6311c9
curran <curran.kelleher@gmail.com>
2018-11-18 01:34:56 +0530

    Add missing file

---
+++ packages/controllers/src/apiController/visualizationAPIController/getPreviewController.js
@@ -0,0 +1,24 @@
+import { GetPreview } from 'datavis-tech-use-cases';
+
+export const getPreviewController = (expressApp, gateways) => {
+  const getPreview = new GetPreview(gateways);
+  expressApp.get('/api/visualization/preview/:id.png', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const requestModel = { id };
+      const responseModel = await getPreview.execute(requestModel);
+      const preview = responseModel.preview;
+
+      const img = new Buffer(preview, 'base64');
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': img.length
+      });
+
+      res.end(img); 
+    } catch (error) {
+      res.json({ error })
+    }
+  });
+}
--- packages/web/components/atoms/unfurl.js
+++ packages/web/components/atoms/unfurl.js
@@ -1,10 +1,12 @@
 export const Unfurl = ({ title, description, image, url }) => (
   <React.Fragment>
+    <meta name='twitter:url' value={url} />
     <meta name='twitter:card' content='summary_large_image' />
     <meta name='twitter:site' value='@datavis_tech' />
     <meta name='twitter:title' value={title} />
     <meta name='twitter:description' value={description} />
     <meta name='twitter:image' content={image} />
+    <meta name='twitter:domain' value='vizhub.com' />
 
     <meta property='og:url' content={url}/>
     <meta property='og:title' content={title} />
