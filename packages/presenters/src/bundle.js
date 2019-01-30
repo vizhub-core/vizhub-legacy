495b4e42950560bae4669abf3922a6a651cba6c1
curran <curran.kelleher@gmail.com>
2019-01-30 13:43:42 +0530

    Fix indentation

--- packages/presenters/src/bundle.js
+++ packages/presenters/src/bundle.js
@@ -44,20 +44,20 @@ export const bundle = async (files) => {
     external: d3Packages.concat('react')
   };
 
-const rollupBundle = await rollup(inputOptions);
-const { code, map } = await rollupBundle.generate(outputOptions);
+  const rollupBundle = await rollup(inputOptions);
+  const { code, map } = await rollupBundle.generate(outputOptions);
 
-// Monkey patch magic-string internals
-// to support characters outside of the Latin1 range, e.g. Cyrillic.
-//
-// Related reading:
-//  - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa#Unicode_strings
-//  - https://github.com/Rich-Harris/magic-string/blob/3466b0230dddc95eb378ed3e0d199e36fbd1f572/src/SourceMap.js#L3
-const toString = map.toString.bind(map);
-map.toString = () => unescape(encodeURIComponent(toString()));
+  // Monkey patch magic-string internals
+  // to support characters outside of the Latin1 range, e.g. Cyrillic.
+  //
+  // Related reading:
+  //  - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa#Unicode_strings
+  //  - https://github.com/Rich-Harris/magic-string/blob/3466b0230dddc95eb378ed3e0d199e36fbd1f572/src/SourceMap.js#L3
+  const toString = map.toString.bind(map);
+  map.toString = () => unescape(encodeURIComponent(toString()));
 
-// Inspired by https://github.com/rollup/rollup/issues/121
-const codeWithSourceMap = code + '\n//# sourceMappingURL=' + map.toUrl();
+  // Inspired by https://github.com/rollup/rollup/issues/121
+  const codeWithSourceMap = code + '\n//# sourceMappingURL=' + map.toUrl();
 
   return [{
     name: 'bundle.js',
