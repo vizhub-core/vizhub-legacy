01c3db73d0ae2d502288b9379d2391b03f7a849c
curran <curran.kelleher@gmail.com>
2019-02-13 22:55:17 +0530

    Iterate deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-PROJECT_DIR=$HOME/datavis-tech-2
-
-cd $PROJECT_DIR
 git checkout master
 git pull
-
-cp .gitmodules-ci .gitmodules # Use HTTPS in deployment (so no SSH setup required)
-git submodule sync
-git submodule update --init
 
 lerna bootstrap
 cd packages/web
