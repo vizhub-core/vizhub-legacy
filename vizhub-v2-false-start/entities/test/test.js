ea20a713c3424ce1f87e1f8d57b5e75aa69abe27
curran <curran.kelleher@gmail.com>
2019-03-09 19:39:11 +0530

    More sketches for core data structures

--- vizhub-v2-false-start/entities/test/test.js
+++ vizhub-v2-false-start/entities/test/test.js
@@ -4,7 +4,7 @@ const vizData = {
   views: 1234,
   upvotes: 2345,
   downvotes: 5,
-  workingState:{
+  workingState: {
     state: {
       title: "A Viz",
       showGrid: true,
@@ -15,7 +15,7 @@ const vizData = {
 
       // Keys are random, so that file renames are trivial via JSON OT.
       '9043411906112002': {
-        name: 'index.html',
+        path: 'index.html',
         
         // Parcel-style script tag declaration directly into ES6 module (magic).
         text: '<html><body>Hello World<script src="index.js"></body></html>',
@@ -25,13 +25,23 @@ const vizData = {
         hash: '12fj1d'
       },
 
+      '15886149663202853': {
+
+        // Directories are defined implicitly as part of file paths.
+        // Similar to https://github.com/Permutatrix/rollup-plugin-hypothetical#usage
+        path: 'styles/theme.css',
+        text: 'body { margin: 0; }',
+        hash: '45hd69'
+      }
     }
   },
+
+  // References a Commit by id.
+  head: '0c82a54f22f775a3ed8b97b2dea74036'
 
 describe('Entities', () => {
   describe('Viz', () => {
     it('', () => {
-      assert.deepEqual(new VisualizationViewModel(visualization), {
-      });
+      assert.equal('a', 'a');
     });
   });
