be15ddeca11275dd939ac2a1afd120b0be084e6b
curran <curran.kelleher@gmail.com>
2019-03-14 03:20:43 +0530

    Finesse CodeMirror

--- vizhub-v2-false-start/frontend/src/Studio/Editor/CodeMirror/index.js
+++ vizhub-v2-false-start/frontend/src/Studio/Editor/CodeMirror/index.js
@@ -1,102 +1,25 @@
-import React, {useRef, useEffect} from 'react';
-import {
-  EditorState,
-  EditorView,
-  keymap,
-  history,
-  redo,
-  redoSelection,
-  undo,
-  undoSelection,
-  lineNumbers,
-  baseKeymap,
-  indentSelection,
-  legacyMode,
-  matchBrackets,
-  javascript,
-  specialChars,
-  multipleSelections,
-} from 'codemirror-6-prerelease';
-import styled, {createGlobalStyle} from 'styled-components';
-import { mono } from '../../../styles.js';
-const CodeMirrorTheme = createGlobalStyle`
-  .codemirror {
-    font-family: '${mono.family}';
-    line-height: 1.1;
-    font-size: 24pt;
-  }
-  .codemirror, .codemirror-gutter {
-    background-color: #300a24;
-  }
-  .codemirror ::selection {
-    background-color: #b6b6b6;
-  }
-  .codemirror-gutter {
-    border-right: 1px solid #533d51;
-  }
-  .codemirror-gutter-element {
-    color: #fce94f;
-  }
-  .codemirror-content {
-    caret-color: white;
-  }
+import React, { useRef, useEffect } from 'react';
+import { useCodeMirror } from './useCodeMirror';
+import { Wrapper, CodeMirrorGlobalStyle } from './styles';
 
-  .cm-keyword,
-  .cm-comment,
-  .cm-bracket,
-  .cm-attribute,
-  .codemirror-matchingbracket {
-    color: #34e2e2; /* neon blue */
-  }
-
-  .cm-atom,
-  .cm-string,
-  .cm-string-2,
-  .cm-qualifier {
-    color: #ad7fa8; /* purple */
-  }
-
-  .cm-property {
-    color: #87ffaf; /* pale green */
-  }
-`;
-
-export const CodeMirror = ({initialDoc}) => {
+export const CodeMirror = ({ initialDoc }) => {
   const ref = useRef();
+  const view = useCodeMirror(initialDoc);
 
   useEffect(() => {
-    const mode = legacyMode({mode: javascript({indentUnit: 2}, {})});
+    if (view) {
+      ref.current.appendChild(view.dom);
+    }
+  }, [view]);
 
-    const isMac = /Mac/.test(navigator.platform);
-    const state = EditorState.create({
-      doc: initialDoc,
-      extensions: [
-        lineNumbers(),
-        history(),
-        specialChars(),
-        multipleSelections(),
-        mode,
-        matchBrackets(),
-        keymap({
-          'Mod-z': undo,
-          'Mod-Shift-z': redo,
-          'Mod-u': view => undoSelection(view) || true,
-          [isMac ? 'Mod-Shift-u' : 'Alt-u']: redoSelection,
-          'Ctrl-y': isMac ? undefined : redo,
-          'Shift-Tab': indentSelection,
-        }),
-        keymap(baseKeymap),
-      ],
-    });
-
-    const view = (window.view = new EditorView({state}));
-    ref.current.appendChild(view.dom);
-  }, []);
+  const focusCodeMirror = () => {
+    view.focus();
+  };
 
   return (
     <>
-      <CodeMirrorTheme />
-      <div ref={ref} />
+      <CodeMirrorGlobalStyle />
+      <Wrapper onClick={focusCodeMirror} ref={ref} />
     </>
   );
 };
---
+++ vizhub-v2-false-start/frontend/src/Studio/Editor/CodeMirror/useCodeMirror.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import {
+  EditorState,
+  EditorView,
+  keymap,
+  history,
+  redo,
+  redoSelection,
+  undo,
+  undoSelection,
+  lineNumbers,
+  baseKeymap,
+  indentSelection,
+  legacyMode,
+  matchBrackets,
+  javascript,
+  specialChars,
+  multipleSelections
+} from 'codemirror-6-prerelease';
+
+export const useCodeMirror = initialDoc => {
+  const [view, setView] = useState();
+  useEffect(() => {
+    const mode = legacyMode({ mode: javascript({ indentUnit: 2 }, {}) });
+    const isMac = /Mac/.test(navigator.platform);
+    const state = EditorState.create({
+      doc: initialDoc,
+      extensions: [
+        lineNumbers(),
+        history(),
+        specialChars(),
+        multipleSelections(),
+        mode,
+        matchBrackets(),
+        keymap({
+          'Mod-z': undo,
+          'Mod-Shift-z': redo,
+          'Mod-u': view => undoSelection(view) || true,
+          [isMac ? 'Mod-Shift-u' : 'Alt-u']: redoSelection,
+          'Ctrl-y': isMac ? undefined : redo,
+          'Shift-Tab': indentSelection
+        }),
+        keymap(baseKeymap)
+      ]
+    });
+    const view = new EditorView({ state });
+    setView(view);
+  }, []);
+
+  return view;
+};
