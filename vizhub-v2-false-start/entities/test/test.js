beaa11e4d9990027a157a1780454b4144d39de0e
curran <curran.kelleher@gmail.com>
2019-03-13 14:50:21 +0530

    Iterate entities

--- vizhub-v2-false-start/entities/test/test.js
+++ vizhub-v2-false-start/entities/test/test.js
@@ -1,44 +1,63 @@
 import assert from 'assert';
 
+const userPreferencesData = {
+  editorFont: 'Ubuntu Mono',
+  editorFontSize: '16pt',
+  editorFontLigatures: 'None', // or 'Arrows', 'All'
+  editorTheme: 'Ubuntu',
+};
+
 const vizData = {
   views: 1234,
   upvotes: 2345,
   downvotes: 5,
-  workingState: {
+
+  // Akin to the "working directory" using Git.
+  working: {
+
+    // The configurable state variables for the Viz.
     state: {
-      title: "A Viz",
+      title: 'A Viz',
       showGrid: true,
-      markColor: "#FFEEDD",
-      maxCircleRadius: 50
+      markColor: '#FFEEDD',
+      maxCircleRadius: 50,
     },
+
+    // The text files that define the Viz.
     files: {
 
       // Keys are random, so that file renames are trivial via JSON OT.
       '9043411906112002': {
-        path: 'index.html',
-        
+
+        // The name of the file.
+        name: 'index.html',
+
         // Parcel-style script tag declaration directly into ES6 module (magic).
         text: '<html><body>Hello World<script src="index.js"></body></html>',
 
         // Hash of text is stored to detect content changes in application code.
         // https://www.npmjs.com/package/@emotion/hash
-        hash: '12fj1d'
+        hash: '12fj1d',
       },
 
       '15886149663202853': {
-
         // Directories are defined implicitly as part of file paths.
         // Similar to https://github.com/Permutatrix/rollup-plugin-hypothetical#usage
-        path: 'styles/theme.css',
+        name: 'theme.css',
+
+        // The path of the file in the hypothetical file system.
+        // In this case, the file is located at `./styles/theme.css`.
+        path: 'styles',
         text: 'body { margin: 0; }',
-        hash: '45hd69'
-      }
-    }
+        hash: '45hd69',
+      },
+    },
   },
 
   // References a Commit by id.
   // Related https://www.npmjs.com/package/hat
-  head: '0c82a54f22f775a3ed8b97b2dea74036'
+  head: '0c82a54f22f775a3ed8b97b2dea74036',
+};
 
 describe('Entities', () => {
   describe('Viz', () => {
@@ -46,3 +65,4 @@ describe('Entities', () => {
       assert.equal('a', 'a');
     });
   });
+});
