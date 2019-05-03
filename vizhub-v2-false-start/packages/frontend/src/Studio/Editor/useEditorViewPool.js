a37ddddf089535ec3954be264969a3fa1abefce6
Curran <curran.kelleher@gmail.com>
2019-05-03 13:41:34 -0400

    Make some progress on view pool ideas

--- vizhub-v2-false-start/packages/frontend/src/Studio/Editor/useEditorViewPool.js
+++ vizhub-v2-false-start/packages/frontend/src/Studio/Editor/useEditorViewPool.js
@@ -4,26 +4,35 @@ import { useEffect, useState } from 'react';
 // When the user navigates between visualizations,
 // all these views (and their corresponding subscriptions)
 // should be cleaned up, making way for the views of the newly open visualization.
+class Pool {
+  constructor () {
 
-// Creates an editor view pool.
-//  - Keys are fileId values.
-//  - Values are view instances created in useEditorView.
-const createPool = () => ({});
+    // - Keys are fileId values.
+    // - Values are view instances created in useEditorView.
+    this._views = {};
+  }
+  getView (fileId) {
+    return this._views[fileId];
+  }
+  setView (fileId, view) {
+    return this._views[fileId] = view;
+  }
+  destroy () {
+    Object.values(this._views).forEach(view => {
+      view.destroy();
+    });
+    this._views = {};
+  }
+}
 
-// Cleans up an editor view pool.
-const cleanupPool = pool => {
-  // TODO test
-  Object.values(pool).forEach(view => {
-    view.destroy();
-  });
-};
+const createPool = () => new Pool();
 
 export const useEditorViewPool = vizId => {
   const [pool, setPool] = useState();
   useEffect(() => {
-    const newPool = createPool();
+    const newPool = new Pool();
     setPool(newPool);
-    return cleanupPool(newPool);
+    return () => newPool.destroy()
   }, [vizId]);
   return pool;
 };
--- vizhub-v2-false-start/packages/frontend/src/Studio/Editor/useEditorViewPool.test.js
+++ vizhub-v2-false-start/packages/frontend/src/Studio/Editor/useEditorViewPool.test.js
@@ -8,7 +8,9 @@ describe('useEditorViewPool', () => {
       const viewPool = useEditorViewPool(vizId);
       if (viewPool) {
         expect(viewPool).toBeTruthy();
-        expect(Object.keys(viewPool).length).toEqual(0);
+        expect(typeof viewPool.getView).toEqual('function');
+        expect(typeof viewPool.setView).toEqual('function');
+        expect(typeof viewPool.destroy).toEqual('function');
         done();
       }
       return null;
@@ -16,6 +18,40 @@ describe('useEditorViewPool', () => {
 
     const div = document.createElement('div');
     ReactDOM.render(<Component vizId="foo" />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should clean up existing pool when vizId changes', done => {
+    let destroyFirst = jest.fn();
+    const Component = ({ vizId }) => {
+      console.log(vizId);
+      const viewPool = useEditorViewPool(vizId);
+      console.log(viewPool);
+
+      if (viewPool) {
+        if (vizId === 'first') {
+          const fileId = 'a';
+          if (!viewPool.getView(fileId)) {
+            viewPool.setView(fileId, { destroy: destroyFirst });
+            expect(destroyFirst).toHaveBeenCalledTimes(0);
+          }
+          //const view = viewPool.getView(fileId);
+          //const view = viewPool.getOrCreateView(fileId);
+          //expect(Object.keys(viewPool).length).toEqual(0);
+          //viewPool[fileId] = view;
+          //expect(Object.keys(viewPool).length).toEqual(1);
+        }
+        if (vizId === 'second') {
+          //expect(destroyFirst).toHaveBeenCalledTimes(1);
+          done();
+        }
+      }
+      return null;
+    };
+
+    const div = document.createElement('div');
+    ReactDOM.render(<Component vizId="first" />, div);
+    ReactDOM.render(<Component vizId="second" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
