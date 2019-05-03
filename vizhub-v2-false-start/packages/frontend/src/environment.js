4c9e895dbcd0738c66c90424f7296504a4efeba5
Curran <curran.kelleher@gmail.com>
2019-05-03 08:29:31 -0400

    Add hint for environment configuration

--- vizhub-v2-false-start/packages/frontend/src/environment.js
+++ vizhub-v2-false-start/packages/frontend/src/environment.js
@@ -1,5 +1,7 @@
 export const avoidBackend = false;
 export const avoidShareDB = avoidBackend;
+// You can put the following in your .env file:
+// REACT_APP_WEBSOCKET_URL="ws://vizhub.myDomain.com"
 export const webSocketURL =
   process.env.REACT_APP_WEBSOCKET_URL || 'ws://localhost:4000';
 export const enablePresence = true; //false;
