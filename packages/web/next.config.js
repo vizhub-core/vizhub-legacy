84264fa054196496fa6910a08abf86a4c39ea206
curran <curran.kelleher@gmail.com>
2019-06-25 11:53:39 +0530

    WIP upgrade dependencies

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,7 +1,7 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
-const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
+//const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
 const { ANALYZE } = process.env;
 
 module.exports = withSass(withCSS({
@@ -11,7 +11,7 @@ module.exports = withSass(withCSS({
     // Enable use of both .sass and .css.
     // Should be a temporary workaround.
     // See https://github.com/zeit/next-plugins/issues/127
-    config = commonsChunkConfig(config, /\.(sass|css)$/);
+    //config = commonsChunkConfig(config, /\.(sass|css)$/);
 
     if (ANALYZE) {
       config.plugins.push(new BundleAnalyzerPlugin({
