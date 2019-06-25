ff99b27750ace27ac9a8d7de85ada6a30dc12160
curran <curran.kelleher@gmail.com>
2019-06-25 16:26:34 +0530

    Add banner

---
+++ packages/neoFrontend/src/Home/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  max-width: 1380px;
+  flex: 1;
+`;
+
+export const Banner = styled.div`
+  padding-top: 32px;
+  padding-bottom: 32px;
+  margin: 6px;
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const SignIn = styled.div`
+  color: #f0353c;
+`;
