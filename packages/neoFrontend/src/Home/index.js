cb57faa280f6d665d742c9745ed09d13c4c99e65
curran <curran.kelleher@gmail.com>
2019-06-25 16:47:04 +0530

    Add logo

--- packages/neoFrontend/src/Home/index.js
+++ packages/neoFrontend/src/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Wrapper, Content, Banner, SignIn } from './styles';
+import { Wrapper, Content, Banner, SignIn, Logo } from './styles';
+import { LogoSVG } from '../svg/LogoSVG';
 
 export const Home = () => (
   <Wrapper>
     <Content>
       <Banner>
-        <div>VizHub</div>
+        <Logo height={40}>
+          <LogoSVG fill='currentcolor'/>
+        </Logo>
         <SignIn>Sign up / Sign in</SignIn>
       </Banner>
     </Content>
