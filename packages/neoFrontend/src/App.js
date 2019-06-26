4ab8067c23b74d4a473176033f0db4491110035d
curran <curran.kelleher@gmail.com>
2019-06-26 12:37:03 +0530

    Iron out layout, adopt prettier, add click event for sign in.

--- packages/neoFrontend/src/App.js
+++ packages/neoFrontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home } from './Home';
+import { Home } from './pages';
 
 export const App = () => (
   <Router>
