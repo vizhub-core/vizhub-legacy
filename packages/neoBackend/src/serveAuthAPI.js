86d3866a46afd02804bbbed0ed3f6de55f21c8bd
curran <curran.kelleher@gmail.com>
2019-06-26 18:03:58 +0530

    Get GitHub access token, show success in client

--- packages/neoBackend/src/serveAuthAPI.js
+++ packages/neoBackend/src/serveAuthAPI.js
@@ -1,32 +1,32 @@
 import jwt from 'jsonwebtoken';
 import fetch from 'node-fetch';
-import cors from 'cors';
+// TODO uppercase.
 import { errorResponse } from './errorResponse';
 
+const oAuthAccessTokenURL = 'https://github.com/login/oauth/access_token';
+const client_id = process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_ID;
+const client_secret = process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_SECRET;
+
 export const serveAuthAPI = app => {
-  app.post('/api/auth/github', cors(), (req, res) => {
-    const body = JSON.stringify({
-      client_id: process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_ID,
-      client_secret: process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_SECRET,
-      code: req.body.code
-    });
+  app.post('/api/auth/github', (req, res) => {
+    const { code } = req.body;
 
-    console.log('here');
-    console.log(body);
-
-    fetch('https://github.com/login/oauth/access_token', {
+    // Assemble options for fetch request.
+    const fetchOptions = {
       method: 'POST',
-      mode: 'cors',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body
-    })
+      body: JSON.stringify({ client_id, client_secret, code })
+    };
+
+    // Get an access token from GitHub's API.
+    const accessToken = fetch(oAuthAccessTokenURL, fetchOptions)
       .then(response => response.json())
       .then(data => {
         if (data.error) {
-          return res.send(
+          return Promise.reject(
             errorResponse({
               error: data.error,
               errorDescription: data.error_description,
@@ -34,45 +34,52 @@ export const serveAuthAPI = app => {
             })
           );
         }
-        console.log(data);
-        // // store token in variaable
-        // const accessToken = data.access_token;
-
-        // // set token in header for fetching user
-        // // api for getting profile data
-        // return fetch('https://api.github.com/user', {
-        //   method: 'GET',
-        //   mode: 'cors',
-        //   headers: {
-        //     Accept: 'application/json',
-        //     'Content-Type': 'application/json',
-        //     Authorization: `token ${accessToken}`,
-        //   },
-        // })
-        //   .then(response => response.json())
-        //   .then(data => {
-        //     // storing user profile for creating access token
-        //     const user = {
-        //       user: data.login,
-        //       id: data.id,
-        //     };
-
-        //     // generating jwt token with expired in 24 hour and sending to front
-        //     jwt.sign(
-        //       user,
-        //       process.env.JWT_SECRET,
-        //       {expiresIn: 60 * 60 * 24},
-        //       (err, token) => {
-        //         res.json({
-        //           token,
-        //         });
-        //         console.log('l' + token);
-        //       },
-        //     );
-        //   });
-      })
-      .catch(error => {
-        console.log(error);
+        return data.access_token;
       });
+
+    accessToken
+      .catch(errorResponse => res.send(errorResponse))
+      .then(token => {
+        console.log(token);
+        res.send({ token });
+      });
+
+    //        // set token in header for fetching user
+    //        // api for getting profile data
+    //        return fetch('https://api.github.com/user', {
+    //          method: 'GET',
+    //          mode: 'cors',
+    //          headers: {
+    //            Accept: 'application/json',
+    //            'Content-Type': 'application/json',
+    //            Authorization: `token ${accessToken}`,
+    //          },
+    //        })
+    //          .then(response => response.json())
+    //          .then(data => {
+    //            console.log(data);
+    //            // storing user profile for creating access token
+    //            const user = {
+    //              user: data.login,
+    //              id: data.id,
+    //            };
+    //
+    //            // generating jwt token with expired in 24 hour and sending to front
+    //            jwt.sign(
+    //              user,
+    //              process.env.JWT_SECRET,
+    //              {expiresIn: 60 * 60 * 24},
+    //              (err, token) => {
+    //                res.json({
+    //                  token,
+    //                });
+    //                console.log('l' + token);
+    //              },
+    //            );
+    //          });
+    //      .catch(error => {
+    //        console.log(error);
+    //      });
+    //      })
   });
 };
