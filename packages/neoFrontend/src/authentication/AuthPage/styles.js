152caeca43b65569d67ab79a1550d99960ebc7dc
curran <curran.kelleher@gmail.com>
2019-06-28 09:46:15 +0530

    Refactor auth modules into smaller files

---
+++ packages/neoFrontend/src/authentication/AuthPage/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  max-width: 1380px;
+  flex: 1;
+`;
+
+export const Banner = styled.div`
+  height: 100px;
+  margin-left: 6px;
+  margin-right: 6px;
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SignIn = styled.div`
+  color: #f0353d;
+  font-weight: bold;
+  font-size: 14px;
+  cursor: pointer;
+  user-select: none;
+`;
