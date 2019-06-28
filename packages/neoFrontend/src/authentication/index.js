2a1f0e0af977b060525de0d7da861c6042aefdae
curran <curran.kelleher@gmail.com>
2019-06-28 10:28:32 +0530

    Refactor sign in flow

--- packages/neoFrontend/src/authentication/index.js
+++ packages/neoFrontend/src/authentication/index.js
@@ -1,5 +1,4 @@
 export { AuthContext, AuthContextProvider } from './authContext';
 export { AuthPopupPage } from './AuthPopupPage';
 export { AuthPage } from './AuthPage';
-export { signIn } from './signIn';
 export { AUTH_PENDING } from './constants';
