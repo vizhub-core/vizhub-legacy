c91286fff7fd5e42ea296db3571412399fc5803a
curran <curran.kelleher@gmail.com>
2019-06-28 10:05:56 +0530

    Introduce AUTH_PENDING state. Enforce assumption of single AuthContextProvider.

--- packages/neoFrontend/src/authentication/authContext/index.js
+++ packages/neoFrontend/src/authentication/authContext/index.js
@@ -1,12 +1,29 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { fetchMe } from './fetchMe';
+import { AUTH_PENDING } from '../constants';
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {
-  const [me, setMe] = useState();
+let fetchMeCount = 0;
 
+export const AuthContextProvider = ({ children }) => {
+  // The value of `me` can be:
+  //  - AUTH_PENDING - Not yet ascertained whether user is authenticated or not.
+  //  - null - Not authenticated.
+  //  - User object - Authenticated.
+  const [me, setMe] = useState(AUTH_PENDING);
+
+  // Fetch the currently authenticated user once, on page load.
   useEffect(() => {
+    // Enforce assumption of single AuthContextProvider.
+    if (fetchMeCount > 0) {
+      throw new Error(
+        'There should only ever be a single instance of AuthContextProvider'
+      );
+    }
+    fetchMeCount++;
+
+    // TODO handle non-authenticated case.
     fetchMe().then(setMe);
   }, []);
 
