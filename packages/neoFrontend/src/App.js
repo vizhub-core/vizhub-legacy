11d5c82e7e25ec97cd80218d4b49220fa940eec3
curran <curran.kelleher@gmail.com>
2019-06-28 07:40:50 +0530

    Fundamental changes to popup flow

--- packages/neoFrontend/src/App.js
+++ packages/neoFrontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HomePage } from './HomePage';
-import { AuthPopupPage, AuthContextProvider } from './authentication';
+import { AuthPage, AuthPopupPage, AuthContextProvider } from './authentication';
 
 export const App = () => (
   <Router>
     <AuthContextProvider>
       <Route exact path="/" component={HomePage} />
+      <Route path="/auth" component={AuthPage} />
     </AuthContextProvider>
     <Route path="/authenticated" component={AuthPopupPage} />
   </Router>
--- packages/neoFrontend/src/authentication/signIn/openPopup.js
+++ packages/neoFrontend/src/authentication/signIn/openPopup.js
@@ -1,18 +1,5 @@
-import { oAuthURL } from '../constants';
-
-// Configure popup dimensions here (in pixels).
-const popupWidth = 1000;
-const popupHeight = 600;
-
 // Opens a popup window that uses OAuth to authenticate.
 // The popup ends up at the route /authenticated (see Authenticated.js).
 export const openPopup = () => {
-  const popupOptions = [
-    'scrollbars=yes',
-    `width=${popupWidth}`,
-    `height=${popupHeight}`,
-    `top=${window.innerHeight / 2 - popupHeight / 2}`,
-    `left=${window.innerWidth / 2 - popupWidth / 2}`
-  ].join(',');
-  return window.open(oAuthURL, 'github-oauth-authorize', popupOptions);
+  return window.open('/auth', 'vithub-auth');
 };
