f393189f0707cfc0139e73c15730380de6d0e2a5
curran <curran.kelleher@gmail.com>
2019-06-29 07:33:42 +0530

    Rename constants

--- packages/neoFrontend/src/authentication/constants.js
+++ packages/neoFrontend/src/authentication/constants.js
@@ -1,6 +1,6 @@
 // TODO capitalize these for strong convention
-export const clientId = process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_ID;
-export const oAuthURLBase = 'https://github.com/login/oauth/authorize';
-export const oAuthURL = oAuthURLBase + '?client_id=' + clientId;
+export const GITHUB_CLIENT_ID = process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_ID;
+export const OAUTH_URL_BASE = 'https://github.com/login/oauth/authorize';
+export const GITHUB_OAUTH_URL = OAUTH_URL_BASE + '?client_id=' + GITHUB_CLIENT_ID;
 
 export const AUTH_PENDING = 'AUTH_PENDING';
