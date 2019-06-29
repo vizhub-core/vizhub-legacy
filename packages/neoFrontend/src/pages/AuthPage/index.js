bb1a864bdac3ecd9f568198c8058df57d87af90e
curran <curran.kelleher@gmail.com>
2019-06-29 08:11:27 +0530

    Establish theme system. Closes #11

--- packages/neoFrontend/src/pages/AuthPage/index.js
+++ packages/neoFrontend/src/pages/AuthPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Wrapper, Content, Banner, Title } from '../styles';
-import { BoxWrapper, Box } from './styles';
+import { BoxWrapper, Box, Button } from './styles';
 import { LogoSVG } from '../../svg';
 import { GITHUB_OAUTH_URL } from '../../authentication';
 
@@ -14,7 +14,8 @@ export const AuthPage = () => {
         <BoxWrapper>
           <Box>
             <Title>Sign up or Sign in</Title>
-            <a href={GITHUB_OAUTH_URL}>Sign in with GitHub</a>
+            If you don't have an account, one will be created when you sign in.
+            <Button href={GITHUB_OAUTH_URL}>Sign in with GitHub</Button>
           </Box>
         </BoxWrapper>
       </Content>
