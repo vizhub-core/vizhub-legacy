94cd675c1418caa7ec89e1b6657175e7d18062b6
curran <curran.kelleher@gmail.com>
2019-06-29 10:48:56 +0530

    Finalize Auth page.

--- packages/neoFrontend/src/pages/AuthPage/index.js
+++ packages/neoFrontend/src/pages/AuthPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Wrapper, Content, Banner, Title } from '../styles';
-import { BoxWrapper, Box, Button } from './styles';
+import { BoxWrapper, Box, Button, Octocat, Terms } from './styles';
 import { LogoSVG } from '../../svg';
 import { GITHUB_OAUTH_URL } from '../../authentication';
 
@@ -15,7 +15,14 @@ export const AuthPage = () => {
           <Box>
             <Title>Sign up or Sign in</Title>
             If you don't have an account, one will be created when you sign in.
+            <Octocat src="/images/GitHub-Mark-120px-plus.png" />
             <Button href={GITHUB_OAUTH_URL}>Sign in with GitHub</Button>
+            <Terms>
+              By signing in you agree to our{' '}
+              <a href="https://datavis.tech/static/legal/Terms%20of%20Use.pdf">
+                terms of use.
+              </a>
+            </Terms>
           </Box>
         </BoxWrapper>
       </Content>
