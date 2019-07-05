bc6ff9327e0581196378144d49008e1ae6d97384
Curran <curran.kelleher@gmail.com>
2019-07-05 12:39:56 +0530

    Add loading indicator. Closes #12

---
+++ packages/neoFrontend/src/pages/HomePage/HomePageData.js
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react';
+import { useHomePageData } from './useHomePageData';
+
+export const HomePageDataContext = createContext();
+
+export const HomePageDataProvider = ({ fallback, children }) => {
+  const homePageData = useHomePageData();
+
+  return homePageData ? (
+    <HomePageDataContext.Provider value={homePageData}>
+      {children}
+    </HomePageDataContext.Provider>
+  ) : (
+    fallback
+  );
+};
--- packages/neoFrontend/src/pages/HomePage/index.js
+++ packages/neoFrontend/src/pages/HomePage/index.js
@@ -1,24 +1,28 @@
 import React, { useContext } from 'react';
-import { Wrapper, Content, Banner } from '../styles';
-import { SignIn } from './styles';
 import { LogoSVG } from '../../svg';
 import { AuthContext, AUTH_PENDING } from '../../authentication';
+import { LoadingScreen } from '../../LoadingScreen';
+import { Wrapper, Content, Banner } from '../styles';
+import { SignIn } from './styles';
+import { HomePageDataProvider } from './HomePageData';
 
 export const HomePage = () => {
   const { me, signIn, signOut } = useContext(AuthContext);
 
   return (
-    <Wrapper>
-      <Content>
-        <Banner>
-          <LogoSVG height={40} fill="currentcolor" />
-          {me === AUTH_PENDING ? null : me ? (
-            <div onClick={signOut}>sign out</div>
-          ) : (
-            <SignIn onClick={signIn}>Sign up / Sign in</SignIn>
-          )}
-        </Banner>
-      </Content>
-    </Wrapper>
+    <HomePageDataProvider fallback={<LoadingScreen />}>
+      <Wrapper>
+        <Content>
+          <Banner>
+            <LogoSVG height={40} fill="currentcolor" />
+            {me === AUTH_PENDING ? null : me ? (
+              <div onClick={signOut}>sign out</div>
+            ) : (
+              <SignIn onClick={signIn}>Sign up / Sign in</SignIn>
+            )}
+          </Banner>
+        </Content>
+      </Wrapper>
+    </HomePageDataProvider>
   );
 };
