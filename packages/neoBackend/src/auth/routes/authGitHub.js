533f4ae3b25890ac17ecd50de8075a1f00c8141f
Curran <curran.kelleher@gmail.com>
2019-07-06 15:05:48 +0530

    Implement get or create user.

--- packages/neoBackend/src/auth/routes/authGitHub.js
+++ packages/neoBackend/src/auth/routes/authGitHub.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import { CreateUser } from 'datavis-tech-use-cases';
 import { toErrorResponse } from '../../Error';
 import { getAccessToken } from '../getAccessToken';
 import { getGitHubUser } from '../getGitHubUser';
@@ -7,13 +8,26 @@ import { jwtSign, jwtVerify } from '../jwt';
 export const authGitHub = userGateway => asyncHandler(async (req, res) => {
   try {
     const accessToken = await getAccessToken(req.body.code);
-    const oAuthProfile = await getGitHubUser(accessToken);
+    const gitHubUser = await getGitHubUser(accessToken);
+    const userName = gitHubUser.login;
 
-    // TODO get or create VizHub user entity here,
-    // pass the ID from that
-    const requestModel = { oAuthProfile };
-    const responseModel = await createUser.execute(requestModel);
-    return responseModel.user;
+    // TODO move this logic into getOrCreateUser interactor (in useCases).
+    try {
+      const user = await userGateway.getUserByUserName(userName);
+      console.log('user exists!');
+      console.log(user);
+    } catch (error) {
+      console.log('user does not exist, creating!');
+      const createUser = new CreateUser({ userGateway });
+      const oAuthProfile = {
+        id: '' + gitHubUser.id,
+        username: gitHubUser.login,
+        _json: gitHubUser
+      };
+      const requestModel = { oAuthProfile };
+      const responseModel = await createUser.execute(requestModel);
+      console.log(responseModel);
+    }
 
     const vizHubJWT = await jwtSign(gitHubUser);
     res.cookie('vizHubJWT', vizHubJWT, { httpOnly: true });
