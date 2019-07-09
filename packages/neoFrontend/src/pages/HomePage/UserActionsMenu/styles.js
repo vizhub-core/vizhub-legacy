b987d2929d6d4ee7192c0faba9231d79075dbdd9
Curran <curran.kelleher@gmail.com>
2019-07-09 11:53:46 +0530

    Implement AvatarOverlay

--- packages/neoFrontend/src/pages/HomePage/UserActionsMenu/styles.js
+++ packages/neoFrontend/src/pages/HomePage/UserActionsMenu/styles.js
@@ -1,4 +1,16 @@
 import styled from 'styled-components';
 
 export const AvatarWrapper = styled.div`
+  position: relative;
+`;
+
+export const AvatarOverlay = styled.div`
+  position: absolute;
+  top: 0px;
+  width: 40px;
+  height: 40px;
+  border: solid 1px ${props => props.theme.dark};
+  border-radius: 20px;
+  background-color: rgba(246, 238, 227, 0.83);
+  pointer-events: none;
 `;
