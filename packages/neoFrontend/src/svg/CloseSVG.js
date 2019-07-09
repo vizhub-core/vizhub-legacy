844956e4813f32b858009d79531dbdf5dfc76b1e
Curran <curran.kelleher@gmail.com>
2019-07-09 11:22:56 +0530

    Prettier

--- packages/neoFrontend/src/svg/CloseSVG.js
+++ packages/neoFrontend/src/svg/CloseSVG.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-export const CloseSVG = ({ height, fill = "currentcolor" }) => (
+export const CloseSVG = ({ height, fill = 'currentcolor' }) => (
   <svg height={height} viewBox={`0 0 20 20`}>
-    <g fill={fill} >
-        <rect width="24" height="2" x="-2" y="9" rx="1" transform="rotate(45 10 10)"/>
-        <rect width="24" height="2" x="-2" y="9" rx="1" transform="rotate(135 10 10)"/>
+    <g fill={fill}>
+      <rect
+        width="24"
+        height="2"
+        x="-2"
+        y="9"
+        rx="1"
+        transform="rotate(45 10 10)"
+      />
+      <rect
+        width="24"
+        height="2"
+        x="-2"
+        y="9"
+        rx="1"
+        transform="rotate(135 10 10)"
+      />
     </g>
   </svg>
 );
