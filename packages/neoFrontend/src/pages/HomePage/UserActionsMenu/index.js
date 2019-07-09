637a39f8e76d9c9cb9e1c2f6e98234b47afd5cf9
Curran <curran.kelleher@gmail.com>
2019-07-09 12:10:22 +0530

    Close menu on global click

--- packages/neoFrontend/src/pages/HomePage/UserActionsMenu/index.js
+++ packages/neoFrontend/src/pages/HomePage/UserActionsMenu/index.js
@@ -1,17 +1,47 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { AuthContext } from '../../../authentication';
 import { Avatar } from '../Avatar';
 import { AvatarOverlay, AvatarWrapper } from './styles';
 import { CloseSVG } from '../../../svg';
+
+// Close the menu if the user clicks anywhere else on the page.
+const useCloseOnGlobalClick = (open, close) => {
+  useEffect(() => {
+    if (open) {
+      document.addEventListener('click', close);
+      return () => {
+        document.removeEventListener('click', close);
+      };
+    }
+  }, [open, close]);
+};
+
 export const UserActionsMenu = () => {
   const { me } = useContext(AuthContext);
-  //const [open, setOpen] = useState(false);
   const [open, setOpen] = useState(true);
-  const toggle = () => setOpen(!open);
+
+  const toggle = useCallback(
+    event => {
+      setOpen(!open);
+    },
+    [open]
+  );
+
+  const close = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  useCloseOnGlobalClick(open, close);
+
   return (
     <AvatarWrapper>
       <Avatar user={me} onClick={toggle} />
-      {open ? <AvatarOverlay> <CloseSVG height="25"/></AvatarOverlay> : null}
+      {open ? (
+        <AvatarOverlay>
+          {' '}
+          <CloseSVG height="25" />
+        </AvatarOverlay>
+      ) : null}
     </AvatarWrapper>
   );
 };
