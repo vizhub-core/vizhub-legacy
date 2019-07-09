9c9b3da9b034a2a4ffcb74e2eb16e9032cdd782d
Curran <curran.kelleher@gmail.com>
2019-07-09 10:59:07 +0530

    Add SVG icons

---
+++ packages/neoFrontend/src/svg/ArrowLeftSVG.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ArrowLeftSVG = ({ height, fill = "currentcolor" }) => (
+  <svg height={height} viewBox={`0 0 11 20`}>
+    <g fill={fill}>
+        <rect width="13" height="2" x="-1" y="12.778" rx="1" transform="rotate(45 5.5 13.778)"/>
+        <rect width="13" height="2" x="-1" y="5" rx="1" transform="rotate(135 5.5 6)"/>
+    </g>
+  </svg>
+);
---
+++ packages/neoFrontend/src/svg/CloseSVG.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const CloseSVG = ({ height, fill = "currentcolor" }) => (
+  <svg height={height} viewBox={`0 0 20 20`}>
+    <g fill={fill} >
+        <rect width="24" height="2" x="-2" y="9" rx="1" transform="rotate(45 10 10)"/>
+        <rect width="24" height="2" x="-2" y="9" rx="1" transform="rotate(135 10 10)"/>
+    </g>
+  </svg>
+);
