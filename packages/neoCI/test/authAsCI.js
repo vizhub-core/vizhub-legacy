0eb21d9f0fd4094c047ba01a461ac7ab99a0fc5e
Curran <curran.kelleher@gmail.com>
2019-07-09 15:42:33 +0530

    Add end to end test for log out

--- packages/neoCI/test/authAsCI.js
+++ packages/neoCI/test/authAsCI.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 
+// TODO rename to signIn
 export const authAsCI = my => async () => {
   const { popup, page } = my;
   await (await popup.waitFor('.test-sign-in-as-ci')).click();
--- packages/neoFrontend/src/pages/HomePage/UserActionsMenu/useClose.js
+++
@@ -1,6 +0,0 @@
-import { useCallback } from 'react';
-
-export const useClose = setOpen =>
-  useCallback(() => {
-    setOpen(false);
-  }, [setOpen]);
