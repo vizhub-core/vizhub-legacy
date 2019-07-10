141b78e0247fa030316049eea6fc5e53fc8d10ee
Curran <curran.kelleher@gmail.com>
2019-07-10 14:04:29 +0530

    Migrate JWT auth logic into controllers package

--- packages/controllers/src/jwtAuth/index.js
+++ packages/controllers/src/jwtAuth/index.js
@@ -1,6 +1,6 @@
 import { authGitHub, authMe, authCI, authSignOut } from './routes';
 
-export const auth = (app, userGateway) => {
+export const jwtAuth = (app, userGateway) => {
   app.post('/api/auth/github', authGitHub(userGateway));
   app.get('/api/auth/me', authMe(userGateway));
   app.post('/api/auth/ci', authCI);
