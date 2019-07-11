b26e6664d81228b07dd8f6196aab6975e2fd55f0
Curran <curran.kelleher@gmail.com>
2019-07-11 12:12:33 +0530

    Establish approach for dark navbar theme

--- packages/neoFrontend/src/pages/VizPage/index.js
+++ packages/neoFrontend/src/pages/VizPage/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { LoadingScreen } from '../../LoadingScreen';
+import { darkNavbarTheme } from '../../theme';
 import { VizPageDataProvider } from './VizPageDataContext';
 import { URLStateProvider } from './URLStateContext';
 import { VizPageBody } from './VizPageBody';
@@ -7,7 +9,9 @@ import { VizPageBody } from './VizPageBody';
 export const VizPage = () => (
   <URLStateProvider>
     <VizPageDataProvider fallback={<LoadingScreen />}>
-      <VizPageBody />
+      <ThemeProvider theme={darkNavbarTheme}>
+        <VizPageBody />
+      </ThemeProvider>
     </VizPageDataProvider>
   </URLStateProvider>
 );
