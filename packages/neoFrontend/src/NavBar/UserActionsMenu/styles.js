995dd79f0bf4a94c0ecf8cd5b9ba1e561133a298
Curran <curran.kelleher@gmail.com>
2019-07-11 14:33:17 +0530

    Implement foreground of dark variant user actions menu

--- packages/neoFrontend/src/NavBar/UserActionsMenu/styles.js
+++ packages/neoFrontend/src/NavBar/UserActionsMenu/styles.js
@@ -11,9 +11,9 @@ export const AvatarOverlay = styled.div`
   width: ${props => props.size}px;
   height: ${props => props.size}px;
   box-sizing: border-box;
-  border: solid 1px ${props => props.theme.dark};
   border-radius: 20px;
-  background-color: rgba(246, 238, 227, 0.83);
+  border: solid 1px ${props => props.theme.userMenuOverlayForeground};
+  background-color: ${props => props.theme.userMenuOverlayBackground};
 
   display: flex;
   align-items: center;
@@ -23,7 +23,7 @@ export const AvatarOverlay = styled.div`
 
 export const Menu = styled.div`
   position: absolute;
-  top: 46px;
+  top: ${props => props.height + 6}px;
   right: 0;
 
   border-radius: 6px;
