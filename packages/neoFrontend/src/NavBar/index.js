b022e408cc0ee3f06c4b84ab77921d790b69d1f8
Curran <curran.kelleher@gmail.com>
2019-07-11 12:29:34 +0530

    Themify navbar logo color and banner padding

--- packages/neoFrontend/src/NavBar/index.js
+++ packages/neoFrontend/src/NavBar/index.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
+import { withTheme } from 'styled-components';
 import { LogoSVG } from '../svg';
 import { AuthContext, AUTH_PENDING } from '../authentication';
 import { Banner } from '../styles';
 import { SignIn } from './styles';
 import { UserActionsMenu } from './UserActionsMenu';
 
-export const NavBar = () => {
+export const NavBar = withTheme(({ theme }) => {
   const { me, signIn } = useContext(AuthContext);
+  console.log(theme.navbarLogoColor);
   return (
     <Banner>
-      <LogoSVG height={40} fill="currentcolor" />
+      <LogoSVG height={theme.navbarLogoHeight} fill={theme.navbarLogoColor} />
       {me === AUTH_PENDING ? null : me ? (
         <UserActionsMenu />
       ) : (
@@ -19,4 +21,4 @@ export const NavBar = () => {
       )}
     </Banner>
   );
-};
+});
