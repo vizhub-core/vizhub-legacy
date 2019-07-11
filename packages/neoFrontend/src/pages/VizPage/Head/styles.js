b81097931205dd63d6442765f857167d5924eda9
Curran <curran.kelleher@gmail.com>
2019-07-11 14:50:39 +0530

    Add stub for Viz page Head component

---
+++ packages/neoFrontend/src/pages/VizPage/Head/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 30px;
+  background-color: #ffffff;
+  box-shadow: ${props => props.theme.shadow};
+`;
--- packages/neoFrontend/src/pages/VizPage/VizPageBody.js
+++ packages/neoFrontend/src/pages/VizPage/VizPageBody.js
@@ -2,12 +2,15 @@ import React, { useContext } from 'react';
 import { NavBar } from '../../NavBar';
 import { Wrapper, Content } from '../styles';
 import { URLStateContext } from './URLStateContext';
+import { Head } from './Head';
 
 export const VizPageBody = () => {
+  // TODO move this access logic into useVizPageData
   const { vizId } = useContext(URLStateContext);
   return (
     <>
       <NavBar />
+      <Head />
       <Wrapper>
         <Content>Hello viz {vizId}!</Content>
       </Wrapper>
