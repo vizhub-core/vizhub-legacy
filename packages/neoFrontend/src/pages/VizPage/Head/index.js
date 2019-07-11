bdda9fb181bc7b0ffba96fbbf278282258040105
Curran <curran.kelleher@gmail.com>
2019-07-11 16:18:05 +0530

    Add action icons to Viz page Head.

--- packages/neoFrontend/src/pages/VizPage/Head/index.js
+++ packages/neoFrontend/src/pages/VizPage/Head/index.js
@@ -1,4 +1,34 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import { Wrapper, Right, Left, Icon } from './styles';
 
-export const Head = () => <Wrapper />;
+import {
+  ForkSVG,
+  PullSVG,
+  SettingsSVG,
+  ShareSVG,
+  ArrowLeftSVG,
+  ArrowRightSVG
+} from '../../../svg';
+
+export const Head = () => (
+  <Wrapper>
+    <Left>
+      <ArrowLeftSVG />
+      <ArrowRightSVG />
+    </Left>
+    <Right>
+      <Icon>
+        <PullSVG />
+      </Icon>
+      <Icon>
+        <ForkSVG />
+      </Icon>
+      <Icon>
+        <ShareSVG />
+      </Icon>
+      <Icon rightmost={true}>
+        <SettingsSVG />
+      </Icon>
+    </Right>
+  </Wrapper>
+);
--- packages/neoFrontend/src/pages/VizPage/Head/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Head/styles.js
@@ -4,4 +4,32 @@ export const Wrapper = styled.div`
   min-height: 30px;
   background-color: #ffffff;
   box-shadow: ${props => props.theme.shadow};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Left = styled.div`
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Right = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 30px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  margin-right: ${props => (props.rightmost ? '9px' : '0')};
+  cursor: pointer;
 `;
