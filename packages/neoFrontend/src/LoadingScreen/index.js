ba52f9f6dea9b0046556a5d086c7470fe0e652f2
Curran <curran.kelleher@gmail.com>
2019-07-12 09:48:02 +0530

    Refine loading screen timing

--- packages/neoFrontend/src/LoadingScreen/index.js
+++ packages/neoFrontend/src/LoadingScreen/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LogoSVG } from '../svg';
 import { LoadingScreenWrapper, SpinningLogo, Message } from './styles';
+import { blankScreenDelay } from './animationDelay';
 export { animationDelay } from './animationDelay';
+export { blankScreenDelay };
 
-export const LoadingScreen = ({ message }) => (
-  <LoadingScreenWrapper>
-    <SpinningLogo>
-      <LogoSVG />
-    </SpinningLogo>
-    {message ? <Message>{message}</Message> : null}
-  </LoadingScreenWrapper>
-);
+export const LoadingScreen = ({ message }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShow(true);
+    }, blankScreenDelay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return show ? (
+    <LoadingScreenWrapper>
+      <SpinningLogo>
+        <LogoSVG />
+      </SpinningLogo>
+      {message ? <Message>{message}</Message> : null}
+    </LoadingScreenWrapper>
+  ) : null;
+};
