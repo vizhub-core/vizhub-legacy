00438d996eb69dde252087ac9370737a48c16077
Curran <curran.kelleher@gmail.com>
2019-07-12 09:48:02 +0530

    Refine loading screen timing

--- packages/neoFrontend/src/LoadingScreen/animationDelay.js
+++ packages/neoFrontend/src/LoadingScreen/animationDelay.js
@@ -1,2 +1,9 @@
-// Don't show the animation in development, as it slows down iterations and tests.
-export const animationDelay = process.env.NODE_ENV === 'development' ? 0 : 1000;
+// The time in ms before kicking off the loading screen.
+// If the data request finishes before this time,
+// the loading screen will never appear.
+export const blankScreenDelay = 200;
+
+// The minimum time in ms to keep the loading screen visible,
+// in the case that loading the data takes longer than
+// blankScreenDelay.
+export const animationDelay = 800;
--- packages/neoFrontend/src/pages/HomePage/useHomePageData.js
+++ packages/neoFrontend/src/pages/HomePage/useHomePageData.js
@@ -1,13 +1,40 @@
 import { useState, useEffect } from 'react';
-import { animationDelay } from '../../LoadingScreen';
+import { animationDelay, blankScreenDelay } from '../../LoadingScreen';
+
+// TODO generalize this, use it on all pages:
+// src/pages/VizPage/useVizPageData.js
+// src/pages/CreateVizPage/useCreateVizPageData.js
+const waitForSpinner = dataLoaded =>
+  new Promise(resolve => {
+    let animationStarted = false;
+
+    const blankScreenTimeoutId = setTimeout(() => {
+      animationStarted = true;
+      setTimeout(() => {
+        resolve();
+      }, animationDelay);
+    }, blankScreenDelay);
+
+    dataLoaded.then(() => {
+      if (!animationStarted) {
+        clearTimeout(blankScreenTimeoutId);
+        resolve();
+      }
+    });
+  });
 
 // TODO make an API request here.
 export const useHomePageData = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setTimeout(() => {
+    const dataLoaded = new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 300);
+    });
+    waitForSpinner(dataLoaded).then(() => {
       setLoading(false);
-    }, animationDelay);
+    });
   }, []);
   return !loading;
 };
