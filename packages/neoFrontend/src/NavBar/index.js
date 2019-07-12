952ee3651eed2e6ca76a1dbaaa3ac038d2d1c1ba
Curran <curran.kelleher@gmail.com>
2019-07-12 09:23:48 +0530

    Refine Logo link in navbar

--- packages/neoFrontend/src/NavBar/index.js
+++ packages/neoFrontend/src/NavBar/index.js
@@ -1,25 +1,35 @@
 import React, { useContext } from 'react';
 import { withTheme } from 'styled-components';
+import { withRouter } from 'react-router';
 import { LogoSVG } from '../svg';
 import { AuthContext, AUTH_PENDING } from '../authentication';
 import { Banner } from '../styles';
-import { SignIn } from './styles';
+import { SignIn, LogoLink } from './styles';
 import { UserActionsMenu } from './UserActionsMenu';
 
-export const NavBar = withTheme(({ theme }) => {
-  const { navbarItemHeight, navbarLogoColor } = theme;
-  const { me, signIn } = useContext(AuthContext);
+export const NavBar = withRouter(
+  withTheme(({ theme, location }) => {
+    const { navbarItemHeight, navbarLogoColor } = theme;
+    const { me, signIn } = useContext(AuthContext);
 
-  return (
-    <Banner>
-      <LogoSVG height={navbarItemHeight} fill={navbarLogoColor} />
-      {me === AUTH_PENDING ? null : me ? (
-        <UserActionsMenu />
-      ) : (
-        <SignIn className="test-sign-in" onClick={signIn}>
-          Sign up / Sign in
-        </SignIn>
-      )}
-    </Banner>
-  );
-});
+    return (
+      <Banner>
+        {location.pathname === '/' ? (
+          <LogoSVG height={navbarItemHeight} fill={navbarLogoColor} />
+        ) : (
+          <LogoLink to="/">
+            <LogoSVG height={navbarItemHeight} fill={navbarLogoColor} />
+          </LogoLink>
+        )}
+
+        {me === AUTH_PENDING ? null : me ? (
+          <UserActionsMenu />
+        ) : (
+          <SignIn className="test-sign-in" onClick={signIn}>
+            Sign up / Sign in
+          </SignIn>
+        )}
+      </Banner>
+    );
+  })
+);
