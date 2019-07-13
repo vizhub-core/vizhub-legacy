c654c47f46636d43b36f7e87e2164fd8cea55e53
Curran <curran.kelleher@gmail.com>
2019-07-13 16:20:17 +0530

    Add description section first pass

---
+++ packages/neoFrontend/src/pages/VizPage/Body/DescriptionSection/styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  padding: 20px 0 20px 0;
+  font-size: 10px;
+`;
+
+export const Left = styled.div`
+  padding-right: 10px;
+`;
+
+export const Middle = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const Right = styled.div`
+  padding-left: 50px;
+`;
+
+export const Authorship = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
+`;
+
+export const Video = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const VideoThumbnail = styled.div`
+  width: 200px;
+  height: 113px;
+  box-shadow: ${props => props.theme.shadowLight};
+  background-color: #ffffff;
+`;
+
+export const Description = styled.div`
+  font-size: 12px;
+`;
+
+export const AuthorName = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const AuthorshipMeta = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
