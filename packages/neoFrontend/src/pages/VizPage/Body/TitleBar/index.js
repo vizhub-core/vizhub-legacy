164702e018d988bee48cae5d8ee20d129f5fdc86
Curran <curran.kelleher@gmail.com>
2019-07-13 14:18:45 +0530

    Add upvote and downvote icons

--- packages/neoFrontend/src/pages/VizPage/Body/TitleBar/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/TitleBar/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Wrapper, Title, Voter } from './styles';
+import { Wrapper, Title, Voter, VoteIcon } from './styles';
+import { VoteSVG } from '../../../../svg';
 
 export const TitleBar = () => (
   <Wrapper>
     <Title>Let's make a bar chart</Title>
-    <Voter>Voter</Voter>
+    <Voter>
+      <VoteIcon>
+        <VoteSVG />
+      </VoteIcon>
+      36
+      <VoteIcon>
+        <VoteSVG down={true} />
+      </VoteIcon>
+      6
+    </Voter>
   </Wrapper>
 );
