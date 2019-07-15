c793f83f025bedce42be2d17ef4aef59a156e6b1
Curran <curran.kelleher@gmail.com>
2019-07-15 14:20:00 +0530

    Iterate loading indicator to use spinner.

--- packages/neoFrontend/src/pages/fakeDataLoaded.js
+++ packages/neoFrontend/src/pages/fakeDataLoaded.js
@@ -1,5 +1,5 @@
 const delay = process.env.NODE_ENV === 'development' ? 0 : 1000;
-//const delay = 1000;
+//const delay = 1000000;
 export const fakeDataLoaded = () =>
   new Promise(resolve => {
     setTimeout(() => {
