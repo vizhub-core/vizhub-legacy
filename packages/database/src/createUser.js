a6a02f86b8abd2524a13a1d49a832b6224e9c4e9
Curran <curran.kelleher@gmail.com>
2019-07-15 14:01:25 +0530

    Fix GitHub oAuth error

--- packages/database/src/createUser.js
+++ packages/database/src/createUser.js
@@ -2,7 +2,6 @@ import { USER } from './collectionName';
 
 export const createUser = connection => user => {
   return new Promise(resolve => {
-    // TODO handle errors here
     connection.get(USER, user.id).create(user);
     resolve(user);
   });
