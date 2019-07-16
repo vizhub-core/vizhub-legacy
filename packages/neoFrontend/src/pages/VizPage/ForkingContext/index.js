42bd64d135279d615f95bb4780b87f5c10eb865d
Curran <curran.kelleher@gmail.com>
2019-07-16 17:52:04 +0530

    Forking navigation. Closes #50

--- packages/neoFrontend/src/pages/VizPage/ForkingContext/index.js
+++ packages/neoFrontend/src/pages/VizPage/ForkingContext/index.js
@@ -1,14 +1,15 @@
 import React, { createContext } from 'react';
 import { useForking } from './useForking';
+import { withRouter } from 'react-router';
 
 export const ForkingContext = createContext();
 
-export const ForkingProvider = ({ fallback, children }) => {
-  const { onFork, isForking } = useForking();
+export const ForkingProvider = withRouter(({ fallback, children, history }) => {
+  const { onFork, isForking } = useForking(history);
 
   return !isForking ? (
     <ForkingContext.Provider value={onFork}>{children}</ForkingContext.Provider>
   ) : (
     fallback
   );
-};
+});
