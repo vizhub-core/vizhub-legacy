d44e51235d69d8a80ad42a9914ad633b7b5ef38f
Curran <curran.kelleher@gmail.com>
2019-07-16 11:18:45 +0530

    Fetch viz page data from backend

---
+++ packages/neoFrontend/src/pages/VizPage/fetchVizPageData.js
@@ -0,0 +1,7 @@
+export const fetchVizPageData = async vizId => {
+  const response = await fetch(`/api/visualization/get/${vizId}`, {
+    method: 'GET',
+    credentials: 'same-origin'
+  });
+  return await response.json();
+};
