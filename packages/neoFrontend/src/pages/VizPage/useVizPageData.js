9563d98669e9f51a18c69fbac5a00ef1b8da2cc7
Curran <curran.kelleher@gmail.com>
2019-07-16 11:18:45 +0530

    Fetch viz page data from backend

--- packages/neoFrontend/src/pages/VizPage/useVizPageData.js
+++ packages/neoFrontend/src/pages/VizPage/useVizPageData.js
@@ -1,9 +1,8 @@
 import { useState, useEffect, useContext } from 'react';
 import { waitForSpinner } from '../../LoadingScreen';
-import { fakeDataLoaded } from '../fakeDataLoaded';
 import { URLStateContext } from './URLStateContext';
+import { fetchVizPageData } from './fetchVizPageData';
 
-// TODO make an API request here, for the viz content required.
 export const useVizPageData = () => {
   const [loading, setLoading] = useState(true);
 
@@ -11,7 +10,9 @@ export const useVizPageData = () => {
 
   useEffect(() => {
     setLoading(true);
-    const dataLoaded = fakeDataLoaded();
+
+    const dataLoaded = fetchVizPageData(vizId);
+
     console.log('TODO fetch data for ' + vizId);
     waitForSpinner(dataLoaded).then(data => {
       console.log(data);
