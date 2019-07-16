6cb6f2fd98914bd7ce45df82f5942284e95a5067
Curran <curran.kelleher@gmail.com>
2019-07-16 14:24:02 +0530

    Render actual dates for created and edited

--- packages/neoFrontend/src/pages/VizPage/Body/DescriptionSection/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/DescriptionSection/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import marked from 'marked';
+import { toDate } from 'vizhub-entities';
 import { Avatar } from '../../../../Avatar';
 import {
   Wrapper,
@@ -17,37 +18,56 @@ import {
   Author
 } from './styles';
 
-export const DescriptionSection = ({ visualization, ownerUser }) => (
-  <Wrapper>
-    <Left>
-      <Authorship>
-        <Author>
-          <AuthorAvatar>
-            <Avatar size={40} user={ownerUser} />
-          </AuthorAvatar>
-          <AuthorName>{ownerUser.fullName}</AuthorName>
-        </Author>
-        <AuthorshipMeta>
-          <div>
-            Lasted Edited <SemiBold>June 5, 2019</SemiBold>
-          </div>
-          <div>
-            Forked from <VizLink to="/">Name of forked viz</VizLink>{' '}
-            <SemiBold>January 6, 2018</SemiBold>
-          </div>
-        </AuthorshipMeta>
-      </Authorship>
-      <Description
-        dangerouslySetInnerHTML={{
-          __html: marked(visualization.info.description)
-        }}
-      />
-    </Left>
-    <Right>
-      <Video>
-        <VideoThumbnail />
-        Video title
-      </Video>
-    </Right>
-  </Wrapper>
-);
+const formatTimestamp = timestamp =>
+  toDate(timestamp).toLocaleString(undefined, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+export const DescriptionSection = ({ visualization, ownerUser }) => {
+  const created = useMemo(
+    () => formatTimestamp(visualization.info.createdTimestamp),
+    [visualization.info.createdTimestamp]
+  );
+
+  const lastUpdated = useMemo(
+    () => formatTimestamp(visualization.info.lastUpdatedTimestamp),
+    [visualization.info.lastUpdatedTimestamp]
+  );
+
+  return (
+    <Wrapper>
+      <Left>
+        <Authorship>
+          <Author>
+            <AuthorAvatar>
+              <Avatar size={40} user={ownerUser} />
+            </AuthorAvatar>
+            <AuthorName>{ownerUser.fullName}</AuthorName>
+          </Author>
+          <AuthorshipMeta>
+            <div>
+              Lasted Edited <SemiBold>{lastUpdated}</SemiBold>
+            </div>
+            <div>
+              Forked from <VizLink to="/">Name of forked viz</VizLink>{' '}
+              <SemiBold>{created}</SemiBold>
+            </div>
+          </AuthorshipMeta>
+        </Authorship>
+        <Description
+          dangerouslySetInnerHTML={{
+            __html: marked(visualization.info.description)
+          }}
+        />
+      </Left>
+      <Right>
+        <Video>
+          <VideoThumbnail />
+          Video title
+        </Video>
+      </Right>
+    </Wrapper>
+  );
+};
