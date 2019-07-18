8c2f16fe208e19d125b308ebbda185cb28aa1b93
Curran <curran.kelleher@gmail.com>
2019-07-18 17:44:14 +0530

    Got iframe to transform correctly

--- packages/neoFrontend/src/pages/VizPage/VizRunnerContext/index.js
+++ packages/neoFrontend/src/pages/VizPage/VizRunnerContext/index.js
@@ -1,36 +1,39 @@
-import React, { createContext, useState, useLayoutEffect } from 'react';
+import React, { createContext, useContext, useState, useRef } from 'react';
+import { VizPageDataContext } from '../VizPageDataContext';
+import { defaultVizHeight, vizWidth } from '../../../constants';
+import { IFrame } from './styles';
 
 export const VizRunnerContext = createContext();
 
 export const VizRunnerProvider = ({ children }) => {
-  const [vizRunnerIFrame, setVizRunnerIFrame] = useState();
+  const iFrameRef = useRef();
+  const { visualization } = useContext(VizPageDataContext);
+  const vizHeight = visualization.info.height || defaultVizHeight;
 
-  useLayoutEffect(() => {
-    const iFrame = document.createElement('iframe');
-    iFrame.setAttribute(
-      'src',
-      'https://vizhub.com/curran/d53bb1e4cefc44a09d11cc3af81f521a/fullscreen'
-    );
-    iFrame.style.border = 0;
-    iFrame.setAttribute('width', 960);
+  const [vizRunnerTransform, setVizRunnerTransform] = useState({
+    x: 0,
+    y: 0,
+    scale: 1
+  });
 
-    // TODO set this dynamically from viz data
-    iFrame.setAttribute('height', 500);
+  const contextValue = { vizRunnerTransform, setVizRunnerTransform };
 
-    // style={{
-    //   transform: `scale(${scale})`,
-    //   transformOrigin: '0 0',
-    // }}
+  const { x, y, scale } = vizRunnerTransform;
 
-    setVizRunnerIFrame(iFrame);
-    //return () => {
-    //  iFrame.remove();
-    //};
-  }, []);
-
-  return vizRunnerIFrame ? (
-    <VizRunnerContext.Provider value={{ vizRunnerIFrame }}>
-      {children}
-    </VizRunnerContext.Provider>
-  ) : null;
+  return (
+    <>
+      <VizRunnerContext.Provider value={contextValue}>
+        {children}
+      </VizRunnerContext.Provider>
+      <IFrame
+        src="https://vizhub.com/curran/d53bb1e4cefc44a09d11cc3af81f521a/fullscreen"
+        ref={iFrameRef}
+        width={vizWidth}
+        height={vizHeight}
+        style={{
+          transform: `scale(${scale}) translate(${x}px, ${y}px)`
+        }}
+      />
+    </>
+  );
 };
---
+++ packages/neoFrontend/src/pages/VizPage/VizRunnerContext/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const IFrame = styled.iframe`
+  position: fixed;
+  top: 0;
+  left: 0;
+  border: 0;
+  transform-origin: 0 0;
+`;
