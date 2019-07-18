0cb01351e46bc751d567cbd878178a144e4dd3d1
Curran <curran.kelleher@gmail.com>
2019-07-18 15:23:55 +0530

    Make VizContent interactive

--- packages/neoFrontend/src/pages/VizPage/Body/VizFrame/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/VizFrame/styles.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
-import { Z_ABOVE, Z_BELOW } from '../../../../styles';
+import { Z_ABOVE, Z_BELOW, Z_NEW_STACKING_CONTEXT } from '../../../../styles';
 import { Footer } from '../styles';
 
+// Use a new stacking context in order to
+//  - Have the shadow from the footer cast onto the VizContent, and
+//  - also have the VizContent interactive (exposed to pointer events).
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  position: relative;
+  z-index: ${Z_NEW_STACKING_CONTEXT};
 `;
 
 export const Content = styled.div`
--- packages/neoFrontend/src/pages/VizPage/VizRunnerContext/index.js
+++ packages/neoFrontend/src/pages/VizPage/VizRunnerContext/index.js
@@ -9,10 +9,20 @@ export const VizRunnerProvider = ({ children }) => {
     const iFrame = document.createElement('iframe');
     iFrame.setAttribute(
       'src',
-      'https://vizhub.com/curran/7ff25d963fbe460387ba07ac4c6494c6/fullscreen'
+      'https://vizhub.com/curran/d53bb1e4cefc44a09d11cc3af81f521a/fullscreen'
     );
+    iFrame.style.border = 0;
+    iFrame.setAttribute('width', 960);
+
+    // TODO set this dynamically from viz data
+    iFrame.setAttribute('height', 500);
+
+    // style={{
+    //   transform: `scale(${scale})`,
+    //   transformOrigin: '0 0',
+    // }}
+
     setVizRunnerIFrame(iFrame);
-    return () => iFrame.remove();
   }, []);
 
   return vizRunnerIFrame ? (
