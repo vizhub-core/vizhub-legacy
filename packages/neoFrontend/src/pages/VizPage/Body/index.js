fe05f9147a89087c9839de132e941bd3ea19ba6f
Curran <curran.kelleher@gmail.com>
2019-07-19 15:44:07 +0530

    Restore missing title. Delete prose.

--- packages/neoFrontend/src/pages/VizPage/Body/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/index.js
@@ -16,36 +16,6 @@ import { TitleBar } from './TitleBar';
 import { DescriptionSection } from './DescriptionSection';
 import { FullScreen } from './FullScreen';
 
-/*
-
-TODO write blog post on this? maybe?
-How to handle runner iframe?
-
-Woule like to have low-level control over:
-
- - positioning
- - transitions in position
- - when the srcDoc attribute is reset
-
-Do not want to tear down and re-create on each view change.
-
-Approaches:
-
- - Have a persistent DOM node that is always in the same place in the DOM tree.
- - Have a persistent DOM node that is attached and detached from DOM tree as needed.
-
-Cases to handle:
-
- - Within viewer
-   - Need to scroll nicely
-   - Ideally could simply attach to DOM here
-     - Would get scrolling and Z ordering "for free"
- - Full screen
- - Mini view
- - Transitions
-   - Would need to do transition "outside" DOM tree.
-*/
-
 export const Body = () => {
   const {
     visualization,
@@ -85,7 +55,7 @@ export const Body = () => {
               onFullScreen={onFullScreen}
               scrollerRef={scrollerRef}
             />
-            <TitleBar title={visualization.title} />
+            <TitleBar title={visualization.info.title} />
             <HorizontalRule />
             <DescriptionSection
               visualization={visualization}
