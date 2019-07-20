1e2ca95655014013f70ac5cff2a0e78ada85d6c2
Curran <curran.kelleher@gmail.com>
2019-07-20 17:51:58 +0530

    Introduce icon in upper left of CodeEditor

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/styles.js
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
+import { Icon } from '../../styles';
 
 export const Wrapper = styled.div`
   width: 500px;
   border-left: 1px solid rgba(255, 255, 255, 0.5);
+  position: relative;
+`;
+
+export const CodeEditorIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 10px 10px 10px 10px;
 `;
