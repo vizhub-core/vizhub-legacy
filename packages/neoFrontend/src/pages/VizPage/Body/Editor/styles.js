e3a44fe6f2054c5d5b9ada7bd48884788295ac59
Curran <curran.kelleher@gmail.com>
2019-07-20 12:23:14 +0530

    Refactor: introduce Section component dir

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/styles.js
@@ -1,17 +1,11 @@
 import styled from 'styled-components';
-import { Clickable } from '../../../../styles';
 
 export const Wrapper = styled.div`
   width: 150px;
   color: #ffffff;
   background-color: #3d4b65;
   font-family: ${props => props.theme.defaultCodingFontFamily};
+  line-height: 1.36;
   display: flex;
   flex-direction: column;
-`;
-
-export const Section = styled(Clickable)`
-  border-left: 5px solid rgba(192, 192, 192, 0.22);
-  margin-bottom: 1px;
-  padding: 5px 15px 5px 15px;
 `;
