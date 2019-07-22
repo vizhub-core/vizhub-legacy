d2084a102842c00527f69b3805ebb71670395229
Curran <curran.kelleher@gmail.com>
2019-07-22 15:08:12 +0530

    Refactor useDimensions.

--- packages/neoFrontend/src/pages/VizPage/Body/useDimensions/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/useDimensions/index.js
@@ -1,16 +1,21 @@
-import { useCallback } from 'react';
-import { useListener } from '../useListener';
+import { useCallback, useLayoutEffect } from 'react';
+import { useListener } from '../../useListener';
 import { useWidthDetector } from './useWidthDetector';
 
 // Inspired by https://github.com/Swizec/useDimensions
-export const useDimensions = ({ wrapperRef, setDomRect, scrollerRef = {} }) => {
+export const useDimensions = ({ wrapperRef, setDomRect, scrollerRef }) => {
   // Measures the current dimensions.
   const measure = useCallback(() => {
     setDomRect(wrapperRef.current.getBoundingClientRect());
   }, [wrapperRef, setDomRect]);
 
   // Measure the dimensions on scroll (if scrollerRef provided).
-  useListener('scroll', measure, scrollerRef.current);
+  useListener('scroll', measure, scrollerRef && scrollerRef.current);
+
+  // Capture the initial dimensions before first render.
+  // This is also before the vertical scrollbar appears,
+  // but having this here avoids a flash of zero-height viz.
+  useLayoutEffect(measure, []);
 
   // Detect when width of the scroller element changes.
   // This also fires when the scrollbar is added or removed.
