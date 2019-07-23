c0ed395c7e30c83ab4f9e399aa07d6cd25be09ff
Curran <curran.kelleher@gmail.com>
2019-07-23 17:30:35 +0530

    Implement localStorage solution

--- packages/neoFrontend/src/pages/VizPage/SplitPaneResizeContext/index.js
+++ packages/neoFrontend/src/pages/VizPage/SplitPaneResizeContext/index.js
@@ -1,15 +1,27 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 
 export const SplitPaneResizeContext = createContext();
 
-// TODO local storage
-const initialWidth = window.innerWidth * 1 / 3;
+const initialWidth =
+  +localStorage.getItem('codeEditorWidth') || (window.innerWidth * 1) / 3;
 
 const reducer = (codeEditorWidth, movementClientX) =>
   codeEditorWidth + movementClientX;
 
 export const SplitPaneResizeProvider = ({ children }) => {
   const [codeEditorWidth, moveSplitPane] = useReducer(reducer, initialWidth);
+
+  useEffect(() => {
+    if (codeEditorWidth !== initialWidth) {
+      const timeout = setTimeout(() => {
+        console.log('writing ' + codeEditorWidth);
+        localStorage.setItem('codeEditorWidth', codeEditorWidth);
+      }, 800);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [codeEditorWidth]);
 
   return (
     <SplitPaneResizeContext.Provider value={{ codeEditorWidth, moveSplitPane }}>
