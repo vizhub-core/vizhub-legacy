7a41d25227b9a14ab8cfebeb41552e24241160ff
Curran <curran.kelleher@gmail.com>
2019-07-24 17:07:53 +0530

    Remove measureTwice hack

--- packages/neoFrontend/src/pages/VizPage/Body/useDimensions/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/useDimensions/index.js
@@ -15,12 +15,6 @@ export const useDimensions = ({
     setDomRect(wrapperRef.current.getBoundingClientRect());
   }, [wrapperRef, setDomRect]);
 
-  // Necessary to catch up to scrollbar appearances and disappearances.
-  const measureTwice = useCallback(() => {
-    measure();
-    requestAnimationFrame(measure);
-  }, [measure]);
-
   // Measure the dimensions on scroll (if scrollerRef provided).
   useListener('scroll', measure, scrollerRef && scrollerRef.current);
 
@@ -32,7 +26,7 @@ export const useDimensions = ({
   // Detect when width of the scroller element changes.
   // This also fires when the scrollbar is added or removed.
   useWidthDetector(scrollerRef, () => {
-    measureTwice();
+    measure();
     if (onWidthChanged) {
       onWidthChanged();
     }
