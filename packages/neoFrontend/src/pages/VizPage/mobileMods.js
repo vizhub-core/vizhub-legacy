df5ca35bfcca7db357f0ebf1dcf57589b0a80d1f
Curran <curran.kelleher@gmail.com>
2019-07-26 10:40:56 +0530

    Adopt is-mobile

--- packages/neoFrontend/src/pages/VizPage/mobileMods.js
+++ packages/neoFrontend/src/pages/VizPage/mobileMods.js
@@ -1,32 +1,33 @@
+import mobile from 'is-mobile';
 // Modifications and tweaks to support mobile experience.
 
 // Are we on mobile?
-const mobile = window.innerWidth < 500;
+const isMobile = mobile();
 
 // The identity function.
 const identity = x => x;
 
 // On mobile, don't show viewer if editor is open.
-export const modShowViewer = mobile
+export const modShowViewer = isMobile
   ? (showViewer, showEditor) => (showEditor ? false : showViewer)
   : identity;
 
 // On mobile, don't show viz runner if editor is open.
-export const modMode = mobile
+export const modMode = isMobile
   ? (mode, showEditor) => (showEditor ? 'micro' : mode)
   : identity;
 
 // On mobile, if editor is open, allow it to expand to full width.
-export const expandEditor = showEditor => mobile && showEditor;
+export const expandEditor = showEditor => isMobile && showEditor;
 
 // On mobile, don't show editor sidebar if a file is open.
-export const modShowEditor = mobile
+export const modShowEditor = isMobile
   ? (showEditor, activeFile) => (activeFile ? false : showEditor)
   : identity;
 
 // On mobile, if a file is open and you click "Close Editor",
 // it will close the active file AND the editor (sidebar).
-export const modToggleEditor = mobile
+export const modToggleEditor = isMobile
   ? (toggleEditor, closeActiveFile) => () => {
       toggleEditor();
       closeActiveFile();
