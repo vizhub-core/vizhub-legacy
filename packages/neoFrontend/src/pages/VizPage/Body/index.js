0ac00944e735fe2c4b38068957e0e2d90055aa37
Curran <curran.kelleher@gmail.com>
2019-07-26 14:11:43 +0530

    Add placeholder microconsole

--- packages/neoFrontend/src/pages/VizPage/Body/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/index.js
@@ -3,12 +3,14 @@ import { NavBar } from '../../../NavBar';
 import { ForkingContext } from '../ForkingContext';
 import { URLStateContext } from '../URLStateContext';
 import { modShowViewer, modMode } from '../mobileMods';
-import { Wrapper, Top, Bottom } from './styles';
+import { Wrapper, Top, Bottom, MicroConsole } from './styles';
 import { Head } from './Head';
 import { FullScreen } from './FullScreen';
 import { Editor } from './Editor';
 import { Viewer } from './Viewer';
 import { Mini } from './Mini';
+
+const consoleOutput = `hello world\nn = 1\nn = 2`;
 
 export const Body = () => {
   const onFork = useContext(ForkingContext);
@@ -18,15 +20,23 @@ export const Body = () => {
 
   const mod = modMode(mode, showEditor, activeFile);
   const isMini = mod === 'mini';
-  const showTopRight = mod !== 'micro';
+  const isMicro = mod === 'micro';
+  const showTopRight = !isMicro;
+  const showMicroConsole = isMicro && activeFile;
 
   return isFullScreen ? (
     <FullScreen />
   ) : (
     <Wrapper>
       <Top>
-        <NavBar showRight={showTopRight} />
-        <Head showRight={showTopRight} onFork={onFork} />
+        {showMicroConsole ? (
+          <MicroConsole>{consoleOutput}</MicroConsole>
+        ) : (
+          <>
+            <NavBar showRight={showTopRight} />
+            <Head showRight={showTopRight} onFork={onFork} />
+          </>
+        )}
       </Top>
       <Bottom>
         <Editor />
