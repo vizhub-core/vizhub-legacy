6c98b30faa2b40e0a9312b8160024e57074190ca
Curran <curran.kelleher@gmail.com>
2019-07-26 10:59:28 +0530

    Preparations for mobile console

--- packages/neoFrontend/src/pages/VizPage/Body/Head/EditorToggler/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Head/EditorToggler/index.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react';
 import { ArrowSVG } from '../../../../../svg';
 import { URLStateContext } from '../../../URLStateContext';
-import { modToggleEditor } from '../../../mobileMods';
 import { Wrapper, Text } from './styles';
 
 export const EditorToggler = () => {
-  const { showEditor, toggleEditor, closeActiveFile } = useContext(
+  const { showEditor, toggleEditor } = useContext(
     URLStateContext
   );
   return (
-    <Wrapper onClick={modToggleEditor(toggleEditor, closeActiveFile)}>
+    <Wrapper onClick={toggleEditor}>
       <ArrowSVG left={showEditor} />
       <Text>{showEditor ? 'Close Editor' : 'Open Editor'}</Text>
     </Wrapper>
