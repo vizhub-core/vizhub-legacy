cc99b5d9093b1d14981e18136661295b93f3e3dc
Curran <curran.kelleher@gmail.com>
2019-07-27 19:41:52 +0530

    Add first actual test for mobile

--- packages/neoCI/test/toggleEditor.js
+++ packages/neoCI/test/toggleEditor.js
@@ -1,9 +1,16 @@
 import assert from 'assert';
 
-export const toggleEditor = my => async () => {
-  const { page } = my;
+export const toggleEditor = (my, isMobile) => async () => {
+  const page = isMobile ? my.mobilePage : my.page;
   await (await page.waitFor('.test-toggle-editor')).click();
   await page.waitFor('.test-editor');
+
+  // The viewer should still be visible on desktop only.
+  if (isMobile) {
+    assert.equal(await page.$('.test-viewer'), null);
+  } else {
+    await page.waitFor('.test-viewer');
+  }
 
   // If we're here then the editor opened.
   // Now test that the editor remains open on page load.
