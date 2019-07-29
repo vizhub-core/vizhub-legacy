4736cd74f1fdc5a01defecb466ada3e03e9c4463
Curran <curran.kelleher@gmail.com>
2019-07-29 13:42:12 +0530

    Introduce convenience method for click and wait for navigation

--- packages/neoCI/test/toggleCodeEditor.js
+++ packages/neoCI/test/toggleCodeEditor.js
@@ -1,29 +1,33 @@
 import assert from 'assert';
 
-const awaitNavigation = async (page, callback) => {
-  const navigation = page.waitForNavigation();
-  await callback();
-  await navigation;
+//export const awaitNavigation = async (page, callback) => {
+//  const navigation = page.waitForNavigation();
+//  await callback();
+//  await navigation;
+//};
+
+const convenience = (my, isMobile) => {
+  const page = isMobile ? my.mobilePage : my.page;
+  const navClick = async selector =>
+    await Promise.all([
+      page.waitForNavigation(),
+      await (await page.waitFor(selector)).click()
+    ]);
+  return { page, navClick };
 };
 
 export const toggleCodeEditor = (my, isMobile) => async () => {
-  const page = isMobile ? my.mobilePage : my.page;
+  const { page, navClick } = convenience(my, isMobile);
 
   // Open editor.
-  awaitNavigation(page, async () => {
-    await (await page.waitFor('.test-toggle-editor')).click();
-  });
+  await navClick('.test-toggle-editor');
   await page.waitFor('.test-editor');
 
   // Open files section.
-  awaitNavigation(page, async () => {
-    await (await page.waitFor('.test-editor-files-section')).click();
-  });
+  await navClick('.test-editor-files-section');
 
   // Open a file.
-  const openNavigation = page.waitForNavigation();
-  await (await page.waitFor('.test-editor-file-entry-index-html')).click();
-  await openNavigation;
+  await navClick('.test-editor-file-entry-index-html');
 
   await page.waitFor('.test-code-editor');
   const fileName = await page.evaluate(
@@ -35,18 +39,19 @@ export const toggleCodeEditor = (my, isMobile) => async () => {
   await page.waitFor('.test-editor');
 
   // Close code editor.
-  const closeNavigation = page.waitForNavigation();
-
   // There's a strange bug that happens around 5% of the time
   // where clicking on test-close-code-editor doesn't navigate.
-  console.log('url before click: ' + page.url());
-  await (await page.waitFor('.test-close-code-editor')).click();
-
   // URL after click should be different than before click.
   // If it's not, the 5% bug has been encountered,
   // and navigation will never happen. The test suite will fail.
-  console.log('url after click:  ' + page.url());
-  await closeNavigation;
+  console.log('before closing code editor');
+
+  // This timeout here appears to make this bug go away.
+  // No idea why.
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  await navClick('.test-close-code-editor');
+  console.log('after closing code editor');
 
   // Code editor should not be visible anymore.
   assert.equal(await page.$('.test-code-editor'), null);
@@ -54,9 +59,6 @@ export const toggleCodeEditor = (my, isMobile) => async () => {
   // Editor (sidebar) should still be open at this point.
   await page.waitFor('.test-editor');
 
-  // Return to home state (wait for navigation to avoid race condition).
-  const navigation = page.waitForNavigation();
-  (await page.waitFor('.test-toggle-editor')).click();
-
-  await navigation;
+  // Return to home state.
+  await navClick('.test-toggle-editor');
 };
