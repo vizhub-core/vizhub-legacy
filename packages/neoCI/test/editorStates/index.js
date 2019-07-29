a7bdccea9b56cf02b5dcad18d992032663141803
Curran <curran.kelleher@gmail.com>
2019-07-29 16:25:57 +0530

    Add directory structure to tests

---
+++ packages/neoCI/test/editorStates/index.js
@@ -0,0 +1,31 @@
+import { toggleEditor } from './toggleEditor';
+import { toggleFullScreen } from './toggleFullScreen';
+import { toggleMini } from './toggleMini';
+import { toggleFullEditor } from './toggleFullEditor';
+import { toggleCodeEditor } from './toggleCodeEditor';
+import { verifyHomeState } from './verifyHomeState';
+import { codeEditorIndependence } from './codeEditorIndependence';
+import { openMobilePage } from './openMobilePage';
+
+export const editorStates = my => () => {
+  describe('Desktop Editor States', () => {
+    afterEach(verifyHomeState(my));
+    it('should toggle editor', toggleEditor(my));
+    it('should toggle fullscreen', toggleFullScreen(my));
+    it('should toggle mini', toggleMini(my));
+    it('should toggle code editor', toggleCodeEditor(my));
+    it('should toggle full code editor mode', toggleFullEditor(my));
+    it('should toggle code editor independently', codeEditorIndependence(my));
+  });
+
+  describe('Mobile Editor States', () => {
+    afterEach(verifyHomeState(my, true));
+    it('should open mobile page', openMobilePage(my));
+    it('should toggle editor', toggleEditor(my, true));
+    it('should toggle fullscreen', toggleFullScreen(my, true));
+    //it('should toggle mini', toggleMini(my, true));
+    // it('should toggle code editor', toggleCodeEditor(my, true));
+    //it('should toggle full code editor mode', toggleFullEditor(my));
+    //it('should toggle code editor independently', codeEditorIndependence(my));
+  });
+};
