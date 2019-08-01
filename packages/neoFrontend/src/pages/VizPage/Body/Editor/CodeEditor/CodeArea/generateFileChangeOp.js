757fd68832de974e8b4b46c3255f511c66b3c67b
Curran <curran.kelleher@gmail.com>
2019-08-01 17:07:29 +0530

    Refactoring for selection transforms

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/generateFileChangeOp.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/generateFileChangeOp.js
@@ -1,14 +1,9 @@
+import { getFileIndex } from '../../../../../../accessors';
+
 export const generateFileChangeOp = (files, name, newText, realtimeModules) => {
   // Derive old text and file index.
-  let oldText, fileIndex;
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    if (file.name === name) {
-      oldText = file.text;
-      fileIndex = i;
-      break;
-    }
-  }
+  const fileIndex = getFileIndex(files, name);
+  const oldText = files[fileIndex].text;
 
   // Derive the op for this change by diffing the text.
   const { diffMatchPatch, jsondiff } = realtimeModules;
