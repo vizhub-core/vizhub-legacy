94f8279b27463a5a07dbc91564967957dcb60bad
Curran <curran.kelleher@gmail.com>
2019-08-02 18:00:17 +0530

    Set up dedicated viz for realtime testing

--- packages/neoCI/test/realtime/saveAndRestoreChanges.js
+++ packages/neoCI/test/realtime/saveAndRestoreChanges.js
@@ -2,10 +2,10 @@ import assert from 'assert';
 //import { convenience } from './convenience';
 
 export const saveAndRestoreChanges = my => async () => {
-
-  // Open editor to index.html.
-  await navClick('.test-toggle-editor');
-  await page.waitFor('.test-editor');
-  await navClick('.test-editor-files-section');
-  await navClick('.test-editor-file-entry-index-html');
+  //const page = my
+  //// Open editor to index.html.
+  //await navClick('.test-toggle-editor');
+  //await page.waitFor('.test-editor');
+  //await navClick('.test-editor-files-section');
+  //await navClick('.test-editor-file-entry-index-html');
 };
