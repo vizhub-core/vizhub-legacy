8cc3b28d2e02018e8dfd2c5fb6adacb93ec742bd
Curran <curran.kelleher@gmail.com>
2019-08-02 17:53:07 +0530

    Refactor tests to use navClick

--- packages/neoCI/test/vizCreation/createVizFromScratch.js
+++ packages/neoCI/test/vizCreation/createVizFromScratch.js
@@ -1,15 +1,15 @@
 import assert from 'assert';
+import { navClick } from '../navClick';
 
 export const createVizFromScratch = my => async () => {
   const { page } = my;
-  const navigation = page.waitForNavigation();
-  await (await page.waitFor('.test-create-viz-from-scratch')).click();
 
   // Navigate to /creating-viz-from-scratch
-  await navigation;
+  await navClick(page, '.test-create-viz-from-scratch');
 
   // Navigate to actual new viz URL.
   await page.waitForNavigation();
+
   const url = page.url();
 
   const split = url.split('/');
