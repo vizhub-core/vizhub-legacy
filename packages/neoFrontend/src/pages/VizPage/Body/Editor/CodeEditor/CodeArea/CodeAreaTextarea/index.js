151bc3127290afb82278c51cc074ef97f3dc4b1d
Curran <curran.kelleher@gmail.com>
2019-08-06 12:30:19 +0530

    Clean up unused cursor transform logic

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaTextarea/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaTextarea/index.js
@@ -14,30 +14,13 @@ export const CodeAreaTextarea = ({ file, fileIndex, submitVizContentOp }) => {
     realtimeModules
   );
 
-  //const reducer = useCallback(
-  //  (selection, action) => {
-  //    switch (action.type) {
-  //      case 'remoteOp':
-  //        return selection.map(position =>
-  //          realtimeModules.json0.transformCursor(position, action.c, 'left')
-  //        );
-  //      case 'localChange':
-  //        return action.selection;
-  //      default:
-  //        throw new Error();
-  //    }
-  //  },
-  //  [realtimeModules]
-  //);
-
   const ref = useRef();
-  //const [selection, dispatch] = useReducer(reducer, [0, 0]);
 
+  // TODO submit presence
+  // onSelect={updateSelection}
   //const updateSelection = () => {
   //  const { selectionStart, selectionEnd } = ref.current;
-  //  const selection = [selectionStart, selectionEnd];
-  //  selection.isLocal = true;
-  //  dispatch({ type: 'localChange', selection });
+  //  console.log({selectionStart, selectionEnd});
   //};
 
   useEffect(() => {
@@ -46,60 +29,6 @@ export const CodeAreaTextarea = ({ file, fileIndex, submitVizContentOp }) => {
     ref.current.setSelectionRange(selectionStart, selectionEnd);
   }, [text, ref]);
 
-  //useEffect(() => {
-  //  if (!selection.isLocal) {
-  //    ref.current.setSelectionRange(selection[0], selection[1]);
-  //  }
-  //}, [selection, ref]);
-
-  //// Test for cursor transform.
-  //useEffect(() => {
-  //  if (!vizContentDoc) {
-  //    return;
-  //  }
-  //  document.addEventListener('keydown', e => {
-  //    if (e.altKey && e.code === 'KeyD') {
-  //      setInterval(() => {
-  //        vizContentDoc.submitOp({ si: 'e', p: ['files', 1, 'text', 5] });
-  //      }, 1000);
-  //    }
-  //  });
-  //}, [vizContentDoc]);
-
-  //useEffect(() => {
-  //  if (!vizContentDoc) {
-  //    return;
-  //  }
-
-  //  const transformCursor = (op, originatedLocally) => {
-  //    if (!originatedLocally) {
-  //      const files = vizContentDoc.data.files;
-  //      const fileIndex = getFileIndex(files, name);
-  //      const path = ['files', fileIndex, 'text'];
-  //      const { json0 } = realtimeModules;
-
-  //      // Delay execution of this so that the text gets set FIRST,
-  //      // then the selection gets set SECOND.
-  //      // This avoids a bug where if the cursor is in the last position,
-  //      // it doesn't get transformed on screen.
-  //      setTimeout(() => {
-  //        op.forEach(c => {
-  //          if (json0.canOpAffectPath(c, path)) {
-  //            dispatch({ type: 'remoteOp', c });
-  //          }
-  //        });
-  //      }, 0);
-  //    }
-  //  };
-
-  //  // Update on each change.
-  //  vizContentDoc.on('op', transformCursor);
-
-  //  return () => {
-  //    vizContentDoc.off('op', transformCursor);
-  //  };
-  //}, [vizContentDoc, name, realtimeModules]);
-
   return (
     <Wrapper
       className="test-codearea-textarea"
@@ -107,7 +36,6 @@ export const CodeAreaTextarea = ({ file, fileIndex, submitVizContentOp }) => {
       onChange={event => {
         onTextChange(event.target.value);
       }}
-      //onSelect={updateSelection}
       readOnly={!allowEditing}
     />
   );
