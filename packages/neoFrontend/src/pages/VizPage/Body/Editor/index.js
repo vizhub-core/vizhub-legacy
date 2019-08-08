a64dcfa6159e2c226899d4cc263b384cca046135
Curran <curran.kelleher@gmail.com>
2019-08-08 12:01:45 +0530

    Iterate colors

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { URLStateContext } from '../../URLStateContext';
 import { modExpandEditor, modShowEditor } from '../../../../mobileMods';
@@ -8,9 +8,19 @@ import { FilesSection } from './FilesSection';
 import { CodeEditor } from './CodeEditor';
 import { useEditorTheme } from './useEditorTheme';
 
+const defaultRotation = 0.397;
+
 export const Editor = () => {
   const { showEditor, activeFile } = useContext(URLStateContext);
-  const editorTheme = useEditorTheme();
+  const [rotation, setRotation] = useState(defaultRotation);
+  const editorTheme = useEditorTheme(rotation);
+
+  // Easter egg.
+  window.enableRotation = () => {
+    window.addEventListener('mousemove', event => {
+      setRotation(event.clientX / 1000);
+    });
+  };
 
   return (
     <ThemeProvider theme={{ editor: editorTheme }}>
@@ -19,6 +29,11 @@ export const Editor = () => {
           <Sidebar expand={modExpandEditor(showEditor)} className="test-editor">
             <Section title="visual editor" id="visual" />
             <FilesSection />
+            {rotation !== defaultRotation
+              ? editorTheme.colors.map((color, i) => (
+                  <div style={{ background: color, height: '20px' }} />
+                ))
+              : null}
           </Sidebar>
         ) : null}
         {activeFile ? <CodeEditor /> : null}
--- packages/neoFrontend/src/pages/VizPage/Body/Editor/useEditorTheme.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/useEditorTheme.js
@@ -6,7 +6,7 @@ import {
 //import * as themes from './themes';
 import { vizHub } from './themes';
 
-export const useEditorTheme = () => {
+export const useEditorTheme = rotation => {
   // TODO port PreferencesContext from vizhub2 repo
   //const { colorTheme, font, ligatures } = useContext(PreferencesContext);
   const font = defaultCodingFontFamily;
@@ -15,13 +15,13 @@ export const useEditorTheme = () => {
   return useMemo(
     () =>
       //Object.assign({}, themes[colorTheme], {
-      Object.assign({}, vizHub, {
+      Object.assign({}, vizHub(rotation), {
         font: {
           family: font + (ligatures !== 'all' ? ' Arrowized' : ''),
           size: defaultCodingFontSize,
           ligatures: ligatures !== 'none'
         }
       }),
-    [font, ligatures]
+    [font, ligatures, rotation]
   );
 };
