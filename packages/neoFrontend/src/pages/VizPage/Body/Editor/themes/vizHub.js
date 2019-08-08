20a8f69cc7b58b76c6a62ed1c462b1614667feff
Curran <curran.kelleher@gmail.com>
2019-08-08 12:01:45 +0530

    Iterate colors

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/themes/vizHub.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/themes/vizHub.js
@@ -1,56 +1,67 @@
-import { cubehelix } from 'd3-color';
+import { hcl } from 'd3-color';
 const entries = [
-  ['string', 'string2'],
   'keyword',
   'tag',
   ['definition', 'variable', 'variable2', 'variable3'],
-  'atom',
   'qualifier',
-  ['link', 'attribute', 'property'],
-  'meta'
+  ['string', 'string2'],
+  ['atom', 'number'],
+  ['link', 'attribute', 'property']
 ];
-const theme = {
-  container: {
-    color: '#fff',
-    background: 'rgba(0,0,0,0.1)'
-    //background: 'transparent'
-  },
-  //selection: { background: '#b6b6b6' },
-  selectionBackground: 'rgba(0,0,0,0.3)',
-  lineNumbers: { color: '#5c6370' },
-  //keyword: { color: '#c679de' },
-  //atom: { color: '#98c379' },
-  //property: { color: '#61afef' },
-  //number: { color: '#d19a66' },
-  comment: { color: '#ddd' },
-  //meta: { color: '#ddd' },
-  //variable: { color: '#5bafef' },
-  operator: { color: '#ffffff' },
-  number: { color: '#eee' },
-  default: { color: '#ffffff' }
-};
 
-entries.forEach((entry, i) => {
-  const t = (i / (entries.length + 1) + 0.35) % 1;
-  const color = cubehelix(t * 360, 2, 0.82);
-  const keys = typeof entry === 'string' ? [entry] : entry;
-  keys.forEach(key => {
-    theme[key] = { color: color.formatHex() };
-    if (key === 'link') {
-      theme[key].textDecoration = 'none';
-    }
+const bkg = { h: 269.346, c: 17.195, l: 31.512 };
+
+const luminance = 88;
+const saturation = 75;
+const light = hcl(0, 0, luminance).formatHex();
+const dark = hcl(bkg.h, bkg.c, 78).formatHex();
+
+export const vizHub = rotation => {
+  const theme = {
+    container: {
+      color: light,
+      selectionBackground: 'rgba(0,0,0,0.5)',
+      backgroundColor: hcl(bkg.h, bkg.c * 0.8, 10).formatHex()
+    },
+    //selection: { background: '#b6b6b6' },
+    selectionBackground: 'rgba(0,0,0,0.5)',
+    lineNumbers: { color: '#5c6370' },
+    //keyword: { color: '#c679de' },
+    //atom: { color: '#98c379' },
+    //property: { color: '#61afef' },
+    //number: { color: '#d19a66' },
+    comment: { color: dark },
+    meta: { color: dark },
+    //variable: { color: '#5bafef' },
+    operator: { color: light },
+    //number: { color: light },
+    default: { color: light }
+  };
+
+  theme.colors = entries.map((entry, i) => {
+    const t = ((i + 0) / entries.length + rotation) % 1;
+    const color = hcl(t * 360, saturation, luminance).formatHex();
+    const keys = typeof entry === 'string' ? [entry] : entry;
+    keys.forEach(key => {
+      theme[key] = { color };
+      if (key === 'link') {
+        theme[key].textDecoration = 'none';
+      }
+    });
+    return color;
   });
-});
+  theme.colors = theme.colors.concat(theme.colors[0]);
 
-theme.caretColor = 'white';
+  theme.caretColor = 'white';
 
-theme.gutter = Object.assign({}, theme.container, {
-  borderRight: '1px solid rgba(255,255,255,0.1)'
-});
+  theme.gutter = Object.assign({}, theme.container, {
+    borderRight: '1px solid rgba(255,255,255,0.1)'
+  });
 
-theme.matchingBracket = {
-  color: theme.container.color,
-  borderBottom: `1px solid ${theme.property.color}`
+  theme.matchingBracket = {
+    color: theme.container.color,
+    borderBottom: `1px solid ${theme.property.color}`
+  };
+
+  return theme;
 };
-
-export const vizHub = theme;
