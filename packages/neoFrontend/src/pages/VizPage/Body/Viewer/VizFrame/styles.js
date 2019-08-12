7103bda12713334528cf340eabe35f93f586ffd5
curran <curran.kelleher@gmail.com>
2019-08-12 18:36:48 +0530

    Auto-run indicator blue strip

--- packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
@@ -4,3 +4,15 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const RunTimerProgressIndicator = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 2px;
+  transition: opacity 2s;
+  opacity: ${props => (props.runTimerProgress === null ? 0 : 1)};
+  width: ${props =>
+    props.runTimerProgress === null ? 100 : props.runTimerProgress * 100}%;
+  background-color: #3866e9;
+`;
