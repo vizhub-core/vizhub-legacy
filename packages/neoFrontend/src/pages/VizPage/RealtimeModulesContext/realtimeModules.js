634598daa98bb9646aeec7887bde654bbc381fe1
Curran <curran.kelleher@gmail.com>
2019-08-15 18:22:06 +0530

    Reconnect WebSocket on auth user change

--- packages/neoFrontend/src/pages/VizPage/RealtimeModulesContext/realtimeModules.js
+++ packages/neoFrontend/src/pages/VizPage/RealtimeModulesContext/realtimeModules.js
@@ -1,10 +1,14 @@
 // These modules required for real-time features
 // are split out into a separate chunk and loaded dynamically.
+import ShareDB, { Connection } from '@teamwork/sharedb/lib/client';
 import { type as json0 } from '@datavis-tech/ot-json0';
 import jsondiff from 'json0-ot-diff';
 import diffMatchPatch from 'diff-match-patch';
 import produce from 'immer';
-import { connection } from './connection';
+
+// Register our custom OT type (that implements presence) as the default.
+ShareDB.types.register(json0);
+ShareDB.types.defaultType = json0;
 
 // Make json0 apply immutable.
 // Related: https://github.com/ottypes/json0/issues/26
@@ -14,4 +18,4 @@ json0.apply = (snapshot, op) =>
     originalApply(draftSnapshot, op);
   });
 
-export { json0, jsondiff, diffMatchPatch, produce, connection };
+export { json0, jsondiff, diffMatchPatch, produce, Connection };
--- packages/neoFrontend/src/pages/VizPage/index.js
+++ packages/neoFrontend/src/pages/VizPage/index.js
@@ -9,6 +9,7 @@ import { VizRunnerProvider } from './VizRunnerContext';
 import { VizProvider } from './VizContext';
 import { RunProvider } from './RunContext';
 import { RealtimeModulesProvider } from './RealtimeModulesContext';
+import { ConnectionProvider } from './ConnectionContext';
 import { EditorModulesProvider } from './EditorModulesContext';
 import { SplitPaneResizeProvider } from './SplitPaneResizeContext';
 import { Body } from './Body';
@@ -17,23 +18,25 @@ export const VizPage = () => (
   <URLStateProvider>
     <VizPageDataProvider fallback={<LoadingScreen />}>
       <RealtimeModulesProvider>
-        <EditorModulesProvider>
-          <VizProvider>
-            <RunProvider>
-              <ForkingProvider
-                fallback={<LoadingScreen message="Forking..." />}
-              >
-                <ThemeProvider theme={darkNavbarTheme}>
-                  <VizRunnerProvider>
-                    <SplitPaneResizeProvider>
-                      <Body />
-                    </SplitPaneResizeProvider>
-                  </VizRunnerProvider>
-                </ThemeProvider>
-              </ForkingProvider>
-            </RunProvider>
-          </VizProvider>
-        </EditorModulesProvider>
+        <ConnectionProvider>
+          <EditorModulesProvider>
+            <VizProvider>
+              <RunProvider>
+                <ForkingProvider
+                  fallback={<LoadingScreen message="Forking..." />}
+                >
+                  <ThemeProvider theme={darkNavbarTheme}>
+                    <VizRunnerProvider>
+                      <SplitPaneResizeProvider>
+                        <Body />
+                      </SplitPaneResizeProvider>
+                    </VizRunnerProvider>
+                  </ThemeProvider>
+                </ForkingProvider>
+              </RunProvider>
+            </VizProvider>
+          </EditorModulesProvider>
+        </ConnectionProvider>
       </RealtimeModulesProvider>
     </VizPageDataProvider>
   </URLStateProvider>
