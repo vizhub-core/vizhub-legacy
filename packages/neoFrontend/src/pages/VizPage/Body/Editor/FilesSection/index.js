c07f03b8b333cf645c2d4cba94785df2d32ca45f
Curran <curran.kelleher@gmail.com>
2019-08-17 17:56:23 +0530

    Update file list reactively

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/index.js
@@ -1,33 +1,37 @@
 import React, { useContext } from 'react';
+import { getVizFiles } from '../../../../../accessors';
+import { useValue } from '../../../../../useValue';
 import { URLStateContext } from '../../../URLStateContext';
-import { VizPageDataContext } from '../../../VizPageDataContext';
+import { VizContext } from '../../../VizContext';
 import { Section } from '../Section';
 import { FileEntry } from './styles';
 
 export const FilesSection = () => {
   const { activeFile, setActiveFile } = useContext(URLStateContext);
 
-  const { visualization } = useContext(VizPageDataContext);
-  const { files } = visualization.content;
+  const { viz$ } = useContext(VizContext);
+  const files = useValue(viz$, getVizFiles);
 
   return (
     <Section title="files" id="files" className="test-editor-files-section">
-      {files.map(file => (
-        <FileEntry
-          key={file.name}
-          isActive={file.name === activeFile}
-          onClick={() => {
-            setActiveFile(file.name);
-          }}
-          className={
-            file.name === 'index.html'
-              ? 'test-editor-file-entry-index-html'
-              : ''
-          }
-        >
-          {file.name}
-        </FileEntry>
-      ))}
+      {files
+        ? files.map(file => (
+            <FileEntry
+              key={file.name}
+              isActive={file.name === activeFile}
+              onClick={() => {
+                setActiveFile(file.name);
+              }}
+              className={
+                file.name === 'index.html'
+                  ? 'test-editor-file-entry-index-html'
+                  : ''
+              }
+            >
+              {file.name}
+            </FileEntry>
+          ))
+        : null}
     </Section>
   );
 };
