7f8adc57dc8dfce64b1b5156ab7481d786035ba3
Curran <curran.kelleher@gmail.com>
2019-08-17 13:50:16 +0530

    Got two buttons to show up and look clickable

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import { Icon } from '../../styles';
 
 export const Wrapper = styled.div`
+  display: flex;
   background-color: ${props => props.theme.editorSectionActiveColor};
+`;
+
+export const BottomButton = styled(Icon)`
+  flex: 1;
   height: 40px;
 `;
