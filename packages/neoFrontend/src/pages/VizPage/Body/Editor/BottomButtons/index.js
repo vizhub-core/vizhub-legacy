41b28d3ab3289fc79da59bd0ecf44e81b27672b9
Curran <curran.kelleher@gmail.com>
2019-08-17 14:15:46 +0530

    Use active color for bottom buttons

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Wrapper, BottomButton } from './styles';
 import { SettingsSVG } from '../../../../../svg';
 
-export const BottomButtons = () => (
-  <Wrapper>
-    <BottomButton>
-      <SettingsSVG />
-    </BottomButton>
-    <BottomButton>
-      <SettingsSVG />
-    </BottomButton>
-    <BottomButton>
-      <SettingsSVG />
-    </BottomButton>
-    <BottomButton>
-      <SettingsSVG />
-    </BottomButton>
-  </Wrapper>
-);
+const DELETE_BUTTON = 'delete';
+
+export const BottomButtons = () => {
+  const [activeButton, setActiveButton] = useState(null);
+  const onDeleteClick = () => {
+    setActiveButton(DELETE_BUTTON);
+  };
+  return (
+    <Wrapper>
+      <BottomButton>
+        <SettingsSVG />
+      </BottomButton>
+      <BottomButton>
+        <SettingsSVG />
+      </BottomButton>
+      <BottomButton>
+        <SettingsSVG />
+      </BottomButton>
+      <BottomButton
+        onClick={onDeleteClick}
+        isActive={activeButton === DELETE_BUTTON}
+      >
+        D
+      </BottomButton>
+    </Wrapper>
+  );
+};
