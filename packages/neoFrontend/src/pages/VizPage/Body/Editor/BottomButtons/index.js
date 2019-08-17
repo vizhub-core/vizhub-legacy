3292d7b9b81b9a5d78c59b344baed0b5e932e2af
Curran <curran.kelleher@gmail.com>
2019-08-17 16:56:24 +0530

    Add top of bottom for bottom buttons

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Wrapper, BottomButton, ClickableOverlay } from './styles';
+import { Wrapper, BottomButton, ClickableOverlay, Top, Bottom } from './styles';
 import { SettingsSVG } from '../../../../../svg';
 
 const DELETE_BUTTON = 'delete';
@@ -11,24 +11,32 @@ export const BottomButtons = () => {
   };
   return (
     <Wrapper>
-      <BottomButton>
-        <ClickableOverlay>
-          <SettingsSVG />
-        </ClickableOverlay>
-      </BottomButton>
-      <BottomButton>
-        <ClickableOverlay>
-          <SettingsSVG />
-        </ClickableOverlay>
-      </BottomButton>
-      <BottomButton>
-        <ClickableOverlay>
-          <SettingsSVG />
-        </ClickableOverlay>
-      </BottomButton>
-      <BottomButton isActive={activeButton === DELETE_BUTTON}>
-        <ClickableOverlay onClick={onDeleteClick}>D</ClickableOverlay>
-      </BottomButton>
+      {activeButton === DELETE_BUTTON ? (
+        <Top>Are you sure you want to delete this file?</Top>
+      ) : null}
+      <Bottom>
+        <BottomButton>
+          <ClickableOverlay>
+            <SettingsSVG />
+          </ClickableOverlay>
+        </BottomButton>
+        <BottomButton>
+          <ClickableOverlay>
+            <SettingsSVG />
+          </ClickableOverlay>
+        </BottomButton>
+        <BottomButton>
+          <ClickableOverlay>
+            <SettingsSVG />
+          </ClickableOverlay>
+        </BottomButton>
+        <BottomButton
+          isActive={activeButton === DELETE_BUTTON}
+          activeColor="#f0353d"
+        >
+          <ClickableOverlay onClick={onDeleteClick}>D</ClickableOverlay>
+        </BottomButton>
+      </Bottom>
     </Wrapper>
   );
 };
