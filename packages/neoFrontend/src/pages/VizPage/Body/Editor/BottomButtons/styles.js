8d1aa3a514e64706d0c7e0336d29ce650ad25c8d
Curran <curran.kelleher@gmail.com>
2019-08-17 14:20:02 +0530

    Solve background issues for bottom buttons

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/styles.js
@@ -5,11 +5,20 @@ export const Wrapper = styled.div`
   display: flex;
 `;
 
-export const BottomButton = styled(Icon)`
+export const BottomButton = styled.div`
   flex: 1;
   height: 40px;
   background-color: ${props =>
     props.isActive
       ? props.theme.bottomButtonBackgroundActive
       : props.theme.bottomButtonBackground};
+  position: relative;
+`;
+
+export const ClickableOverlay = styled(Icon)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `;
