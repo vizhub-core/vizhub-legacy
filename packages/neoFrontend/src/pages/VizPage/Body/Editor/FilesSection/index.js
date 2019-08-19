4eedd903d70a8694764c31bb93da86ffd2e3f86f
Curran <curran.kelleher@gmail.com>
2019-08-19 15:19:07 +0530

    Make progress on file rename UI

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/index.js
@@ -1,36 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { getVizFiles } from '../../../../../accessors';
 import { useValue } from '../../../../../useValue';
 import { URLStateContext } from '../../../URLStateContext';
 import { VizContext } from '../../../VizContext';
 import { Section } from '../Section';
-import { FileEntry } from './styles';
+import { FileEntry, EditableFileEntry } from './styles';
 
 export const FilesSection = () => {
   const { activeFile, setActiveFile } = useContext(URLStateContext);
 
+  // editableFile stores the original name, not the changed name.
+  const [editableFile, setEditableFile] = useState(null);
+
+  // editableFileNewName stores the new name, as it's being typed.
+  const [editableFileNewName, setEditableFileNewName] = useState(null);
+
   const { viz$ } = useContext(VizContext);
   const files = useValue(viz$, getVizFiles);
+
+  const onEditableFileChange = event => {
+    setEditableFileNewName(event.target.value);
+  };
 
   return (
     <Section title="files" id="files" className="test-editor-files-section">
       {files
-        ? files.map(file => (
-            <FileEntry
-              key={file.name}
-              isActive={file.name === activeFile}
-              onClick={() => {
-                setActiveFile(file.name);
-              }}
-              className={
-                file.name === 'index.html'
-                  ? 'test-editor-file-entry-index-html'
-                  : ''
-              }
-            >
-              {file.name}
-            </FileEntry>
-          ))
+        ? files.map(file =>
+            file.name === editableFile ? (
+              <EditableFileEntry
+                key={file.name}
+                type="text"
+                value={editableFileNewName}
+                onChange={onEditableFileChange}
+              />
+            ) : (
+              <FileEntry
+                key={file.name}
+                isActive={file.name === activeFile}
+                onClick={() => {
+                  if (activeFile === file.name) {
+                    setEditableFileNewName(file.name);
+                    setEditableFile(file.name);
+                  } else {
+                    setEditableFile(null);
+                    setActiveFile(file.name);
+                  }
+                }}
+                className={
+                  file.name === 'index.html'
+                    ? 'test-editor-file-entry-index-html'
+                    : ''
+                }
+              >
+                {file.name}
+              </FileEntry>
+            )
+          )
         : null}
     </Section>
   );
--- packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/styles.js
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
 import { Clickable } from '../../../../../styles';
 
-export const FileEntry = styled(Clickable)`
+const FileStyle = props => `
   display: flex;
   align-items: center;
-  height: ${props => props.theme.editorEntryHeight}px;
-  padding-left: ${props =>
-    props.theme.editorEntryHorizontalPadding +
+  height: ${props.theme.editorEntryHeight}px;
+  padding-left: ${props.theme.editorEntryHorizontalPadding +
     props.theme.editorEntryIndentation}px;
-  border-left: ${props => props.theme.editorEntryLeftBorderSize}px solid
-    ${props =>
-      props.isActive ? props.theme.editorFileActiveColor : 'transparent'};
+  border-left: ${props.theme.editorEntryLeftBorderSize}px solid
+    ${props.isActive ? props.theme.editorFileActiveColor : 'transparent'};
+`;
+
+export const FileEntry = styled(Clickable)`
+  ${FileStyle}
   text-decoration: ${props => (props.isActive ? 'underline' : 'none')};
+`;
+
+export const EditableFileEntry = styled.input`
+  ${FileStyle}
+  font-family: ${props => props.theme.defaultCodingFontFamily};
+  font-size: ${props => props.theme.defaultCodingFontSize};
+  border-top: 0;
+  border-bottom: 0;
+  border-right: 0;
+  outline: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  padding-right: 0;
+  color: ${props => props.theme.dark};
+  width: 100%;
 `;
