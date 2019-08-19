77fb4751610257419a0bd8219f79538f0819548c
Curran <curran.kelleher@gmail.com>
2019-08-19 12:01:54 +0530

    Log Rollup errors to console

--- packages/neoFrontend/src/pages/VizPage/RunContext/updateBundleIfNeeded.js
+++ packages/neoFrontend/src/pages/VizPage/RunContext/updateBundleIfNeeded.js
@@ -12,37 +12,28 @@ export const updateBundleIfNeeded = async (
     const files = getVizFiles(viz$.getValue());
     const indexJSExists = getFileIndex(files, 'index.js') !== -1;
     if (indexJSExists) {
-      try {
-        const output = await editorModules.bundle(files);
-        if (output.length !== 1) {
-          throw new Error(
-            'Expected Rollup output to contain exactly one file.'
-          );
+      const output = await editorModules.bundle(files);
+      if (output.length !== 1) {
+        throw new Error('Expected Rollup output to contain exactly one file.');
+      }
+      const { text } = output[0];
+      const fileIndex = getFileIndex(files, 'bundle.js');
+      const bundleJSExists = fileIndex !== -1;
+      if (bundleJSExists) {
+        const oldText = files[fileIndex].text;
+        const op = generateFileChangeOp(
+          fileIndex,
+          oldText,
+          text,
+          realtimeModules
+        );
+        if (op.length !== 0) {
+          submitVizContentOp(op);
         }
-        const { text } = output[0];
-        const fileIndex = getFileIndex(files, 'bundle.js');
-        const bundleJSExists = fileIndex !== -1;
-        if (bundleJSExists) {
-          const oldText = files[fileIndex].text;
-          const op = generateFileChangeOp(
-            fileIndex,
-            oldText,
-            text,
-            realtimeModules
-          );
-          if (op.length !== 0) {
-            submitVizContentOp(op);
-          }
-        } else {
-          submitVizContentOp(
-            generateFileCreateOp(files, { name: 'bundle.js', text })
-          );
-        }
-      } catch (error) {
-        // TODO proper handling of errors.
-        // Display them where viz should be.
-        // Also output to console.
-        console.log(error);
+      } else {
+        submitVizContentOp(
+          generateFileCreateOp(files, { name: 'bundle.js', text })
+        );
       }
     } else {
       // TODO delete bundle.js
