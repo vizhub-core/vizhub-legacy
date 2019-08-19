3ae28a0380e28357159a667e08023f1ed086b8ac
Curran <curran.kelleher@gmail.com>
2019-08-19 10:46:24 +0530

    Fix error re: 200 classes

--- packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
@@ -5,14 +5,18 @@ export const Wrapper = styled.div`
   flex-direction: column;
 `;
 
-export const RunTimerProgressIndicator = styled.div`
+export const RunTimerProgressIndicator = styled.div.attrs(props => ({
+  style: {
+    width:
+      (props.runTimerProgress === null ? 100 : props.runTimerProgress * 100) +
+      '%'
+  }
+}))`
   position: absolute;
   top: 0;
   left: 0;
   height: 2px;
   transition: opacity 2s;
   opacity: ${props => (props.runTimerProgress === null ? 0 : 1)};
-  width: ${props =>
-    props.runTimerProgress === null ? 100 : props.runTimerProgress * 100}%;
   background-color: #3866e9;
 `;
