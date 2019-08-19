97fae41174b821d07eda111ab18984e54a9b2043
Curran <curran.kelleher@gmail.com>
2019-08-19 19:49:58 +0530

    Start on UI for new file

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/DeleteTop.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/DeleteTop.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { ClickableOverlay, TopMessage, TopOptions, TopOption } from './styles';
-export const DeleteTop = ({ onNoClick, onDeleteConfirm, theme }) => (
+export const DeleteTop = ({
+  onNoClick,
+  onDeleteConfirm,
+  theme,
+  activeFile
+}) => (
   <>
-    <TopMessage>Are you sure you want to delete this file?</TopMessage>
+    <TopMessage>
+      Are you sure you want to delete the file {activeFile}?
+    </TopMessage>
     <TopOptions>
       <TopOption>
         <ClickableOverlay onClick={onNoClick}>no</ClickableOverlay>
