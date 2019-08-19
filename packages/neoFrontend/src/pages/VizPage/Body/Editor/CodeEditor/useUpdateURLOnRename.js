3e8fb46a2326035be9f9c13616c0f888cf82d450
Curran <curran.kelleher@gmail.com>
2019-08-19 17:09:55 +0530

    Fix bug where content changes were closing files

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/useUpdateURLOnRename.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/useUpdateURLOnRename.js
@@ -1,28 +1,31 @@
 import { useContext, useEffect } from 'react';
 import { getFileIndex } from '../../../../../accessors';
 import { URLStateContext } from '../../../URLStateContext';
-import { RealtimeModulesContext } from '../../../RealtimeModulesContext';
 import { VizContext } from '../../../VizContext';
 import { getPath } from './usePath';
+
+const affectsPath = (path, opPath) => {
+  for (let i = 0; i < path.length; i++) {
+    if (opPath[i] !== path[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 // When the active file is renamed,
 // we need to change the URL to reflect the new name.
 export const useUpdateURLOnRename = () => {
   const { vizContentOp$ } = useContext(VizContext);
   const { activeFile, setActiveFile } = useContext(URLStateContext);
-  const realtimeModules = useContext(RealtimeModulesContext);
 
   useEffect(() => {
-    if (!realtimeModules) {
-      return;
-    }
-    const { json0 } = realtimeModules;
     const subscription = vizContentOp$.subscribe(
       ({ previousContent, nextContent, op }) => {
         const fileIndex = getFileIndex(previousContent.files, activeFile);
         const path = getPath(fileIndex, 'name');
         op.forEach(c => {
-          if (json0.canOpAffectPath(c, path)) {
+          if (affectsPath(path, c.p)) {
             const newName = nextContent.files[fileIndex].name;
             setActiveFile(newName);
           }
@@ -32,5 +35,5 @@ export const useUpdateURLOnRename = () => {
     return () => {
       subscription.unsubscribe();
     };
-  }, [realtimeModules, vizContentOp$, activeFile]);
+  }, [vizContentOp$, activeFile, setActiveFile]);
 };
