28c9cf2ea63f025f72321bc9061cf7770bfd86a5
curran <curran.kelleher@gmail.com>
2019-08-20 18:11:34 +0530

    Iterate profile page layout

--- packages/neoFrontend/src/pages/ProfilePage/Body/styles.js
+++ packages/neoFrontend/src/pages/ProfilePage/Body/styles.js
@@ -1,24 +1,28 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-// Inspired by https://vega.github.io/vega/
-export const VizPreview = styled(Link)`
-  display: inline-block;
-  margin: 10px;
-  width: 230px;
+export const VizPreviews = styled.div`
+  display: flex;
 `;
 
-export const VizPreviewImage = styled.img`
-  width: 252px;
-  height: 150px;
+// Inspired by https://vega.github.io/vega/
+export const VizPreview = styled(Link)`
+  margin: 30px;
+`;
+
+export const VizPreviewImage = styled.div`
+  border: 0px;
+  width: 230px;
+  height: 120px;
   padding: 0px;
   background-position: left top;
   background-size: cover;
   overflow: hidden;
   position: relative;
   transition: background-position 3s;
-  outline: 1px solid #ddd;
   ${VizPreview}:hover & {
     background-position: right bottom;
   }
+  box-shadow: ${props => props.theme.shadow};
+  border-radius: 6px;
 `;
