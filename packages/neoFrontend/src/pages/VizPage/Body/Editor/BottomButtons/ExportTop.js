ac6bc4f171f944da9cea9fd6ed0170b4c014c8ff
curran <curran.kelleher@gmail.com>
2019-08-20 15:38:08 +0530

    Add export button UI

---
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/ExportTop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TopList, TopListItem } from './styles';
+
+export const ExportTop = () => (
+  <TopList>
+    <TopListItem>export</TopListItem>
+  </TopList>
+);
+// <TopListItem>new directory</TopListItem>
