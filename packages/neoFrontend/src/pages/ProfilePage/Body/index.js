7b871faa08c7227c8219167e7848a00c46e7b800
curran <curran.kelleher@gmail.com>
2019-08-20 18:11:34 +0530

    Iterate profile page layout

--- packages/neoFrontend/src/pages/ProfilePage/Body/index.js
+++ packages/neoFrontend/src/pages/ProfilePage/Body/index.js
@@ -1,18 +1,31 @@
 import React, { useContext } from 'react';
 import { ProfilePageDataContext } from '../ProfilePageDataContext';
-import { VizPreview, VizPreviewImage } from './styles';
+import { NavBar } from '../../../NavBar';
+import { Wrapper, Content } from '../../styles';
+import { VizPreviews, VizPreview, VizPreviewImage } from './styles';
 
 export const Body = () => {
   const profilePageData = useContext(ProfilePageDataContext);
   const { user, visualizationInfos } = profilePageData;
   const { userName } = user;
   console.log(JSON.stringify(profilePageData, null, 2));
-  return visualizationInfos.map(({ id }) => {
-    const url = `/api/visualization/thumbnail/${id}.png`;
-    return (
-      <VizPreview key={id} to={`/${userName}/${id}`}>
-        <VizPreviewImage style={{ backgroundImage: `url(${url})` }} />
-      </VizPreview>
-    );
-  });
+
+  return (
+    <Wrapper>
+      <Content>
+        <NavBar />
+        <VizPreviews>
+          {visualizationInfos.map(({ id }) => (
+            <VizPreview key={id} to={`/${userName}/${id}`}>
+              <VizPreviewImage
+                style={{
+                  backgroundImage: `url(/api/visualization/thumbnail/${id}.png)`
+                }}
+              />
+            </VizPreview>
+          ))}
+        </VizPreviews>
+      </Content>
+    </Wrapper>
+  );
 };
