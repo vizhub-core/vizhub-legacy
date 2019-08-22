a77dfb741d911b4361264fdd929837cd8c2d7485
Curran <curran.kelleher@gmail.com>
2019-08-22 16:34:44 +0530

    Use Sublime Key map

--- packages/neoFrontend/src/pages/VizPage/EditorModulesContext/editorModules.js
+++ packages/neoFrontend/src/pages/VizPage/EditorModulesContext/editorModules.js
@@ -8,14 +8,14 @@ import 'codemirror/mode/markdown/markdown';
 import 'codemirror/keymap/vim';
 
 import 'codemirror/addon/search/searchcursor';
-import 'codemirror/addon/search/search';
-import 'codemirror/addon/dialog/dialog';
+//import 'codemirror/addon/search/search';
+//import 'codemirror/addon/dialog/dialog';
 import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/edit/closebrackets';
+//import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/addon/comment/comment';
-import 'codemirror/addon/wrap/hardwrap';
-import 'codemirror/addon/fold/foldcode';
-import 'codemirror/addon/fold/brace-fold';
+//import 'codemirror/addon/wrap/hardwrap';
+//import 'codemirror/addon/fold/foldcode';
+//import 'codemirror/addon/fold/brace-fold';
 import 'codemirror/keymap/sublime';
 
 import { bundle } from 'vizhub-presenters';
