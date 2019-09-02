f47e287ede3b63ef7338611f47d1128f944736e3
Curran <curran.kelleher@gmail.com>
2019-09-02 16:48:16 +0530

    Make viz footer buttons clickable again in mini mode. Closes #121

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import { Z_NEW_STACKING_CONTEXT } from '../../../../../../../styles';
 
 export const Wrapper = styled.div`
   flex: 1;
   font-family: ${props => props.theme.defaultCodingFontFamily};
   position: relative;
   min-height: 0px;
+  z-index: ${Z_NEW_STACKING_CONTEXT};
 `;
