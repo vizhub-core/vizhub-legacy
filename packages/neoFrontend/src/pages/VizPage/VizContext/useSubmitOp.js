06f34cac723e3cc6b2d7e40b2f6977ee64604321
Curran <curran.kelleher@gmail.com>
2019-09-02 15:00:07 +0530

    Use diff-based approach to generating ops from CodeMirror editing. Closes #111. Closes #83.

--- packages/neoFrontend/src/pages/VizPage/VizContext/useSubmitOp.js
+++ packages/neoFrontend/src/pages/VizPage/VizContext/useSubmitOp.js
@@ -2,7 +2,11 @@ import { useMemo } from 'react';
 export const useSubmitOp = shareDBDoc =>
   useMemo(() => {
     if (shareDBDoc) {
-      return op => shareDBDoc.submitOp(op);
+      return op => {
+        if (op.length > 0) {
+          shareDBDoc.submitOp(op);
+        }
+      };
     }
     return undefined;
   }, [shareDBDoc]);
