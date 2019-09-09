1537040401c669c5fd1a0fb35bdb2aebd40e7dcc
Curran <curran.kelleher@gmail.com>
2019-09-09 16:28:25 +0530

    Flesh out language parsers for Prettier

--- packages/neoFrontend/src/pages/VizPage/PrettierContext/prettierModules.js
+++ packages/neoFrontend/src/pages/VizPage/PrettierContext/prettierModules.js
@@ -1,4 +1,9 @@
 import prettier from 'prettier/standalone';
 import parserBabylon from 'prettier/parser-babylon';
+import parserHTML from 'prettier/parser-html';
+import parserCSS from 'prettier/parser-postcss';
+import parserMD from 'prettier/parser-markdown';
 
-export { prettier, parserBabylon };
+export const plugins = [parserBabylon, parserHTML, parserCSS, parserMD];
+
+export { prettier };
