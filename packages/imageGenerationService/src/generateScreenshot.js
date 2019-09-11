1d2855880f5ff41822c42f6dfca52a5f93543182
Ubuntu <ubuntu@ip-172-31-25-189.ec2.internal>
2019-09-11 10:10:14 +0000

    Exit image generation service process on error

--- packages/imageGenerationService/src/generateScreenshot.js
+++ packages/imageGenerationService/src/generateScreenshot.js
@@ -5,17 +5,22 @@ export const generateScreenshot = async ({ visualizationViewModel, waitTime }) =
   const { width, height, files } = visualizationViewModel;
 
   const html = computeSrcDoc(files);
+  try {
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.setViewport({ width, height });
-  await page.setContent(html);
-  await page.waitFor(waitTime);
-
-  const screenshotBuffer = await page.screenshot();
-  await page.close();
-  await browser.close();
-
-  return screenshotBuffer;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    await page.setViewport({ width, height });
+    await page.setContent(html);
+    await page.waitFor(waitTime);
+  
+    const screenshotBuffer = await page.screenshot();
+    await page.close();
+    await browser.close();
+  
+    return screenshotBuffer;
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
 };
