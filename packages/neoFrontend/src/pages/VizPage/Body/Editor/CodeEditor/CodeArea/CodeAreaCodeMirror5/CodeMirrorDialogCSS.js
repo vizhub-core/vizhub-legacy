e8026fc7c7a59de33014cd5fdebff7a246d1fae9
Curran <curran.kelleher@gmail.com>
2019-09-17 13:13:40 +0530

    Custom button style for search & replace

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/CodeMirrorDialogCSS.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/CodeMirrorDialogCSS.js
@@ -34,6 +34,13 @@ export const CodeMirrorDialogCSS = `
 
 .CodeMirror-dialog button {
   font-size: 70%;
+  background-color: transparent;
+  border: solid 1px white;
+  color: white;
+  border-radius: 3px;
+  margin-left: 5px;
+  margin-right: 5px;
+  outline: none;
 }
 .CodeMirror-search-label {
   color: white;
