f81362d0719cc3182ca762c0c0fca9885c70aff4
Curran <curran.kelleher@gmail.com>
2019-09-27 14:05:17 +0530

    Got first directory test to pass

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/getFileTree.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/getFileTree.js
@@ -2,22 +2,23 @@ export const getFileTree = files => {
   const tree = { name: 'files' };
   files.forEach(file => {
     const path = file.name.split('/');
-    const node = tree;
     const n = path.length;
-    for (let i = 0; i < n; i++) {
-      (node.children || (node.children = [])).push({
-        name: path[i],
-        file
-      });
-      //const pathItem = path[i];
-      //if (!node[pathItem]) {
-      //  const child ={name: pathItem};
-      //  if(i === n - 1){
-      //    child.data = file;
-      //  }
-      //}
-      //node = node[pathItem];
+    let node = tree;
+    for (let i = 0; i < n - 1; i++) {
+      const child = { name: path[i] };
+      (node.children || (node.children = [])).push(child);
+    //  if (!node[pathItem]) {
+    //    const child ={name: pathItem};
+    //    if(i === n - 1){
+    //      child.data = file;
+    //    }
+    //  }
+      node = child;
     }
+    (node.children || (node.children = [])).push({
+      name: path[n - 1],
+      file
+    });
   });
   return tree;
 };
