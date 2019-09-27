704601cf775e9062a60e7eb120281d3e298c9b3c
Curran <curran.kelleher@gmail.com>
2019-09-27 14:00:10 +0530

    Add test for two files

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/getFileTree.test.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/getFileTree.test.js
@@ -1,5 +1,20 @@
 import { getFileTree } from './getFileTree';
+
 test('Parses a single file', () => {
-  const tree = getFileTree([{ name: 'name', text: 'text' }]);
+  const file = { name: 'name', text: 'text' };
+  const tree = getFileTree([file]);
   expect(tree.children.length).toBe(1);
+  expect(tree.children[0].name).toBe('name');
+  expect(tree.children[0].file).toBe(file);
+});
+
+test('Parses two files', () => {
+  const file1 = { name: 'name1', text: 'text1' };
+  const file2 = { name: 'name2', text: 'text2' };
+  const tree = getFileTree([file1, file2]);
+  expect(tree.children.length).toBe(2);
+  expect(tree.children[0].name).toBe('name1');
+  expect(tree.children[0].file).toBe(file1);
+  expect(tree.children[1].name).toBe('name2');
+  expect(tree.children[1].file).toBe(file2);
 });
