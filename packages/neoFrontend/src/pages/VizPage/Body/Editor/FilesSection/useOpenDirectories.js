a1e86a09d18860a92934ecd52ebf90c701528e58
Curran <curran.kelleher@gmail.com>
2019-09-28 15:46:03 +0530

    Split out useOpenDirectories

---
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/FilesSection/useOpenDirectories.js
@@ -0,0 +1,20 @@
+import { useState, useCallback } from 'react';
+import { initialOpenDirectories } from './initialOpenDirectories';
+
+export const useOpenDirectories = activeFile => {
+  const [openDirectories, setOpenDirectories] = useState(
+    initialOpenDirectories(activeFile)
+  );
+
+  const toggleDirectory = useCallback(
+    directory => {
+      setOpenDirectories(
+        Object.assign({}, openDirectories, {
+          [directory]: !openDirectories[directory]
+        })
+      );
+    },
+    [openDirectories, setOpenDirectories]
+  );
+  return { openDirectories, toggleDirectory };
+};
