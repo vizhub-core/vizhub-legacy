d13386756460f11c86d1dc8f3fd7661ff142ac41
Curran <curran.kelleher@gmail.com>
2019-12-09 02:49:16 +0530

    Open own vizzes with files open

--- packages/neoFrontend/src/pages/ProfilePage/Body/index.js
+++ packages/neoFrontend/src/pages/ProfilePage/Body/index.js
@@ -17,7 +17,7 @@ export const Body = () => {
           {visualizationInfos.map(({ id, title }) => (
             <VizPreview
               key={id}
-              to={`/${userName}/${id}`}
+              to={`/${userName}/${id}?edit=files`}
               title={title}
               style={{
                 backgroundImage: `url(/api/visualization/thumbnail/${id}.png)`
