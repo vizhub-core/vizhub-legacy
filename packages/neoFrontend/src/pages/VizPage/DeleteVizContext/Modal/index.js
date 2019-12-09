c94d7f5f067ba41c86da091b8fea062a0eebaab9
Curran <curran.kelleher@gmail.com>
2019-12-09 07:44:42 +0530

    Roughly place X icon in modal

--- packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/index.js
+++ packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Wrapper, Box } from './styles';
+import { Wrapper, Box, CloseButton } from './styles';
+import { CloseSVG } from '../../../../svg';
+import { Icon } from '../../Body/styles';
 
 // Inspired by:
 // https://wecodetheweb.com/2019/03/02/easy-modals-with-react-hooks/
 
 export const Modal = ({ children, onClose }) => (
-  <Wrapper onClick={onClose}><Box>{children}</Box></Wrapper>
+  <Wrapper onClick={onClose}>
+    <Box>
+      <Icon onClick={onClose}>
+        <CloseSVG />
+      </Icon>
+      {children}
+    </Box>
+  </Wrapper>
 );
