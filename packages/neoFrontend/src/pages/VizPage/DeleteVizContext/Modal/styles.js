d6965b44bb5a69f5fcaffa0aa1610848e7e5d2be
Curran <curran.kelleher@gmail.com>
2019-12-09 07:26:43 +0530

    Add modal box

--- packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/styles.js
+++ packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/styles.js
@@ -8,4 +8,17 @@ export const Wrapper = styled.div`
   left: 0;
   background-color: rgba(22, 21, 20, 0.46);
   z-index: ${Z_WAY_WAY_ABOVE};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Box = styled.div`
+  flex: 1;
+  max-width: 600px;
+  border-radius: 6px;
+  background-color: white;
+  margin: 10px 10px 10px 10px;
+  padding: 30px 10px 40px 10px;
+  box-shadow: ${props => props.theme.shadow};
 `;
