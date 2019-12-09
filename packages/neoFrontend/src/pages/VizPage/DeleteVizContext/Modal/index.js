c11c5bcedba7a84de9d40662f8e4a631dc75b1eb
Curran <curran.kelleher@gmail.com>
2019-12-09 07:26:43 +0530

    Add modal box

--- packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/index.js
+++ packages/neoFrontend/src/pages/VizPage/DeleteVizContext/Modal/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import { Wrapper, Box } from './styles';
 
 // Inspired by:
 // https://wecodetheweb.com/2019/03/02/easy-modals-with-react-hooks/
 
 export const Modal = ({ children, onClose }) => (
-  <Wrapper onClick={onClose}>{children}</Wrapper>
+  <Wrapper onClick={onClose}><Box>{children}</Box></Wrapper>
 );
