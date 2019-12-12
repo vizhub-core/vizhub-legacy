299acfafa24fd0efc0374ffa1c53390e07ee18fc
Curran <curran.kelleher@gmail.com>
2019-12-12 10:29:29 +0530

    Infra for home page infinite scroll

--- packages/neoFrontend/src/pages/HomePage/useHomePageData.js
+++ packages/neoFrontend/src/pages/HomePage/useHomePageData.js
@@ -1,13 +1,33 @@
-import { useState, useEffect } from 'react';
-import { waitForSpinner } from '../../LoadingScreen';
+import { useState, useEffect, useCallback } from 'react';
 import { fetchHomePageData } from './fetchHomePageData';
 
 export const useHomePageData = () => {
-  const [homePageData, setHomePageData] = useState(null);
-  useEffect(() => {
-    waitForSpinner(fetchHomePageData()).then(data => {
-      setHomePageData(data.reverse());
+  const [homePageVisualizationInfos, setHomePageVisualizationInfos] = useState(
+    []
+  );
+  const [isFetchingNextPage, setIsFetchingNextPage] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const fetchNextPage = useCallback(() => {
+    const nextPage = currentPage + 1;
+    setIsFetchingNextPage(true);
+    fetchHomePageData(nextPage).then(data => {
+      console.log(data);
+      setHomePageVisualizationInfos(homePageVisualizationInfos.concat(data));
+      setCurrentPage(nextPage);
+      setIsFetchingNextPage(false);
     });
-  }, []);
-  return homePageData;
+    setCurrentPage(nextPage);
+  }, [currentPage, homePageVisualizationInfos]);
+
+  // Fetch the first page of visualizations.
+  useEffect(() => {
+    if (currentPage === 0) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, currentPage]);
+
+  return homePageVisualizationInfos.length
+    ? { homePageVisualizationInfos, fetchNextPage, isFetchingNextPage }
+    : null;
 };
