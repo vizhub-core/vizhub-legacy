9308f2f9bddcddb1dca8250446f28bd204c660a8
Curran <curran.kelleher@gmail.com>
2019-12-14 09:24:51 +0530

    Show privacy entry in bottom buttons settings

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/SettingsTop.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/SettingsTop.js
@@ -3,7 +3,6 @@ import { TopList, TopListItem } from './styles';
 
 export const SettingsTop = () => (
   <TopList>
-    <TopListItem>height</TopListItem>
-    <TopListItem>anyone can edit</TopListItem>
+    <TopListItem>privacy</TopListItem>
   </TopList>
 );
