134a1f959e5b35a2e4b551ba8aed8079da5bb0ee
Curran <curran.kelleher@gmail.com>
2019-12-14 15:30:58 +0530

    Finesse modal UX

--- packages/neoFrontend/src/Modal/index.js
+++ packages/neoFrontend/src/Modal/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { CloseSVG } from '../svg';
 import { Wrapper, Box, CloseIcon, Message } from './styles';
 
+const stopPropagation = event => event.stopPropagation();
+
 export const Modal = ({ children, onClose }) => (
   <Wrapper onClick={onClose}>
-    <Box>
+    <Box onClick={stopPropagation}>
       <CloseIcon onClick={onClose} rightmost={true} leftmost={true}>
         <CloseSVG />
       </CloseIcon>
