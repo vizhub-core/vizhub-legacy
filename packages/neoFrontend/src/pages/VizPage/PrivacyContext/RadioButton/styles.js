4ef5c55fee019ba114116f2625f02d44388173e6
Curran <curran.kelleher@gmail.com>
2019-12-14 12:14:40 +0530

    Finesse visual design for privacy modal

--- packages/neoFrontend/src/pages/VizPage/PrivacyContext/RadioButton/styles.js
+++ packages/neoFrontend/src/pages/VizPage/PrivacyContext/RadioButton/styles.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  cursor: 'pointer';
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+export const Label = styled.div`
+  margin-left: 5px;
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
