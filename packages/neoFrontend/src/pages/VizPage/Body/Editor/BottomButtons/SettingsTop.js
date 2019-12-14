1678b89c5f9ddd614126922ed19bb60ae4976e30
Curran <curran.kelleher@gmail.com>
2019-12-14 09:49:12 +0530

    Show privacy modal

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/SettingsTop.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/BottomButtons/SettingsTop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TopList, TopListItem } from './styles';
 
-export const SettingsTop = ({onPrivacyClick}) => (
+export const SettingsTop = ({ onPrivacyClick }) => (
   <TopList>
     <TopListItem onClick={onPrivacyClick}>privacy</TopListItem>
   </TopList>
