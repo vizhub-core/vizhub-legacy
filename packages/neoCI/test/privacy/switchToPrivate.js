fb20f4259e1b334d3895f357e7e963befe93ab94
Curran <curran.kelleher@gmail.com>
2019-12-17 12:14:57 +0530

    Add sign out test

--- packages/neoCI/test/privacy/switchToPrivate.js
+++ packages/neoCI/test/privacy/switchToPrivate.js
@@ -30,4 +30,7 @@ export const switchToPrivate = my => async () => {
     privateRadioButton
   );
   assert.equal(privateRadioButtonIsActive, 'true');
+
+  // Wait some time to ensure the change was passed to backend fia WebSocket.
+  await new Promise(resolve => setTimeout(resolve, 100));
 };
--- packages/neoFrontend/src/Modal/index.js
+++ packages/neoFrontend/src/Modal/index.js
@@ -4,10 +4,10 @@ import { Wrapper, Box, CloseIcon, Message } from './styles';
 
 const stopPropagation = event => event.stopPropagation();
 
-export const Modal = ({ children, onClose }) => (
+export const Modal = ({ children, onClose, closeButtonClassName = null }) => (
   <Wrapper onClick={onClose}>
     <Box onClick={stopPropagation}>
-      <CloseIcon onClick={onClose} rightmost={true} leftmost={true}>
+      <CloseIcon onClick={onClose} rightmost={true} leftmost={true} className={}>
         <CloseSVG />
       </CloseIcon>
       {children}
