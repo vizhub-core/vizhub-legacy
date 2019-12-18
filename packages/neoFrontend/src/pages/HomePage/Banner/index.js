16004e10e5d54f8e36c7d9cc515c83e1b79bdd8b
Curran <curran.kelleher@gmail.com>
2019-12-18 16:25:27 +0530

    First pass at layout

--- packages/neoFrontend/src/pages/HomePage/Banner/index.js
+++ packages/neoFrontend/src/pages/HomePage/Banner/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Wrapper, Bar } from './styles';
+import { Wrapper, Bar, Left, Right } from './styles';
 export const Banner = () => (
   <Wrapper>
     <Bar></Bar>
+    <Left>Teach, learn, & practice</Left>
+    <Right>Video</Right>
   </Wrapper>
 );
