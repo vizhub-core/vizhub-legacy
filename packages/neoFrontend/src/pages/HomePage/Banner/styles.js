59eb47d3ab42958f7657b3e0abfa61dc1dab3b1e
Curran <curran.kelleher@gmail.com>
2019-12-18 16:25:27 +0530

    First pass at layout

--- packages/neoFrontend/src/pages/HomePage/Banner/styles.js
+++ packages/neoFrontend/src/pages/HomePage/Banner/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
+import { Z_BELOW } from '../../../styles';
 
 export const Wrapper = styled.div`
   display: flex;
   height: 400px;
   position: relative;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
 `;
 
 export const Bar = styled.div`
@@ -15,5 +19,17 @@ export const Bar = styled.div`
   border-radius: 3px;
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.25);
   background-image: linear-gradient(to bottom, #55627c, #3d4b65);
-}
+  z-index: ${Z_BELOW};
+`;
+
+export const Left = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
 `;
