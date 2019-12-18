3f33dcca554d89e3c5cc0cae5151ed6b7103976d
Curran <curran.kelleher@gmail.com>
2019-12-18 09:47:31 +0530

    Redirect users from 'beta.vizhub.com' to 'vizhub.com'

--- packages/neoFrontend/src/index.js
+++ packages/neoFrontend/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-// A place to put feature previews and easter eggs.
-window.vizhub = {};
+// Redirect users from 'beta.vizhub.com' to 'vizhub.com'.
+if (window.location.host === 'beta.vizhub.com') {
+  window.location = window.location.href.replace(
+    'beta.vizhub.com',
+    'vizhub.com'
+  );
+} else {
 
-// Wait for prefetched fonts.
-window.onload = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-};
+  // A place to put feature previews and easter eggs.
+  window.vizhub = {};
+
+  // Wait for prefetched fonts.
+  window.onload = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+  };
+}
