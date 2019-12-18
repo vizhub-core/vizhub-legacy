3bac851f4bf360a3ae9f17ab917d3a2cc054f5f8
Curran <curran.kelleher@gmail.com>
2019-12-18 16:13:18 +0530

    Got bar to show up

--- packages/neoFrontend/src/pages/HomePage/Banner/styles.js
+++ packages/neoFrontend/src/pages/HomePage/Banner/styles.js
@@ -10,6 +10,8 @@ export const Bar = styled.div`
   position: absolute;
   top: 30px;
   bottom: 30px;
+  right: 0;
+  left: 0;
   border-radius: 3px;
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.25);
   background-image: linear-gradient(to bottom, #55627c, #3d4b65);
