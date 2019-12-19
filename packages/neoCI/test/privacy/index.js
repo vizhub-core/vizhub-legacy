057a38a71b820a63087be077841db9692b71531e
Curran <curran.kelleher@gmail.com>
2019-12-19 10:01:55 +0530

    Add failing test for not showing private viz

--- packages/neoCI/test/privacy/index.js
+++ packages/neoCI/test/privacy/index.js
@@ -1,11 +1,13 @@
+import { signOut } from '../authentication';
 import { setupPrivateViz } from './setupPrivateViz';
 import { switchToPrivate } from './switchToPrivate';
-import { signOut } from '../authentication';
+import { vizNotFound } from './vizNotFound';
 
 export const privacy = my => () => {
   before(setupPrivateViz(my));
 
   it('should switch from public to private', switchToPrivate(my));
   it('should sign out', signOut(my));
+  it('should display viz not found if not authenticated', vizNotFound(my));
 
 };
