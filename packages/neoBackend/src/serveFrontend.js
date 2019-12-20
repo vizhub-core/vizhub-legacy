2a026821424a731580988e2934cc1376fef1b406
Curran <curran.kelleher@gmail.com>
2019-12-20 17:06:47 +0530

    WIP access control for reads

--- packages/neoBackend/src/serveFrontend.js
+++ packages/neoBackend/src/serveFrontend.js
@@ -5,9 +5,11 @@ import path from 'path';
 // Serve the frontend build for production deployment.
 // Assumes that `npm run build` has been run in the frontend.
 export const serveFrontend = app => {
-  app.use(express.static(path.join(__dirname, '..', 'build'), {
-    maxAge: '2 days'
-  }));
+  app.use(
+    express.static(path.join(__dirname, '..', 'build'), {
+      maxAge: '2 days'
+    })
+  );
 
   // Always serve index.html, let React Router take care of the rest.
   app.get('*', (req, res) => {
--- packages/neoBackend/src/serveShareDB/shareDBMiddleware/getVizInfo.js
+++ packages/neoBackend/src/serveShareDB/shareDBMiddleware/getVizInfo.js
@@ -7,22 +7,33 @@ import { VisualizationInfo } from 'vizhub-entities';
 
 // Populates request.vizInfo.
 export const getVizInfo = connection => (request, done) => {
-  const { collection, snapshot, op, id } = request;
+  const { collection, snapshot, snapshots, op, id } = request;
 
   // Do nothing in the case of create and delete ops.
-  if (op.create || op.del) {
+  if (op && (op.create || op.del)) {
     return done();
   }
 
   // Query for viz info in case of op agains a viz content document.
   if (collection === DOCUMENT_CONTENT) {
     // Query for corresponding info document
-    fetchShareDBDoc(DOCUMENT_INFO, id, connection).then(infoDoc => {
+    // TODO work on the case of fetching multiple documents.
+    const primaryId = id || snapshots[0].id;
+    fetchShareDBDoc(DOCUMENT_INFO, primaryId, connection).then(infoDoc => {
       request.vizInfo = new VisualizationInfo(infoDoc.data);
       done();
     });
-  } else {
+  } else if (collection === DOCUMENT_INFO){
     // No need to query in case of op against a viz info document.
-    request.vizInfo = new VisualizationInfo(snapshot.data);
+    if (snapshot) {
+      request.vizInfo = new VisualizationInfo(snapshot.data);
+    } else if (snapshots) {
+      if(snapshots.length === 1){
+        request.vizInfo = new VisualizationInfo(snapshots[0].data);
+      } else {
+        return done('Case of multiple info documents not handled')
+      }
+    }
+    return done();
   }
 };
