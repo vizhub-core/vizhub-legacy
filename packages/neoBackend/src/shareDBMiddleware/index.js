545c1cf3006f14218b228583c9b7151a62b1b820
Curran <curran.kelleher@gmail.com>
2019-12-20 14:19:06 +0530

    Get vizInfo to vet each op

--- packages/neoBackend/src/shareDBMiddleware/index.js
+++ packages/neoBackend/src/shareDBMiddleware/index.js
@@ -1,3 +1,4 @@
 export { identifyAgent } from './identifyAgent';
 export { identifyOwner } from './identifyOwner';
 export { accessControl } from './accessControl';
+export { getVizInfo } from './getVizInfo';
