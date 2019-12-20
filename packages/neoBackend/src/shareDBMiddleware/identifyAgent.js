cf7902a00aef4f0b927a6a387c265a438d9f832f
Curran <curran.kelleher@gmail.com>
2019-12-20 10:08:53 +0530

    Cookie report for debugging

--- packages/neoBackend/src/shareDBMiddleware/identifyAgent.js
+++ packages/neoBackend/src/shareDBMiddleware/identifyAgent.js
@@ -5,15 +5,27 @@ export const identifyAgent = (request, done) => {
   // If the connection is coming from the browser,
   if (request.req) {
     const cookie = request.req.headers.cookie;
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("Cookie report:")
+    console.log(cookie);
     if (cookie) {
       const { vizHubJWT } = parse(cookie);
 
+      console.log(JSON.stringify(parse(cookie, null, 2)));
+
       // and the user is authenticated,
-      // expose the user id to downstram middleware via agent.session.
+      // expose the user id to downstream middleware via agent.session.
       if (vizHubJWT) {
         request.agent.userId = getUserIDFromJWT(vizHubJWT);
       }
     }
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
   } else {
     // Otherwise set a flag that clarifies that
     // the connection is coming from the server (e.g. for creating User entries).
