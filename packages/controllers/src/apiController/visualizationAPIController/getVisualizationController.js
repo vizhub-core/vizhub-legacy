c8839d46cd7ac2dfbeaa2ca3ee27997d4ad0291c
Curran <curran.kelleher@gmail.com>
2019-12-21 16:59:02 +0530

    Hide private vizzes in interactor

--- packages/controllers/src/apiController/visualizationAPIController/getVisualizationController.js
+++ packages/controllers/src/apiController/visualizationAPIController/getVisualizationController.js
@@ -1,14 +1,18 @@
 import { GetVisualization } from 'vizhub-use-cases';
+import { userIdFromReq } from '../userIdFromReq';
 
 export const getVisualizationController = (expressApp, gateways) => {
   const getVisualization = new GetVisualization(gateways);
   expressApp.get('/api/visualization/get/:id', async (req, res) => {
     try {
-      const requestModel = { id: req.params.id };
+      const requestModel = {
+        id: req.params.id,
+        user: userIdFromReq(req)
+      };
       const responseModel = await getVisualization.execute(requestModel);
       res.json(responseModel);
     } catch (error) {
-      res.json({ error });
+      res.json({ error: error.message });
     }
   });
 };
