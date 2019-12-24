2a244c05a72ae2777bcde9565fdb0df2d2eed6a2
Curran <curran.kelleher@gmail.com>
2019-12-24 17:10:41 +0530

    Remove markdown from unfurl text

--- packages/controllers/src/serveVizPage.js
+++ packages/controllers/src/serveVizPage.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { GetVisualization } from 'vizhub-use-cases';
 import { userIdFromReq } from './userIdFromReq';
+import { plainText } from './plainText';
 
 const unfurlPlaceholder = '<meta name="unfurl-all-that:shit" value="please"/>';
 //const unfurlPlaceholder = /<meta name="unfurl-all-that:shit" value="please"\/>/;
@@ -9,17 +10,17 @@ const absolute = relative => 'https://vizhub.com' + relative;
 const previewUrl = id => `/api/visualization/preview/${id}.png`;
 export const visualizationRoute = ({ userName, id }) => `/${userName}/${id}`;
 
-const generateUnfurlHTML = ({ title, description, image, url }) => `
+const generateUnfurlHTML = ({ title, descriptionPlainText, image, url }) => `
   <meta name="twitter:url" value="${url}" />
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:site" value="@datavis_tech" />
   <meta name="twitter:title" value="${title}" />
-  <meta name="twitter:description" value="${description}" />
+  <meta name="twitter:description" value="${descriptionPlainText}" />
   <meta name="twitter:image" content="${image}" />
   <meta name="twitter:domain" value="vizhub.com" />
   <meta property="og:url" content="${url}"/>
   <meta property="og:title" content="${title}" />
-  <meta property="og:description" content="${description}" />
+  <meta property="og:description" content="${descriptionPlainText}" />
   <meta property="og:image" content="${image}" />
   <meta property="og:site_name" content="VizHub"/>
 `;
@@ -44,7 +45,7 @@ export const serveVizPage = (gateways, indexHTML) => {
       // TODO find and parse that data in client-side code
       const unfurlHTML = generateUnfurlHTML({
         title,
-        description,
+        descriptionPlainText: plainText(description),
         image: absolute(previewUrl(id)),
         url: absolute(visualizationRoute({ userName, id }))
       });
