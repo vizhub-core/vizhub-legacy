c3a6ccfc48adb16cfca4f2c8009917af0a7f0794
Curran <curran.kelleher@gmail.com>
2019-12-26 15:30:39 +0530

    Add pricing link

--- packages/neoFrontend/src/pages/HomePage/index.js
+++ packages/neoFrontend/src/pages/HomePage/index.js
@@ -10,7 +10,7 @@ export const HomePage = () => (
   <HomePageDataProvider fallback={<LoadingScreen />}>
     <Wrapper>
       <Content>
-        <NavBar />
+        <NavBar showPricing={true}/>
         <Banner />
         <Centering>
           <Vizzes />
