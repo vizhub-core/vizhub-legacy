c4e38d9f345306bba1bb40fb5226491222478245
Curran <curran.kelleher@gmail.com>
2019-12-27 11:31:29 +0530

    Clean up test for authenticating as CI

--- packages/neoCI/test/authentication/index.js
+++ packages/neoCI/test/authentication/index.js
@@ -1,13 +1,13 @@
 import { navigateToAuthPage } from './navigateToAuthPage';
-import { authAsCI } from './authAsCI';
+import { signIn } from './signIn';
 import { signOut } from './signOut';
 
 export { signOut };
 
 export const authentication = my => () => {
   it('should navigate to auth page', navigateToAuthPage(my));
-  it('should authenticate as CI', authAsCI(my));
+  it('should authenticate as CI', signIn(my));
   it('should sign out', signOut(my));
   it('should navigate to auth page a second time', navigateToAuthPage(my));
-  it('should authenticate as CI a second time', authAsCI(my));
+  it('should authenticate as CI a second time', signIn(my));
 };
