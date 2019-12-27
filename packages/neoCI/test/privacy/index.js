8a67dee1e954101bda55bc41b12437eb49868b1c
Curran <curran.kelleher@gmail.com>
2019-12-27 12:07:56 +0530

    Refactor toggling of privacy setting

--- packages/neoCI/test/privacy/index.js
+++ packages/neoCI/test/privacy/index.js
@@ -1,13 +1,18 @@
 import { signOut } from '../authentication';
 import { setupPrivateViz } from './setupPrivateViz';
-import { switchToPrivate } from './switchToPrivate';
+import { togglePrivacy } from './togglePrivacy';
 import { vizNotFound } from './vizNotFound';
 
 export const privacy = my => () => {
   before(setupPrivateViz(my));
 
-  it('should switch from public to private', switchToPrivate(my));
+  it(
+    'should switch from public to private',
+    togglePrivacy(my, 'public', 'private')
+  );
   it('should sign out', signOut(my));
-
   it('should display viz not found if not authenticated', vizNotFound(my));
+
+  //it('should sign in', signIn(my));
+  //it('should switch private to public', togglePrivacy(my, 'public'));
 };
--- packages/neoCI/test/privacy/vizNotFound.js
+++ packages/neoCI/test/privacy/vizNotFound.js
@@ -1,9 +1,6 @@
 import assert from 'assert';
 
 export const vizNotFound = my => async () => {
-  console.log('here');
   await my.page.reload();
-  console.log('there');
-
   await my.page.waitFor('.test-viz-not-found');
 };
