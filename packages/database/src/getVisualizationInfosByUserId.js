0ff3fa5f2668aac9c2406657e98e5d54df0e6ce3
Curran <curran.kelleher@gmail.com>
2019-12-28 17:08:19 +0530

    Exclude private vizzes from profile page

--- packages/database/src/getVisualizationInfosByUserId.js
+++ packages/database/src/getVisualizationInfosByUserId.js
@@ -8,11 +8,18 @@ export const getVisualizationInfosByUserId = connection => async id => {
     owner: id,
     documentType: VISUALIZATION_TYPE
   };
+
+  // TODO show private visualizations if owner is currently authenticated
+  //if(id !== authenticatedId){
+  mongoQuery.privacy = { $ne: 'private' }
+
   const results = await fetchShareDBQuery(
     DOCUMENT_INFO,
     mongoQuery,
     connection
   );
+  // TODO sort by most recently edited, like in home page
+  // TODO pagination for profile page, like in home page
   return results
     .map(shareDBDoc => new VisualizationInfo(shareDBDoc.data))
     .reverse(); // Show most recent first
