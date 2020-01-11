6f681f12baf51b4d950f6bbd37be2adcba4e9cae
curran <curran.kelleher@gmail.com>
2020-01-11 16:22:59 +0530

    Animate arc

--- packages/neoFrontend/src/svg/PlayPauseSVG.js
+++ packages/neoFrontend/src/svg/PlayPauseSVG.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import styled, { withTheme } from 'styled-components';
+import { arc } from 'd3-shape';
 
-const SVG = styled.svg``;
-
+// TODO unify these into one place. Shared by upvote icon.
 const cx = 10;
 const cy = 10;
 const r = 9.45;
+const strokeWidth = 1.1;
 
-// Blue: #3866e9;
-export const PlayPauseSVG = withTheme(
-  ({
-    down = false,
-    height = 20,
-    fill = 'currentcolor',
-    didVote = false,
-    theme
-  }) => (
-    <SVG height={height} viewBox={`0 0 20 20`}>
-      <g
-        fill={fill}
-        transform={`translate(10,10) rotate(-90) translate(-10,-10)`}
-      >
-        <circle
-          cx={cx}
-          cy={cy}
-          r={r}
-          strokeWidth="1.1"
-          stroke={fill}
-          fill="none"
-        />
-        <path d="M10.041 13.974l4.52-4.521a.547.547 0 0 1 .774.774l-4.587 4.587a1 1 0 0 1-1.414 0l-4.587-4.587a.547.547 0 1 1 .774-.774l4.52 4.52z" />
-      </g>
-    </SVG>
-  )
-);
+const runTimerProgressArc = arc()
+  .innerRadius(r - (strokeWidth * 3) / 2)
+  .outerRadius(r - strokeWidth / 2)
+  .startAngle(0);
+
+export const PlayPauseSVG = withTheme(({ height = 20, runTimerProgress }) => (
+  <svg height={height} viewBox={`0 0 20 20`}>
+    <g transform="translate(10,10)">
+      <circle
+        r={r}
+        strokeWidth={strokeWidth}
+        stroke="currentcolor"
+        fill="none"
+      />
+      <path
+        fill="#3866e9"
+        d={runTimerProgressArc({
+          startAngle: 0,
+          endAngle: runTimerProgress * Math.PI * 2
+        })}
+      />
+    </g>
+  </svg>
+));
