c29c059b3e563c360e84acb7bbb187b8f8b1570c
curran <curran.kelleher@gmail.com>
2020-01-11 16:34:10 +0530

    Add pause button

--- packages/neoFrontend/src/svg/PlayPauseSVG.js
+++ packages/neoFrontend/src/svg/PlayPauseSVG.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import styled, { withTheme } from 'styled-components';
 import { arc } from 'd3-shape';
 
 // TODO unify these into one place. Shared by upvote icon.
-const cx = 10;
-const cy = 10;
 const r = 9.45;
 const strokeWidth = 1.1;
 
 const runTimerProgressArc = arc()
   .innerRadius(r - (strokeWidth * 3) / 2)
-  .outerRadius(r - strokeWidth / 2)
+  .outerRadius(r)
   .startAngle(0);
 
-export const PlayPauseSVG = withTheme(({ height = 20, runTimerProgress }) => (
+const Pause = () => (
+  <>
+    <rect x="-3" y="-4" width="2" height="8" />
+    <rect x="1" y="-4" width="2" height="8" />
+  </>
+);
+
+export const PlayPauseSVG = ({ height = 20, runTimerProgress }) => (
   <svg height={height} viewBox={`0 0 20 20`}>
-    <g transform="translate(10,10)">
-      <circle
-        r={r}
-        strokeWidth={strokeWidth}
-        stroke="currentcolor"
-        fill="none"
-      />
+    <g transform="translate(10,10)" fill="currentcolor">
       <path
         fill="#3866e9"
         d={runTimerProgressArc({
@@ -29,6 +27,13 @@ export const PlayPauseSVG = withTheme(({ height = 20, runTimerProgress }) => (
           endAngle: runTimerProgress * Math.PI * 2
         })}
       />
+      <circle
+        r={r}
+        strokeWidth={strokeWidth}
+        stroke="currentcolor"
+        fill="none"
+      />
+      <Pause />
     </g>
   </svg>
-));
+);
