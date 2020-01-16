52bf8bb7b0cd26510389c76321bda8f86bdd2ea6
Curran Kelleher <curran.kelleher@gmail.com>
2020-01-16 14:14:21 +0530

    Fix size & scroll regression

--- packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import { FrameFooter } from '../../styles';
 import { LargeIcon } from '../../../../styles';
 
+// Note that `position: relative;` is required due to useDimensions
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  position: relative;
 `;
 
 export const LargeIconRightmost = styled(LargeIcon)`
