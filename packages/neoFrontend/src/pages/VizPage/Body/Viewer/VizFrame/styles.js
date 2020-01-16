59c27f9eeb73dc0c5b40be99484aa0d2d7ca7ad3
Curran Kelleher <curran.kelleher@gmail.com>
2020-01-16 14:35:09 +0530

    Completely add play pause widget to mini mode

--- packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/VizFrame/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { FrameFooter } from '../../styles';
 import { LargeIcon } from '../../../../styles';
 
 // Note that `position: relative;` is required due to useDimensions
@@ -11,12 +10,4 @@ export const Wrapper = styled.div`
 
 export const LargeIconRightmost = styled(LargeIcon)`
   border-bottom-right-radius: ${props => props.theme.borderRadiusSmall}px;
-`;
-
-export const VizFrameFooter = styled(FrameFooter)`
-  justify-content: space-between;
-`;
-
-export const VizFrameFooterRight = styled.div`
-  display: flex;
 `;
