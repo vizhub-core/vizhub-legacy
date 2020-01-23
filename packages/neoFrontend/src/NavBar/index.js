2c008b1ce19077c526ebb09cf4862e58eda6f0e8
Curran <curran.kelleher@gmail.com>
2020-01-23 17:31:34 +0530

    Open navbar home link in new tab

--- packages/neoFrontend/src/NavBar/index.js
+++ packages/neoFrontend/src/NavBar/index.js
@@ -1,43 +1,50 @@
 import React, { useContext } from 'react';
 import { withTheme } from 'styled-components';
-import { withRouter } from 'react-router';
 import { LogoSVG } from '../svg';
 import { AuthContext, AUTH_PENDING } from '../authentication';
 import { Banner } from '../styles';
 import { SignIn, LogoLink, Right, PricingLink } from './styles';
 import { UserActionsMenu } from './UserActionsMenu';
 
-export const NavBar = withRouter(
-  withTheme(({ theme, location, showRight = true, showPricing = false }) => {
+export const NavBar = withTheme(
+  ({
+    theme,
+    showRight = true,
+    showPricing = false,
+    isHomePage = false,
+    isAuthPage = false
+  }) => {
     const { navbarHeight, navbarLogoColor } = theme;
     const { me, signIn } = useContext(AuthContext);
 
     return (
       <Banner>
-        {location.pathname === '/' ? (
+        {isHomePage ? (
           <LogoSVG height={navbarHeight} fill={navbarLogoColor} />
         ) : (
-          <LogoLink to="/">
+          <LogoLink to="/" target="_blank" rel="noopener noreferrer">
             <LogoSVG height={navbarHeight} fill={navbarLogoColor} />
           </LogoLink>
         )}
 
-        <Right
-          className="test-user-navbar-section"
-          data-test-is-authenticated={Boolean(me)}
-        >
-          {showPricing ? (
-            <PricingLink to="/pricing">Pricing</PricingLink>
-          ) : null}
-          {me === AUTH_PENDING || !showRight ? null : me ? (
-            <UserActionsMenu />
-          ) : (
-            <SignIn className="test-sign-in" onClick={signIn}>
-              Sign up / Sign in
-            </SignIn>
-          )}
-        </Right>
+        {isAuthPage ? null : (
+          <Right
+            className="test-user-navbar-section"
+            data-test-is-authenticated={Boolean(me)}
+          >
+            {showPricing ? (
+              <PricingLink to="/pricing">Pricing</PricingLink>
+            ) : null}
+            {me === AUTH_PENDING || !showRight ? null : me ? (
+              <UserActionsMenu />
+            ) : (
+              <SignIn className="test-sign-in" onClick={signIn}>
+                Sign up / Sign in
+              </SignIn>
+            )}
+          </Right>
+        )}
       </Banner>
     );
-  })
+  }
 );
