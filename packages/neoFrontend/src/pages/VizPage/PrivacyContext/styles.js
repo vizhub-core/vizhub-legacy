cbce0da8ccf80d165d086a0c409dd558c83b1dc6
curran <curran.kelleher@gmail.com>
2020-01-25 16:16:31 +0530

    First steps at new dialog design - section title

---
+++ packages/neoFrontend/src/pages/VizPage/PrivacyContext/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Dialog = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const SectionTitle = styled.div`
+  font-weight: 600;
+  font-size: 22px;
+`;
