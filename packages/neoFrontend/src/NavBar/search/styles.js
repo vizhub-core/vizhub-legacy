bb46abc341e6e61e80e5342fca712ff64f5b99c4
Curran <curran.kelleher@gmail.com>
2020-02-10 15:55:53 -0500

    Finesse Search styles

--- packages/neoFrontend/src/NavBar/search/styles.js
+++ packages/neoFrontend/src/NavBar/search/styles.js
@@ -5,14 +5,9 @@ export const SearchInput = styled.input`
   height: 40px;
   font-family: Poppins;
   font-size: 14px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: center;
-  color: #c0c0c0;
   border-radius: 6px;
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.15);
   background-color: rgba(255, 255, 255, 0.55);
+  border: none;
+  padding-left: 10px;
 `;
