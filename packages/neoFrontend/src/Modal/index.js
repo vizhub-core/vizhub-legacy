5c43247b260881d3c69cebaec342e05956d3eff3
Curran <curran.kelleher@gmail.com>
2020-02-15 13:21:41 -0500

    Make setHeight work in full screen mode

--- packages/neoFrontend/src/Modal/index.js
+++ packages/neoFrontend/src/Modal/index.js
@@ -1,16 +1,15 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { CloseSVG } from '../svg';
 import { Wrapper, Box, CloseIcon, Message } from './styles';
 
 const stopPropagation = event => event.stopPropagation();
 
-const isEscapeKey = ({code}) => code === 'Escape';
+const isEscapeKey = ({ code }) => code === 'Escape';
 
 export const Modal = ({ children, onClose, closeButtonClassName }) => {
   useEffect(() => {
     const onKeyDown = event => {
       if (isEscapeKey(event)) {
-
         // Don't let anything else handle this press of escape.
         stopPropagation(event);
 
