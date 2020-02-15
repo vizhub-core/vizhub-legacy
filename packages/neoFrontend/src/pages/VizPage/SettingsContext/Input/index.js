a1595d2a3ca8f614c6429f3190d59868ef539867
Curran <curran.kelleher@gmail.com>
2020-02-15 12:45:51 -0500

    Support implicit submit in settings modal

--- packages/neoFrontend/src/pages/VizPage/SettingsContext/Input/index.js
+++ packages/neoFrontend/src/pages/VizPage/SettingsContext/Input/index.js
@@ -2,10 +2,9 @@ import React, { useCallback } from 'react';
 import { Wrapper } from './styles';
 
 export const Input = ({ value, onChange, id }) => {
-  const handleChange = useCallback(
-    ({ target: { value } }) => onChange(value),
-    [onChange]
-  );
+  const handleChange = useCallback(({ target: { value } }) => onChange(value), [
+    onChange
+  ]);
 
   return <Wrapper value={value} onChange={handleChange} id={id} />;
 };
