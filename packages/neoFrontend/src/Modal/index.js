9b70503fec67bca1e5665ade225556aa972660ee
Curran <curran.kelleher@gmail.com>
2020-02-15 13:01:42 -0500

    Close modals with escape key

--- packages/neoFrontend/src/Modal/index.js
+++ packages/neoFrontend/src/Modal/index.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { CloseSVG } from '../svg';
 import { Wrapper, Box, CloseIcon, Message } from './styles';
 
 const stopPropagation = event => event.stopPropagation();
 
-export const Modal = ({ children, onClose, closeButtonClassName }) => (
-  <Wrapper onClick={onClose}>
-    <Box onClick={stopPropagation}>
-      <CloseIcon
-        onClick={onClose}
-        rightmost={true}
-        leftmost={true}
-        className={closeButtonClassName}
-      >
-        <CloseSVG />
-      </CloseIcon>
-      {children}
-    </Box>
-  </Wrapper>
-);
+const isEscapeKey = ({code}) => code === 'Escape';
+
+export const Modal = ({ children, onClose, closeButtonClassName }) => {
+  useEffect(() => {
+    const onKeyDown = event => {
+      if (isEscapeKey(event)) {
+
+        // Don't let anything else handle this press of escape.
+        stopPropagation(event);
+
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onClose]);
+  return (
+    <Wrapper onClick={onClose}>
+      <Box onClick={stopPropagation}>
+        <CloseIcon
+          onClick={onClose}
+          rightmost={true}
+          leftmost={true}
+          className={closeButtonClassName}
+        >
+          <CloseSVG />
+        </CloseIcon>
+        {children}
+      </Box>
+    </Wrapper>
+  );
+};
 
 Modal.Message = Message;
