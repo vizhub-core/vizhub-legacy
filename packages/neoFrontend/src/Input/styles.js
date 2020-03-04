6678f3fe48f3e07179e23a6f7ef2f4b81ee842b6
Curran <curran.kelleher@gmail.com>
2020-03-04 08:27:08 -0500

    Add url share input to text field.

--- packages/neoFrontend/src/Input/styles.js
+++ packages/neoFrontend/src/Input/styles.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.input`
   height: 100%;
-  width: 87px;
+  ${props => (props.size === 'grow' ? 'flex: 1;' : '')}
+  ${props =>
+    props.size === 'large' ? 'width: 87px;' : ''}
   font-size: inherit;
   border: 1px solid ${props => props.theme.rule};
   border-radius: 4px;
--- packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { SectionDescription } from '../../styles';
+import { Button } from '../../../styles';
+import { Input } from '../../../../Input';
+import { FormRow } from '../../styles';
 
-export const LinkBody = () => (
-  <SectionDescription>
-    Sharing this link on social media will automatically create a preview of
-    your visualization.
-  </SectionDescription>
-);
+export const LinkBody = () => {
+  const url = 'something';
+  return (
+    <SectionDescription>
+      <FormRow>
+      Sharing this link on social media will automatically create a preview.
+      </FormRow>
+      <FormRow>
+        <Input value={url} size="grow" />
+        <Button>Copy</Button>
+      </FormRow>
+    </SectionDescription>
+  );
+};
