a18bd135a408b0bba6c31d1236761df8674923be
Curran <curran.kelleher@gmail.com>
2020-03-04 08:32:12 -0500

    Use correct URL in share section.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
+
 import { SectionDescription } from '../../styles';
 import { Button } from '../../../styles';
 import { Input } from '../../../../Input';
 import { FormRow } from '../../styles';
 
-export const LinkBody = () => {
-  const url = 'something';
-  return (
-    <SectionDescription>
-      <FormRow>
+export const LinkBody = () => (
+  <SectionDescription>
+    <FormRow>
       Sharing this link on social media will automatically create a preview.
-      </FormRow>
-      <FormRow>
-        <Input value={url} size="grow" />
-        <Button>Copy</Button>
-      </FormRow>
-    </SectionDescription>
-  );
-};
+    </FormRow>
+    <FormRow>
+      <Input value={window.location.href} size="grow" />
+      <Button shrink>Copy</Button>
+    </FormRow>
+  </SectionDescription>
+);
