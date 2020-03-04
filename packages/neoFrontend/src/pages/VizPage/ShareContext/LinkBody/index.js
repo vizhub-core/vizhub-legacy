23a975a7b5abe6193c463371c4e3e49390252139
Curran <curran.kelleher@gmail.com>
2020-03-04 08:49:08 -0500

    Implement copy button.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import copy from 'copy-to-clipboard';
 
 import { SectionDescription } from '../../styles';
 import { Button } from '../../../styles';
 import { Input } from '../../../../Input';
 import { FormRow } from '../../styles';
 
-export const LinkBody = () => (
-  <SectionDescription>
-    <FormRow>
-      Sharing this link on social media will automatically create a preview.
-    </FormRow>
-    <FormRow>
-      <Input value={window.location.href} size="grow" autoSelect />
-      <Button>Copy</Button>
-    </FormRow>
-  </SectionDescription>
-);
+export const LinkBody = () => {
+  const url = window.location.href;
+
+  const handleClick = useCallback(() => {
+    copy(url);
+  }, [url]);
+
+  return (
+    <SectionDescription>
+      <FormRow>
+        Sharing this link on social media will automatically create a preview.
+      </FormRow>
+      <FormRow>
+        <Input value={url} size="grow" autoSelect />
+        <Button onClick={handleClick}>Copy</Button>
+      </FormRow>
+    </SectionDescription>
+  );
+};
