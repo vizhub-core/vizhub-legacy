378c25b6dc7b98617772bd7004903b9b4c194c67
Curran <curran.kelleher@gmail.com>
2020-03-05 11:53:05 -0500

    First pass at adding text input to collaborators section.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { SectionDescription } from '../../styles';
+import { SubSectionDescription, Spacer } from '../../styles';
+import { Input } from '../../../../Input';
+import { FormRow } from '../../styles';
 
 export const CollaboratorsBody = () => (
-  <SectionDescription>
-    Start typing to search available collaborators for this visualization.
-  </SectionDescription>
+  <>
+    <SubSectionDescription>
+      Start typing to search available collaborators for this visualization.
+    </SubSectionDescription>
+    <Spacer height={22} />
+    <FormRow>
+      <Input size="grow" />
+    </FormRow>
+  </>
 );
