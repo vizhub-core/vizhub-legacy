ffaa4dca44a5533449d4c7796bde1e585df28a34
Curran <curran.kelleher@gmail.com>
2020-03-05 09:30:43 -0500

    Add radio buttons to embed body.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/EmbedBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/EmbedBody/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SubSectionDescription } from '../../styles';
+import { RadioButton } from '../../RadioButton';
 import { TextCopier } from '../TextCopier';
+
+const VIZ = 'Embed visualization';
+const WHITELABEL = 'Embed white-label visualization';
+const PREVIEW = 'Embed visualization preview';
 
 export const EmbedBody = () => {
   const html = '<iframe></iframe>';
+  const [embedType, setEmbedType] = useState(VIZ);
 
   return (
     <>
       <SubSectionDescription>Embed Preview Image</SubSectionDescription>
       <SubSectionDescription>Embed Settings</SubSectionDescription>
+      <RadioButton.Group onChange={setEmbedType} currentValue={embedType}>
+        <RadioButton value={VIZ} />
+        <RadioButton value={WHITELABEL} />
+        <RadioButton value={PREVIEW} />
+      </RadioButton.Group>
       <TextCopier text={html} />
     </>
   );
