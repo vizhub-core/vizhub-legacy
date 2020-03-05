e8546d056021f928be9735b6a0bde7ca18b37006
Curran <curran.kelleher@gmail.com>
2020-03-05 12:03:43 -0500

    Implement debounced search for collaborators.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SubSectionDescription, Spacer } from '../../styles';
 import { Input } from '../../../../Input';
 import { FormRow } from '../../styles';
 
-export const CollaboratorsBody = () => (
-  <>
-    <SubSectionDescription>
-      Start typing to search available collaborators for this visualization.
-    </SubSectionDescription>
-    <Spacer height={22} />
-    <FormRow>
-      <Input size="grow" />
-    </FormRow>
-  </>
-);
+const debounceTimeMS = 500;
+export const CollaboratorsBody = () => {
+  const [typedText, setTypedText] = useState('');
+  useEffect(() => {
+    console.log('here');
+    const timeout = setTimeout(() => {
+      console.log('Fetch data for ' + typedText);
+    }, debounceTimeMS);
+    return () => clearTimeout(timeout);
+  }, [typedText]);
+  console.log(typedText);
+  return (
+    <>
+      <SubSectionDescription>
+        Start typing to search available collaborators for this visualization.
+      </SubSectionDescription>
+      <Spacer height={22} />
+      <FormRow>
+        <Input value={typedText} onChange={setTypedText} size="grow" />
+      </FormRow>
+    </>
+  );
+};
