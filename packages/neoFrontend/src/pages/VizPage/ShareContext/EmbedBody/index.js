2081eec7cec1f1d84b98d7db774174be92893b70
Curran <curran.kelleher@gmail.com>
2020-03-05 09:15:30 -0500

    Refactor: generalize TextCopier.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/EmbedBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/EmbedBody/index.js
@@ -1,24 +1,15 @@
-import copy from 'copy-to-clipboard';
-import React, {useCallback} from 'react';
-import { Button } from '../../../styles';
-import { Input } from '../../../../Input';
-import { SubSectionDescription, FormRow } from '../../styles';
+import React from 'react';
+import { SubSectionDescription } from '../../styles';
+import { TextCopier } from '../TextCopier';
 
 export const EmbedBody = () => {
   const html = '<iframe></iframe>';
-
-  const handleClick = useCallback(() => {
-    copy(html);
-  }, [html]);
 
   return (
     <>
       <SubSectionDescription>Embed Preview Image</SubSectionDescription>
       <SubSectionDescription>Embed Settings</SubSectionDescription>
-      <FormRow>
-        <Input value={html} size="grow" autoSelect />
-        <Button onClick={handleClick}>Copy</Button>
-      </FormRow>
+      <TextCopier text={html} />
     </>
   );
 };
--- packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/LinkBody/index.js
@@ -1,25 +1,16 @@
-import React, { useCallback } from 'react';
-import copy from 'copy-to-clipboard';
-import { Button } from '../../../styles';
-import { Input } from '../../../../Input';
-import { SubSectionDescription, FormRow } from '../../styles';
+import React from 'react';
+import { SubSectionDescription } from '../../styles';
+import { TextCopier } from '../TextCopier';
 
 export const LinkBody = () => {
   const url = window.location.href;
-
-  const handleClick = useCallback(() => {
-    copy(url);
-  }, [url]);
 
   return (
     <>
       <SubSectionDescription>
         Sharing this link on social media will automatically create a preview.
       </SubSectionDescription>
-      <FormRow>
-        <Input value={url} size="grow" autoSelect />
-        <Button onClick={handleClick}>Copy</Button>
-      </FormRow>
+      <TextCopier text={url} />
     </>
   );
 };
