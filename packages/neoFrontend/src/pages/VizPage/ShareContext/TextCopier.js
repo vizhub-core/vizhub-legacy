64e7d14348ccd09386b72a6f8d95fde227b0cbfb
Curran <curran.kelleher@gmail.com>
2020-03-05 09:15:30 -0500

    Refactor: generalize TextCopier.

---
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/TextCopier.js
@@ -0,0 +1,21 @@
+import React, { useCallback, useState } from 'react';
+import copy from 'copy-to-clipboard';
+import { Button } from '../../styles';
+import { Input } from '../../../Input';
+import { FormRow } from '../styles';
+
+export const TextCopier = ({ text }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = useCallback(() => {
+    copy(text);
+    setCopied(true);
+  }, [text]);
+
+  return (
+    <FormRow>
+      <Input value={text} size="grow" autoSelect />
+      <Button onClick={handleClick}>{copied ? 'Copied!' : 'Copy'}</Button>
+    </FormRow>
+  );
+};
