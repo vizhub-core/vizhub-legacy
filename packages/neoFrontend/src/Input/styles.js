1b4f172bc7083d264321b7fbb3ef5e58cd790b21
Curran <curran.kelleher@gmail.com>
2020-03-05 09:04:31 -0500

    Fix font style in input.

--- packages/neoFrontend/src/Input/styles.js
+++ packages/neoFrontend/src/Input/styles.js
@@ -5,7 +5,8 @@ export const Wrapper = styled.input`
   ${props => (props.size === 'grow' ? 'flex: 1;' : '')}
   ${props =>
     props.size === 'large' ? 'width: 87px;' : ''}
-  font-size: inherit;
+  font-family: Inter;
+  font-size: 16px;;
   border: 1px solid ${props => props.theme.rule};
   border-radius: 4px;
   padding-left: 14px;
