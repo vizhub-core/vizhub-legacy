2a9186b5ee27cb2a97fcd0367fbf42ed7a09e6b8
Curran <curran.kelleher@gmail.com>
2020-03-27 17:28:49 -0400

    Got anyone can edit toggle and ShareDB property working.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/AnyoneCanEdit.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/AnyoneCanEdit.js
@@ -1,10 +1,37 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
+import { getVizAnyoneCanEdit, anyoneCanEditChangeOp } from 'vizhub-presenters';
 import { CheckBox } from '../../CheckBox';
+import { useValue } from '../../../../useValue';
+import { RealtimeModulesContext } from '../../RealtimeModulesContext';
+import { VizContext } from '../../VizContext';
 
 export const AnyoneCanEdit = () => {
-  const onClick = useCallback(() => {
-    console.log('here');
-  }, []);
+  const { viz$, submitVizInfoOp } = useContext(VizContext);
+  const realtimeModules = useContext(RealtimeModulesContext);
+  const vizAnyoneCanEdit = useValue(viz$, getVizAnyoneCanEdit);
+  console.log(vizAnyoneCanEdit);
+  const setVizAnyoneCanEdit = useCallback(
+    newVizAnyoneCanEdit => {
+      submitVizInfoOp(
+        anyoneCanEditChangeOp(
+          viz$.getValue().info.anyoneCanEdit,
+          newVizAnyoneCanEdit,
+          realtimeModules
+        )
+      );
+    },
+    [submitVizInfoOp, realtimeModules, viz$]
+  );
 
-  return <CheckBox label="Anyone can edit." onClick={onClick} />;
+  const onClick = useCallback(() => {
+    setVizAnyoneCanEdit(!vizAnyoneCanEdit);
+  }, [setVizAnyoneCanEdit, vizAnyoneCanEdit]);
+
+  return (
+    <CheckBox
+      label="Anyone can edit."
+      isActive={vizAnyoneCanEdit}
+      onClick={onClick}
+    />
+  );
 };
