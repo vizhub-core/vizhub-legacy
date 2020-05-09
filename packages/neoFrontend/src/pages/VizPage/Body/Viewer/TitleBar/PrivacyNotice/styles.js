912d5d0ba475120e5d123608c01aae054e8a091e
Curran <curran.kelleher@gmail.com>
2020-05-09 11:41:13 -0400

    First pass privacy notice on viz view page

---
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/TitleBar/PrivacyNotice/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: ${props => props.theme.red};
+  height: 40px;
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 12px 0 12px;
+  font-weight: bold;
+  font-size: 14px;
+  color: white;
+  margin-left: 10px;
+`;
