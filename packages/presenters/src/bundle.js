52c56cbc90da7fb6eff0e9f57bccb454564e668e
Curran <curran.kelleher@gmail.com>
2020-05-09 12:44:23 -0400

    Greenify presenters tests

--- packages/presenters/src/bundle.js
+++ packages/presenters/src/bundle.js
@@ -35,8 +35,10 @@ export const bundle = async (files) => {
     ],
     external,
   };
+  console.log('heere');
 
   const rollupBundle = await rollup(inputOptions);
+  console.log('thheere');
   const { output } = await rollupBundle.generate(outputOptions);
 
   // Monkey patch magic-string internals
