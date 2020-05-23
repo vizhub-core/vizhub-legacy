70757b9741add1a7c6bcda7348bb0e3a7a5f11a2
Curran <curran.kelleher@gmail.com>
2020-05-23 16:55:21 -0400

    Implement removal of a collaborator.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Collaborator } from './Collaborator';
 
-export const CollaboratorList = ({ collaborators }) => {
+export const CollaboratorList = ({ collaborators, removeCollaborator }) => {
   return collaborators.map((collaborator) => (
-    <Collaborator key={collaborator.userId} collaborator={collaborator} />
+    <Collaborator
+      key={collaborator.userId}
+      collaborator={collaborator}
+      removeCollaborator={removeCollaborator}
+    />
   ));
 };
