54b3275ca97e932fcd375df8443a34a0b7bef114
Curran <curran.kelleher@gmail.com>
2020-05-23 15:51:01 -0400

    Get users

--- packages/controllers/src/apiController/userAPIController/index.js
+++ packages/controllers/src/apiController/userAPIController/index.js
@@ -1,7 +1,9 @@
 import { getProfileDataController } from './getProfileDataController';
 import { getUserSearchResultsDataController } from './getUserSearchResultsData';
+import { getUsersController } from './getUsers';
 
 export const userAPIController = (expressApp, userGateway) => {
   getProfileDataController(expressApp, userGateway);
   getUserSearchResultsDataController(expressApp, userGateway);
+  getUsersController(expressApp, userGateway);
 };
