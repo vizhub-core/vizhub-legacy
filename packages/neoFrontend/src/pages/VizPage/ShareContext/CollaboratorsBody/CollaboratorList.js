bbfc61e55a6628ad6d0b67ac7386c249b93fda68
Curran <curran.kelleher@gmail.com>
2020-05-23 16:06:56 -0400

    Got collaborator users to show up.

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import { CollaboratorWrapper } from './styles';
+import { CollaboratorWrapper, UserPreview, UserName } from './styles';
 import { useCache } from './useCache';
+import { Avatar } from '../../../../Avatar';
 import { fetchUsers } from './fetchUsers';
+
+const fetchUser = async (userId) => {
+  try {
+    const result = await fetchUsers([userId]);
+    return result.users[0];
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const Collaborator = ({ collaborator }) => {
   const { userId } = collaborator;
   const { requestData } = useCache();
 
   const user = requestData({
-    cacheKey: userId,
-    goFetch: () => fetchUsers([userId]),
+    cacheKey: 'user_' + userId,
+    goFetch: () => fetchUser(userId),
   });
 
+  console.log(user);
+
   return (
-    <CollaboratorWrapper>{user ? user.id : 'Loading...'}</CollaboratorWrapper>
+    <CollaboratorWrapper>
+      {user ? (
+        <UserPreview>
+          <Avatar size={24} user={user} isDisabled={true} />
+          <UserName>{user.fullName}</UserName>
+        </UserPreview>
+      ) : (
+        'Loading...'
+      )}
+    </CollaboratorWrapper>
   );
 };
+
 export const CollaboratorList = ({ collaborators }) => {
   return collaborators.map((collaborator) => (
     <Collaborator key={collaborator.userId} collaborator={collaborator} />
