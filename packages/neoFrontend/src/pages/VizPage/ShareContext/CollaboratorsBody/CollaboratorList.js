e52f053bded368916217ee64e0d77b497671ef2b
Curran <curran.kelleher@gmail.com>
2020-06-08 16:08:21 -0400

    Add spacing between collaborator entries. Closes #305

--- packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
+++ packages/neoFrontend/src/pages/VizPage/ShareContext/CollaboratorsBody/CollaboratorList.js
@@ -5,11 +5,12 @@ import { CollaboratorListWrapper } from './styles';
 export const CollaboratorList = ({ collaborators, removeCollaborator }) =>
   collaborators && collaborators.length > 0 ? (
     <CollaboratorListWrapper>
-      {collaborators.map((collaborator) => (
+      {collaborators.map((collaborator, i) => (
         <Collaborator
           key={collaborator.userId}
           collaborator={collaborator}
           removeCollaborator={removeCollaborator}
+          isLast={i === collaborators.length - 1}
         />
       ))}
     </CollaboratorListWrapper>
