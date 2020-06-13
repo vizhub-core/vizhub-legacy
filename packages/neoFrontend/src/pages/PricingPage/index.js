6de9a1e5b062e5898f20e4ab0bacf657c846a215
Curran <curran.kelleher@gmail.com>
2020-06-13 15:30:22 -0400

    Make pricing page work on mobile.

--- packages/neoFrontend/src/pages/PricingPage/index.js
+++ packages/neoFrontend/src/pages/PricingPage/index.js
@@ -15,48 +15,57 @@ import {
 import { features, plans, FREE } from './featuresAndPlans';
 import { PlanIncludedSVG, PlanExcludedSVG } from '../../svg';
 
-export const PricingPage = () => (
-  <Wrapper>
-    <Content>
-      <NavBar />
-      <Table>
-        <Row>
-          <Left />
-          <Right>
-            {plans.map((plan) => (
-              <PlanLabel key={plan.id}>{plan.label}</PlanLabel>
-            ))}
-          </Right>
-        </Row>
-        {features.map((feature, i) => (
-          <>
-            <Row key={feature.title}>
-              <Left>
-                <FeatureTitle>{feature.title}</FeatureTitle>
-                <FeatureDescription>{feature.description}</FeatureDescription>
-              </Left>
-              <Right>
-                {plans.map((plan) =>
-                  feature.plans[plan.id] ? (
-                    <PlanIncludedSVG />
-                  ) : (
-                    <PlanExcludedSVG />
-                  )
-                )}
-              </Right>
-            </Row>
-            {i < features.length - 1 ? <HorizontalRule /> : null}
-          </>
-        ))}
-        <Row>
-          <Left />
-          <Right>
-            {plans.map((plan) =>
-              plan.id === FREE ? <EmptySpace /> : <Button>Upgrade</Button>
-            )}
-          </Right>
-        </Row>
-      </Table>
-    </Content>
-  </Wrapper>
-);
+export const PricingPage = () => {
+  const handleUpgradeClick = () => {
+    console.log('here');
+  };
+  return (
+    <Wrapper>
+      <Content>
+        <NavBar />
+        <Table>
+          <Row>
+            <Left />
+            <Right>
+              {plans.map((plan) => (
+                <PlanLabel key={plan.id}>{plan.label}</PlanLabel>
+              ))}
+            </Right>
+          </Row>
+          {features.map((feature, i) => (
+            <>
+              <Row key={feature.title}>
+                <Left>
+                  <FeatureTitle>{feature.title}</FeatureTitle>
+                  <FeatureDescription>{feature.description}</FeatureDescription>
+                </Left>
+                <Right>
+                  {plans.map((plan) =>
+                    feature.plans[plan.id] ? (
+                      <PlanIncludedSVG />
+                    ) : (
+                      <PlanExcludedSVG />
+                    )
+                  )}
+                </Right>
+              </Row>
+              {i < features.length - 1 ? <HorizontalRule /> : null}
+            </>
+          ))}
+          <Row>
+            <Left />
+            <Right>
+              {plans.map((plan) =>
+                plan.id === FREE ? (
+                  <EmptySpace />
+                ) : (
+                  <Button onClick={handleUpgradeClick}>Upgrade</Button>
+                )
+              )}
+            </Right>
+          </Row>
+        </Table>
+      </Content>
+    </Wrapper>
+  );
+};
