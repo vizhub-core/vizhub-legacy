8f28b2e7163860e2ae362183ef26f7924f9beb42
milafrerichs <mila.frerichs@gmail.com>
2020-06-18 12:13:04 +0200

    feat: add react-testing-library for easy unit/integration tests
    react testing library is a really nice and clean way to test the actual
    output of react components it uses the actual DOM and not the virtual
    dom like enzyme.

---
+++ packages/neoFrontend/src/pages/ProfilePage/profilepage.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { AuthContext } from '../../authentication';
+
+import { Body } from './Body/index.js';
+import { ProfilePageDataContext } from './ProfilePageDataContext';
+import { Themed } from '../../theme';
+
+test('NameConsumer shows value from provider', () => {
+  const profilePageData = { visualizationInfos: [
+    { privacy: 'private', title: 'My little private visualization'},
+    { privacy: 'public', title: 'My big public visualization'},
+  ]}
+  const me = {}
+  const tree = (
+    <Themed>
+      <AuthContext.Provider value={me}>
+        <ProfilePageDataContext.Provider value={profilePageData}>
+          <Body />
+        </ProfilePageDataContext.Provider>
+      </AuthContext.Provider>
+    </Themed>
+  )
+  const { getByText } = render(tree, { wrapper: MemoryRouter })
+  expect(getByText('My little private visualization')).toBeInTheDocument()
+  expect(getByText('My big public visualization')).toBeInTheDocument()
+})
