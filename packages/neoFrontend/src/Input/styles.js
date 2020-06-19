1831d8ccfacec47b5c0d2273f8e86630bde6d9ba
Curran <curran.kelleher@gmail.com>
2020-06-19 18:13:49 -0400

    Iron out new button height usage

--- packages/neoFrontend/src/Input/styles.js
+++ packages/neoFrontend/src/Input/styles.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.input`
   height: 100%;
   ${(props) => (props.size === 'grow' ? 'flex: 1;' : '')}
-  ${(props) =>
-    props.size === 'large' ? 'width: 87px;' : ''}
+  ${(props) => (props.size === 'large' ? 'width: 87px;' : '')}
   font-size: 16px;
   border: 1px solid ${(props) => props.theme.lightBorder};
   border-radius: 4px;
