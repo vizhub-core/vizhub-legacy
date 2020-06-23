544f3d13f5db687b57344daff189473a4822a3e5
shurik <stu.shurik@gmail.com>
2020-06-23 17:32:45 +0300

    cleanup

--- packages/neoFrontend/src/pages/ForksPage/Vizzes.js
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from 'react';
-import { Vizzes as VizzesPresentation } from '../../VizzesGrid/Vizzes';
-import { PageDataContext } from './PageDataContext';
-
-export const Vizzes = () => (
-  <VizzesPresentation {...useContext(PageDataContext)} />
-);
