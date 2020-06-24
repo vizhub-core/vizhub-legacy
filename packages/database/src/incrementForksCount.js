70cc4d2c77fa1334c5bba33d6b4f314a0daa0609
shurik <stu.shurik@gmail.com>
2020-06-24 18:49:27 +0300

    use targeted sharedn op for forks count

---
+++ packages/database/src/incrementForksCount.js
@@ -0,0 +1,25 @@
+import { DOCUMENT_INFO } from './collectionName';
+import { fetchShareDBDoc } from './fetchShareDBDoc';
+
+const forksCountChangeOp = (oldForksCount, newForksCount) =>
+  // Initialize the height field if needed.
+  [
+    {
+      p: ['forksCount'],
+      oi: +newForksCount,
+      od: oldForksCount,
+    },
+  ];
+
+const submitChangeForksCountOp = (doc, oldForksCount, newForksCount) =>
+  new Promise((resolve, reject) => {
+    const callback = (error) =>
+      error ? reject(error) : resolve({ status: 'success' });
+
+    doc.submitOp(forksCountChangeOp(oldForksCount, newForksCount), callback);
+  });
+
+export const incrementForksCount = (connection) => ({ id }) =>
+  fetchShareDBDoc(DOCUMENT_INFO, id, connection).then(
+    info => submitChangeForksCountOp(info, info.data.forksCount, info.data.forksCount + 1)
+  );
