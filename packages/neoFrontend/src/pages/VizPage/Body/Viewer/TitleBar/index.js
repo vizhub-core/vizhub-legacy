d0e490ba9cad38d08da6898d163dcc58a5974816
shurik <stu.shurik@gmail.com>
2020-06-24 19:20:41 +0300

    add viz upvotes counter to viz preview

--- packages/neoFrontend/src/pages/VizPage/Body/Viewer/TitleBar/index.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Viewer/TitleBar/index.js
@@ -1,48 +1,13 @@
 import React from 'react';
-import { VoteSVG } from '../../../../../svg';
-import { showUpvote, showDownvote } from '../../../../../featureFlags';
-import { Wrapper, Title, Voter, VoteIcon } from './styles';
-import { PrivacyNotice } from '../../../../../PrivacyNotice';
+import { Wrapper, Title } from './styles';
+import { Voter } from '../../../../../Voter';
 
 export const TitleBar = ({
   title,
-  upvoteCount,
-  onUpvoteClick,
-  canVote,
-  didVote,
-  isPrivate,
+  ...voterProps
 }) => (
   <Wrapper>
     <Title>{title}</Title>
-    <Voter>
-      {showUpvote ? (
-        <>
-          <VoteIcon
-            leftmost={true}
-            onClick={onUpvoteClick}
-            canVote={canVote}
-            title={
-              canVote
-                ? didVote
-                  ? 'Remove upvote'
-                  : 'Upvote'
-                : 'Sign in to upvote.'
-            }
-          >
-            <VoteSVG didVote={didVote} />
-          </VoteIcon>
-          {upvoteCount}
-        </>
-      ) : null}
-      {showDownvote ? (
-        <>
-          <VoteIcon>
-            <VoteSVG down={true} />
-          </VoteIcon>
-          6
-        </>
-      ) : null}
-      {isPrivate ? <PrivacyNotice /> : null}
-    </Voter>
+    <Voter {...voterProps} />
   </Wrapper>
 );
