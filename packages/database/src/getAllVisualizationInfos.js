cec82fe6801d83f6fc5fb7dedc0da2d189158573
Curran <curran.kelleher@gmail.com>
2020-06-25 09:54:56 -0400

    Debug

--- packages/database/src/getAllVisualizationInfos.js
+++ packages/database/src/getAllVisualizationInfos.js
@@ -18,7 +18,7 @@ export const getAllVisualizationInfos = (connection) => async () => {
     // data: { imagesUpdatedTimestamp: 1593086383 }
     // Possibly these are deleted docs that the image generation
     // service attempted to generate images for in the past.
-    .filter(shareDBDoc => shareDBDoc.data && shareDBDoc.data.id)
+    .filter(shareDBDoc => shareDBDoc.data && shareDBDoc.data.id && shareDBDoc.data.id !== '9be4ceba5a3449da8f0176d1a6ec215c')
 
     .map((shareDBDoc) => new VisualizationInfo(shareDBDoc.data));
 };
