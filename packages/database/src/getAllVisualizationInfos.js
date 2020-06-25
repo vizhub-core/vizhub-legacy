56bdb4a0ea01334123310df568c536f4f22aa7b0
Curran <curran.kelleher@gmail.com>
2020-06-25 08:52:04 -0400

    Debug

--- packages/database/src/getAllVisualizationInfos.js
+++ packages/database/src/getAllVisualizationInfos.js
@@ -14,8 +14,10 @@ export const getAllVisualizationInfos = (connection) => async () => {
   return results
 
     // Guard against oddly shaped documents of mysterious origin
-  // that are present in the production database, for example:
-  // data: { imagesUpdatedTimestamp: 1593086383 }
+    // that are present in the production database, for example:
+    // data: { imagesUpdatedTimestamp: 1593086383 }
+    // Possibly these are deleted docs that the image generation
+    // service attempted to generate images for in the past.
     .filter(shareDBDoc => shareDBDoc.data && shareDBDoc.data.id)
 
     .map((shareDBDoc) => new VisualizationInfo(shareDBDoc.data));
