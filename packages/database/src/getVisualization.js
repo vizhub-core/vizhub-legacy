1ba7f784da2032d99a120ed4462c9e12c9d675bb
Curran <curran.kelleher@gmail.com>
2020-06-25 09:59:53 -0400

    Remove debug

--- packages/database/src/getVisualization.js
+++ packages/database/src/getVisualization.js
@@ -7,19 +7,12 @@ import { DOCUMENT_INFO, DOCUMENT_CONTENT } from './collectionName';
 import { fetchShareDBDoc } from './fetchShareDBDoc';
 
 export const getVisualization = (connection) => async ({ id }) => {
-  console.log('id');
-  console.log(id);
   const [info, content] = await Promise.all([
     fetchShareDBDoc(DOCUMENT_INFO, id, connection),
     fetchShareDBDoc(DOCUMENT_CONTENT, id, connection),
   ]);
-  console.log('info');
-  console.log(info);
-  //console.log('content.data');
-  //console.log(content.data);
-  console.log('returning new viz')
   return new Visualization({
     visualizationInfo: new VisualizationInfo(info.data),
     visualizationContent: new VisualizationContent(content.data),
   });
-}
+};
