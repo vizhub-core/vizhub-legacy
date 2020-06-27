19a93a4dd2324ee8792e28e912fdccfcd27538fe
Curran <curran.kelleher@gmail.com>
2020-06-27 16:38:55 +0000

    Delete old deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-git checkout master
 git pull
-
 lerna bootstrap
-cd packages/web
-npm run build
+sh ./buildFrontend.sh
 pm2 restart all
-
-# If no processes are set up in PM2, run this:
-# pm2 start ecosystem.config.js --env production
