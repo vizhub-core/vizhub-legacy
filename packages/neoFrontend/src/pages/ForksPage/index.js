62a6a88ee15c83f36b8b7e09b1e4e60f9724dc9c
Denise Mauldin <denise.mauldin@gmail.com>
2020-06-30 10:04:48 -0700

    Fetch data for visualization and display title

--- packages/neoFrontend/src/pages/ForksPage/index.js
+++ packages/neoFrontend/src/pages/ForksPage/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LoadingScreen } from '../../LoadingScreen';
-import { Wrapper, Content, Centering } from '../styles';
+import { Wrapper, Content, Centering, Title } from '../styles';
 import { NavBar } from '../../NavBar';
 import { Vizzes } from '../../VizzesGrid/Vizzes';
-import { usePageData } from './usePageData';
+import { usePageData, useVizData } from './usePageData';
+import { ErrorContext } from '../../ErrorContext';
 
 export const ForksPage = () => {
   const pageData = usePageData();
+  const vizData = useVizData();
+  const { setError } = useContext(ErrorContext);
 
-  return pageData ? (
+  if (vizData && vizData.error) {
+    setError({
+      message: 'Visualization not found.',
+      className: 'test-viz-not-found',
+    });
+    return null;
+  }
+
+  return vizData && pageData ? (
     <Wrapper>
       <Content>
         <NavBar />
+        <Title>Forks of {vizData.visualization.info.title}</Title>
         <Centering>
           <Vizzes {...pageData} />
         </Centering>
