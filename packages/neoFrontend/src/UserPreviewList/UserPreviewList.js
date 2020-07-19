6c1d3420f4e4c928f2349d63e826dde0caca6dc8
shurik <stu.shurik@gmail.com>
2020-07-19 19:59:22 +0300

    fix search autocomplete focus
    Closes datavis-tech/vizhub-issue-tracker/issues/387

--- packages/neoFrontend/src/UserPreviewList/UserPreviewList.js
+++ packages/neoFrontend/src/UserPreviewList/UserPreviewList.js
@@ -1,51 +1,20 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
+import React from 'react';
 import { getUserName, getUserFullName } from 'vizhub-presenters';
 import { Avatar } from '../Avatar';
 import { Container, UserPreview, UserName } from './styles';
 
-export const UserPreviewList = ({ users, onSelect }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const containerRef = useRef();
-
-  useEffect(() => {
-    if (users.length) {
-      containerRef.current.focus();
-    }
-  }, [users]);
-
-  const handleKeyDown = useCallback(
-    (event) => {
-      if (users.length === 0) return;
-
-      event.preventDefault();
-
-      if (event.key === 'ArrowDown' && activeIndex < users.length - 1) {
-        setActiveIndex(activeIndex + 1);
-      }
-
-      if (event.key === 'ArrowUp' && activeIndex > 0) {
-        setActiveIndex(activeIndex - 1);
-      }
-
-      if (event.key === 'Enter') {
-        onSelect(users[activeIndex]);
-      }
-    },
-    [users, onSelect, activeIndex, setActiveIndex]
-  );
-
+export const UserPreviewList = ({ user, users, onSelect }) => {
   return (
-    <Container ref={containerRef} tabIndex="-1" onKeyDown={handleKeyDown}>
+    <Container>
       {users &&
-        users.map((user, index) => (
+        users.map((userToRender) => (
           <UserPreview
-            className={index === activeIndex ? 'active' : ''}
-            key={getUserName(user)}
-            onClick={() => onSelect(user)}
+            className={user === userToRender ? 'active' : ''}
+            key={getUserName(userToRender)}
+            onClick={() => onSelect(userToRender)}
           >
-            <Avatar size={24} user={user} isDisabled={true} />
-            <UserName>{getUserFullName(user)}</UserName>
+            <Avatar size={24} user={userToRender} isDisabled={true} />
+            <UserName>{getUserFullName(userToRender)}</UserName>
           </UserPreview>
         ))}
     </Container>
