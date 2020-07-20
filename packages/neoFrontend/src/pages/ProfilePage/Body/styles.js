fefb8b1b98846b3e233f35df4de8cb730d71493e
Curran <curran.kelleher@gmail.com>
2020-07-20 15:16:12 -0400

    Fix z index issue and background color for feedback button

--- packages/neoFrontend/src/pages/ProfilePage/Body/styles.js
+++ packages/neoFrontend/src/pages/ProfilePage/Body/styles.js
@@ -1,5 +1,6 @@
 // Common styles used in multiple pages.
 import styled from 'styled-components';
+import { Z_WAY_ABOVE } from '../../../styles';
 import { Button } from '../../styles';
 
 export const FeedbackWrapper = styled.div`
@@ -10,6 +11,7 @@ export const FeedbackWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index: ${Z_WAY_ABOVE};
 `;
 
 export const Feedback = styled(Button)`
@@ -21,4 +23,5 @@ export const Feedback = styled(Button)`
   border-radius: 0 6px 6px 0;
   border-left: none;
   font-size: 14px;
+  background-color: white;
 `;
