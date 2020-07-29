be9c4be1c2887e774b033bdf32d7f6dc456cd293
milafrerichs <mila.frerichs@gmail.com>
2020-07-29 11:35:35 +0200

    chore: update styling for mobile and add active state
    update the styles for the sidebar for mobile displays, add styles
    according to figma and PR discussion
    add active state to LinkWithIcon

--- packages/neoFrontend/src/pages/ProfilePage/styles.js
+++ packages/neoFrontend/src/pages/ProfilePage/styles.js
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
+import { isMobile } from '../../mobileMods';
 
 export const SidebarWrapper = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: row;
+  flex-direction: ${isMobile ? 'column' : 'row'};
 `;
 
 export const Main = styled.div`
   display: flex;
-  width: 85%;
+  flex: 1;
 `;
 
 export const Sidebar = styled.div`
   display: flex;
   flex-direction: column;
-  width: 15%;
+  width: ${isMobile ? '100%' : '220px'};
+  ${isMobile ? 'flex: 1;' : ''}
+  align-items: ${isMobile ? 'center' : 'normal'};
+  margin-right: ${isMobile ? '0px' : '100px'};
+  margin-top: ${isMobile ? '10px' : '0'};
+  padding: ${isMobile ? '0' : '0 6px'};
 `;
 
 
