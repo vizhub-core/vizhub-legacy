9a64eef4dd13a66edc236b3e14ed15a418b6c6ee
Curran <curran.kelleher@gmail.com>
2020-07-30 18:03:22 -0400

    Tweak font

--- packages/neoFrontend/src/theme/text.js
+++ packages/neoFrontend/src/theme/text.js
@@ -2,11 +2,9 @@
 // (font-size, font-weight) combinations that are used
 // exclusively across the app.
 export const text = {
-  huge: 'font-size: 32px;',
-  large: 'font-size: 20px;',
-
-  // font-size: 16px; is the default, so no entry here for it.
-  medium: 'font-size: 14px;',
-  stubby: 'font-size: 14px; font-weight: bold;',
-  small: 'font-size: 12px;',
+  huge: '32px',
+  large: '20px',
+  medium: '16px',
+  small: '14px',
+  tiny: '12px',
 };
