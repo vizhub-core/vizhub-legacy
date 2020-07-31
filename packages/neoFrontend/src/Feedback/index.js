c84266a4165c745b085f511edc2f54de9885cbf7
Curran <curran.kelleher@gmail.com>
2020-07-31 16:34:05 -0400

    Iterate feedback button style

--- packages/neoFrontend/src/Feedback/index.js
+++ packages/neoFrontend/src/Feedback/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Wrapper, FeedbackButton } from './styles';
 
 export const Feedback = () => (
-  <a
-    href="https://github.com/datavis-tech/vizhub-issue-tracker/issues/new"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Wrapper>
+  <Wrapper>
+    <a
+      href="https://github.com/datavis-tech/vizhub-issue-tracker/issues/new"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <FeedbackButton>Feedback</FeedbackButton>
-    </Wrapper>
-  </a>
+    </a>
+  </Wrapper>
 );
