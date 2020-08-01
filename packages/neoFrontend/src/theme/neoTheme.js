fe4ce9d3920cf6966d81741735ec43345c572b0a
Curran <curran.kelleher@gmail.com>
2020-08-01 17:48:32 -0400

    Tweak neonav styles for viz page

--- packages/neoFrontend/src/theme/neoTheme.js
+++ packages/neoFrontend/src/theme/neoTheme.js
@@ -9,15 +9,17 @@ export const theme = {
   navbarHeight: 32,
   avatarHeight: 40,
   navLinkFontSize: defaultTheme.text.medium,
+  navLinkGapSize: 30,
 };
 
 export const darkNavbarTheme = {
   ...theme,
   bannerHeight: theme.bannerHeight / 2,
   bannerHeightMobile: 40,
-  bannerPadding: 20,
+  bannerPadding: 10,
   bannerMarginBottom: 0,
   navbarHeight: 20,
   avatarHeight: 20,
   navLinkFontSize: defaultTheme.text.small,
+  navLinkGapSize: 20,
 };
