11f498ed09940e5ed2a4b8bb0323b48573b6e81a
kalpesh05 <kalpeswhbhad57@gail.com>
2020-08-17 00:41:39 +0530

    manage manual  google auth[as per our recent scenario] and setup Fb auth(remains to test)

---
+++ packages/controllers/src/jwtAuth/getFBAccessToken.js
@@ -0,0 +1,38 @@
+import fetch from "node-fetch";
+import { VizHubAPIError } from "vizhub-entities";
+
+// Documentation: https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow/
+
+// need made key and seceret dynamic once tested
+
+const oAuthAccessTokenURL = `https://graph.facebook.com/v8.0/oauth/access_token?client_id=392704021076365&redirect_uri=http://localhost:3000/authenticated&client_secret=13aedfb953666b2a425e5ba00deef3c1&code=`;
+// const client_id = process.env.REACT_APP_VIZHUB_GITHUB_CLIENT_ID;
+// const client_secret = process.env.VIZHUB_GITHUB_CLIENT_SECRET;
+
+// Get an access token from FB's API.
+export const getFBAccessToken = async code => {
+  const fetchOptions = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  };
+
+  const response = await fetch(`${oAuthAccessTokenURL}${code}`, fetchOptions);
+  const { error, access_token, type } = await response.json();
+  if (error) {
+    if (error === "Not Found") {
+      console.log(
+        "Check that you have your GitHub OAuth environment variables set"
+      );
+    }
+    throw new VizHubAPIError({
+      error: error.message,
+      errorDescription: error.type,
+      errorCode: data.error_subcode
+    });
+  }
+
+  return access_token;
+};
--- packages/neoFrontend/src/authentication/index.js
+++ packages/neoFrontend/src/authentication/index.js
@@ -3,7 +3,9 @@ export {
   AUTH_PENDING,
   GITHUB_OAUTH_URL,
   CI_AUTH_PATH,
-  CI_USER_CODE
+  CI_USER_CODE,
+  GOOGLE_OAUTH_URL,
+  FACEBOOK_OAUTH_URL
 } from "./constants";
 export { getJWT } from "./getJWT";
 export { getJWTForGoogle } from "./getJWTForGoogle";
