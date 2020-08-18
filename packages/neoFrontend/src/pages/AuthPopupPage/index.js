0043e7f6bdf9a4f409c740f01b9d558dd8201dfe
kalpesh05 <kalpeswhbhad57@gail.com>
2020-08-18 22:20:30 +0530

    prettier run for formating

--- packages/neoFrontend/src/pages/AuthPopupPage/index.js
+++ packages/neoFrontend/src/pages/AuthPopupPage/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useContext } from 'react';
-import queryString from 'query-string';
-import { ErrorContext } from '../../ErrorContext';
+import React, { useEffect, useContext } from "react";
+import queryString from "query-string";
+import { ErrorContext } from "../../ErrorContext";
 import {
   getJWT,
   getJWTForGoogle,
-  postMessageToOpener,
-} from '../../authentication';
-import { LoadingScreen } from '../../LoadingScreen';
+  postMessageToOpener
+} from "../../authentication";
+import { LoadingScreen } from "../../LoadingScreen";
 
 // This page will open within the authentication popup,
 // triggered by the OAuth callback URL, which should be set to
@@ -17,7 +17,6 @@ import { LoadingScreen } from '../../LoadingScreen';
 // `${serverURL}/authenticated?code=fff33d3da4333abdr4fe`.
 //
 export const AuthPopupPage = () => {
-  // console.log();
   // Get the code passed from OAuth out of the URL.
   const { code } = queryString.parse(window.location.search);
   const { id_token } = queryString.parse(window.location.hash);
@@ -26,26 +25,25 @@ export const AuthPopupPage = () => {
 
   // Get the JWT token from backend API.
   useEffect(() => {
-    if (code) {
-      getJWT(code).then((data) => {
-        if (data.error) {
-          setError(new Error(data.errorDescription));
-        } else {
-          postMessageToOpener(data);
-        }
-      });
-    }
-    if (id_token) {
-      getJWTForGoogle(id_token).then((data) => {
-        // console.log(data);
-        if (data.error) {
-          setError(new Error(data.errorDescription));
-        } else {
-          postMessageToOpener(data);
-        }
-      });
-    }
-  }, [code, id_token, setError]);
+    getJWT(code).then(data => {
+      if (data.error) {
+        setError(new Error(data.errorDescription));
+      } else {
+        postMessageToOpener(data);
+      }
+    });
+  }, [code, setError]);
+
+  // Get the JWT token from backend API from google id_tokne.
+  useEffect(() => {
+    getJWTForGoogle(id_token).then(data => {
+      if (data.error) {
+        setError(new Error(data.errorDescription));
+      } else {
+        postMessageToOpener(data);
+      }
+    });
+  }, [id_token, setError]);
 
   return <LoadingScreen message="Signing in..." />;
 };
