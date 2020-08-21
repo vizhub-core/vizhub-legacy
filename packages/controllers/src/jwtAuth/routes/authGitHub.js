20526fb530401d5eeef195ab92d9aba9aa8d55cb
kalpesh05 <kalpeswhbhad57@gail.com>
2020-08-21 21:27:17 +0530

    working on suggested changes

---
+++ packages/controllers/src/jwtAuth/routes/authGitHub.js
@@ -0,0 +1,18 @@
+import { getAccessToken } from '../getAccessToken';
+import { getGitHubUser } from '../getGitHubUser';
+import { createAuthController } from './createAuthController';
+
+const getUserFromRequest = async (req) => {
+  const accessToken = await getAccessToken(req.body.code);
+  const gitHubUser = await getGitHubUser(accessToken);
+
+  const user = {
+    id: '' + gitHubUser.id,
+    username: gitHubUser.login,
+    _json: gitHubUser,
+  };
+
+  return user;
+};
+
+export const authGitHub = createAuthController(getUserFromRequest);
