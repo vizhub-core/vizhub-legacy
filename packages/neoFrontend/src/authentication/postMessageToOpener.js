6471847b230cfcc4db419148905b5906f37b2c5f
Curran Kelleher <curran.kelleher@gmail.com>
2020-08-22 10:23:35 -0400

    Update packages/neoFrontend/src/authentication/postMessageToOpener.js

--- packages/neoFrontend/src/authentication/postMessageToOpener.js
+++ packages/neoFrontend/src/authentication/postMessageToOpener.js
@@ -1,5 +1,4 @@
 export const postMessageToOpener = (vizHubAuthenticatedUser) => {
-  console.log(window.opener.location);
   window.opener.postMessage(
     { vizHubAuthenticatedUser },
     window.opener.location
