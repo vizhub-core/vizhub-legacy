0481bbf508cc798e330767b05d90e2ca0dec4a9c
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-05 08:20:48 -0400

    Rough sketch of Commit type

--- packages/commitGraph/lib/index.js
+++ packages/commitGraph/lib/index.js
@@ -1,3 +1,6 @@
+// A unique itentifier for a commit.
+// A unique identifier for a Viz.
+// TODO flesh this out.
 export function square(n) {
   return n * n;
 }
--- packages/commitGraph/src/index.js
+++ packages/commitGraph/src/index.js
@@ -1,4 +1,21 @@
 // @flow
+
+// A unique itentifier for a commit.
+type CommitId = string;
+
+// A unique identifier for a Viz.
+type VizId = string;
+
+// TODO flesh this out.
+type Ops = any;
+
+export type Commit = {
+  id: CommitId,
+  parentId: CommitId,
+  ops: Ops,
+  viz: VizId
+}
+
 export function square(n: number): number {
   return n * n;
 }
