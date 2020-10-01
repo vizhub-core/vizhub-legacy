a92316cd7d292481178b22fca916ae3f06127d04
shurik <stu.shurik@gmail.com>
2020-10-01 22:21:57 +0300

    Clean up duplicated logic around generalized CSS utils
    Closes datavis-tech/vizhub-issue-tracker/issues/483

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/CodeMirrorGlobalStyle.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/CodeMirrorGlobalStyle.js
@@ -1,18 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import { objectToCSS } from '../../../../../../../utils/css';
 import { CodeMirrorCSS } from './CodeMirrorCSS';
 import { CodeMirrorDialogCSS } from './CodeMirrorDialogCSS';
 import { CodeMirrorHintCSS } from './CodeMirrorHintCSS';
 import { CodeMirrorLintCSS } from './CodeMirrorLintCSS';
-
-// https://stackoverflow.com/questions/47836390/how-to-convert-a-camel-case-string-to-dashes-in-javascript
-const dashed = (camel) => camel.replace(/[A-Z]/g, (m) => '-' + m.toLowerCase());
-
-const objectToCSS = (object) =>
-  object
-    ? Object.entries(object)
-        .map(([key, value]) => `${dashed(key)}:${value};`)
-        .join('')
-    : '';
 
 const keys = {};
 
