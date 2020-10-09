24756eaad1c3827dea7086435e4bcfaf00bf1725
shurik <stu.shurik@gmail.com>
2020-10-09 22:22:19 +0300

    Show saved indicator only for local changes
    Closes datavis-tech/vizhub-issue-tracker/issues/505

---
+++ packages/neoFrontend/src/vizRealTimeHooks/useOpSourceTracker.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export const useOpSourceTracker = (shareDBDoc) => {
+  const [isEditedLocally, setIsEditedLocally] = useState(false);
+
+  useEffect(() => {
+    if (!shareDBDoc) {
+      return;
+    }
+    const listener = (_, source) => {
+      if(source) {
+        setIsEditedLocally(true);
+        shareDBDoc.removeListener('op', listener);
+      }
+    };
+
+    shareDBDoc.on('op', listener);
+
+    return () => {
+      shareDBDoc.removeListener('op', listener);
+    };
+  }, [shareDBDoc]);
+
+  return isEditedLocally;
+};
