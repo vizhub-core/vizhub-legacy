4fb768d9a608c6f627e1e3be9ec4b612f8f5c2ca
Curran Kelleher <curran.kelleher@gmail.com>
2020-11-06 08:06:27 -0500

    Correct usage of meta tags using content not value attribute

--- packages/controllers/src/servePage.js
+++ packages/controllers/src/servePage.js
@@ -5,19 +5,19 @@ const unfurlPlaceholder = '<meta name="unfurl-all-that:shit" value="please"/>';
 const absolute = (relative) => 'https://vizhub.com' + relative;
 
 const generateUnfurlHTML = ({ title, descriptionPlainText, image, url }) => `
-  <meta name="description" content="${descriptionPlainText}" />
-  <meta name="twitter:url" value="${url}" />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" value="@datavis_tech" />
-  <meta name="twitter:title" value="${title}" />
-  <meta name="twitter:description" value="${descriptionPlainText}" />
-  <meta name="twitter:image" content="${image}" />
-  <meta name="twitter:domain" value="vizhub.com" />
+  <meta name="description" content="${descriptionPlainText}"/>
+  <meta name="twitter:url" content="${url}"/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:site" content="@datavis_tech"/>
+  <meta name="twitter:title" content="${title}"/>
+  <meta name="twitter:description" content="${descriptionPlainText}"/>
+  <meta name="twitter:image" content="${image}"/>
+  <meta name="twitter:domain" content="vizhub.com"/>
   <meta property="og:url" content="${url}"/>
-  <meta property="og:title" content="${title}" />
-  <meta property="og:description" content="${descriptionPlainText}" />
-  <meta property="og:image" content="${image}" />
-  <meta property="og:site_name" content="VizHub" />
+  <meta property="og:title" content="${title}"/>
+  <meta property="og:description" content="${descriptionPlainText}"/>
+  <meta property="og:image" content="${image}"/>
+  <meta property="og:site_name" content="VizHub"/>
 `;
 
 export const servePage = (indexHTML, { title, description, image, url }) => {
