20285e06bb2d476f0cd1dc665e478098dba6756d
shurik <stu.shurik@gmail.com>
2020-11-21 21:17:30 +0200

    implement oembed

--- packages/controllers/src/index.js
+++ packages/controllers/src/index.js
@@ -3,3 +3,4 @@ export { userController } from './userController';
 export { jwtAuth, getUserIDFromJWT } from './jwtAuth';
 export { serveVizPage } from './serveVizPage';
 export { serveHomePage } from './serveHomePage';
+export { oembedController } from './oembedController';
