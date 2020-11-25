26fce31828e34bf04decb1fb429772b3c8e0ee47
Curran <curran.kelleher@gmail.com>
2020-11-25 17:53:35 -0500

    Add test for edge creation

--- packages/useCases/src/interactors/index.js
+++ packages/useCases/src/interactors/index.js
@@ -25,3 +25,4 @@ export { GetSearchResultsPageData } from './getSearchResultsPageData';
 export { GetUserSearchResultsData } from './getUserSearchResultsData';
 export { GetForksPageData } from './getForksPageData';
 export { CreateCommit } from './createCommit';
+export { CreateEdge } from './createEdge';
--- packages/useCases/test/revisionHistory.js
+++ packages/useCases/test/revisionHistory.js
@@ -1,18 +1,27 @@
 import assert from 'assert';
-import { timestamp, Commit } from 'vizhub-entities';
-import { CreateCommit } from '../src/index';
+import { timestamp, Commit, Edge } from 'vizhub-entities';
+import { CreateCommit, CreateEdge } from '../src/index';
 
 const commits = {};
+const edges = {};
 const revisionHistoryGateway = {
   createCommit: async ({ id, viz, timestamp }) => {
     const commit = new Commit({ id, viz, timestamp });
     commits[id] = commit;
     return commit;
   },
+  createEdge: async ({ source, target, ops }) => {
+    const edge = new Edge({ source, target, ops });
+    const id = source + '|' + target;
+    edges[id] = edge;
+    return edge;
+  },
 };
 
 describe('Revision History Use Cases', () => {
   const createCommit = new CreateCommit({ revisionHistoryGateway });
+  const createEdge = new CreateEdge({ revisionHistoryGateway });
+
   describe('Create Commit', () => {
     it('should return an id if success.', async () => {
       const id = 'commit-1';
@@ -24,9 +33,24 @@ describe('Revision History Use Cases', () => {
   });
   describe('Create Edge', () => {
     it('should return an id if success.', async () => {
-      const viz = 'viz-a';
-      const commit1 = await createCommit.execute({ id: 'commit-1', viz });
-      const commit2 = await createCommit.execute({ id: 'commit-2', viz });
+      // This is the case where a viz is forked - no ops, different viz IDs.
+      const commit1 = await createCommit.execute({
+        id: 'commit-1',
+        viz: 'viz-a',
+      });
+      const commit2 = await createCommit.execute({
+        id: 'commit-2',
+        viz: 'viz-b',
+      });
+      const ops = [];
+      const edge = await createEdge.execute({
+        source: commit1.id,
+        target: commit2.id,
+        ops,
+      });
+      assert.equal(edge.source, commit1.id);
+      assert.equal(edge.target, commit2.id);
+      assert.equal(edge.ops, ops);
     });
   });
 });
