3e839b0a1018e213aad52c2a3f7a468e879cf796
Curran <curran.kelleher@gmail.com>
2020-11-25 13:05:40 -0500

    Prettier on tests

--- packages/entities/test/commit.js
+++ packages/entities/test/commit.js
@@ -1,16 +1,12 @@
 import * as assert from 'assert';
-import {
-  Commit
-} from '../src/index';
+import { Commit } from '../src/index';
 
 describe('Commits', () => {
-
   describe('Constructor', () => {
     it('should expose expected fields', () => {
-const data = {viz:'a'};
+      const data = { viz: 'a' };
       const commit = new Commit(data);
       assert.deepEqual(data.viz, commit.viz);
     });
   });
-
 });
