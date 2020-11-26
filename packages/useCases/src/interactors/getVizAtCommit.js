381592e9c530c6e70263a2e4a7d47bb191e7d6b8
Curran <curran.kelleher@gmail.com>
2020-11-25 19:16:38 -0500

    First pass algorithm for getting viz at commit.

--- packages/useCases/src/interactors/getVizAtCommit.js
+++ packages/useCases/src/interactors/getVizAtCommit.js
@@ -1,3 +1,5 @@
+import { type as json0 } from 'ot-json0';
+
 export class GetVizAtCommit {
   constructor({ revisionHistoryGateway }) {
     this.revisionHistoryGateway = revisionHistoryGateway;
@@ -11,22 +13,39 @@ export class GetVizAtCommit {
     // Phase I Algorithm:
     //  - Trace back all the way to the root commit, apply all ops.
     //
-    // let previousCommit = commit;
-    // const edges = [];
-    // while(!previousCommit.isRoot){
-    //   const edge = getEdgesByTarget(previousCommit)[0];
-    //   edges.push(edge);
-    //   previousCommit = edge.source;
-    // }
-    // const viz = edges.reverse().reduce((accumulator, edge) => json0.apply(accumulator, edge.ops), {})
-    // return viz;
+    let previousCommit = commit;
+    const edges = [];
+    while (true) {
+      const incomingEdges = await this.revisionHistoryGateway.getEdgesByTarget(
+        previousCommit
+      );
 
+      // Break if we've reached the root commit.
+      // We know we've hit the root when it has no incoming edges.
+      if (incomingEdges.length === 0) {
+        break;
+      }
+
+      // If there are multiple incoming edges,
+      // for example a commit that's the result of merging a pull request,
+      // pick one of them arbitrarily to follow (order does not matter).
+      const edge = incomingEdges[0];
+
+      edges.push(edge);
+      previousCommit = edge.source;
+    }
+
+    // Derive the viz at this commit by applying all ops leading to it.
+    const viz = edges
+      .reverse()
+      .reduce((accumulator, edge) => json0.apply(accumulator, edge.ops), {});
+
+    return viz;
 
     //
     // Phase II Algorithm:
     //  - Trace back until we find a cached snapshot, or the root commit.
     //  - Before returning, store the snapshot in snapshot cache.
     //
-
   }
 }
