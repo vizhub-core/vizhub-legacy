35ba3eccb25465373a486e1b984b8c779701d041
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-05 17:34:50 -0500

    Enable local development without MongoDB.

--- packages/database/src/eventRecords.js
+++ packages/database/src/eventRecords.js
@@ -1,26 +1,37 @@
 import { EVENT_RECORDS } from './collectionName';
 
 export const getEventRecords = (mongoDatabase) => {
-  const collection = mongoDatabase.collection(EVENT_RECORDS);
-  return async (eventIDs) => {
-    const mongoQuery = { id: { $in: eventIDs } };
-    const docs = await collection.find(mongoQuery).toArray();
-    return docs;
-  };
+  if (mongoDatabase) {
+    const collection = mongoDatabase.collection(EVENT_RECORDS);
+    return async (eventIDs) => {
+      const mongoQuery = { id: { $in: eventIDs } };
+      const docs = await collection.find(mongoQuery).toArray();
+      return docs;
+    };
+  } else {
+    // Do nothing in development environments with no MongoDB.
+    return async () => [];
+  }
 };
 
 export const setEventRecords = (mongoDatabase) => {
-  const collection = mongoDatabase.collection(EVENT_RECORDS);
-  return async (newEventRecords) => {
-    await collection.bulkWrite(
-      newEventRecords.map((newRecord) => ({
-        replaceOne: {
-          filter: { id: newRecord.id },
-          replacement: newRecord,
-          upsert: true,
-        },
-      }))
-    );
-    return 'success';
-  };
+  if (mongoDatabase) {
+    const collection = mongoDatabase.collection(EVENT_RECORDS);
+    return async (newEventRecords) => {
+      await collection.bulkWrite(
+        newEventRecords.map((newRecord) => ({
+          replaceOne: {
+            filter: { id: newRecord.id },
+            replacement: newRecord,
+            upsert: true,
+          },
+        }))
+      );
+      return 'success';
+    };
+  } else {
+    // Do nothing in development environments with no MongoDB.
+    return async () =>
+      'success (no-op during development, no MongoDB connection)';
+  }
 };
--- packages/neoFrontend/src/sendEvent.js
+++ packages/neoFrontend/src/sendEvent.js
@@ -1,12 +1,7 @@
-export const sendEvent = async (eventIDs) => {
-  console.log('sending event');
-  console.log(eventIDs);
-  const response = await fetch('/api/event/send', {
+export const sendEvent = (eventIDs) => {
+  fetch('/api/event/send', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ eventIDs }),
   });
-  console.log(response);
 };
