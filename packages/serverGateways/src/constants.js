5f5cb8bcfcfbff153f4d83fa4b1e236b9ef607aa
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-05 17:35:22 -0500

    Fix env var

--- packages/serverGateways/src/constants.js
+++ packages/serverGateways/src/constants.js
@@ -1,4 +1,4 @@
 // Environment variables.
-export const mongoURI = process.env.VIZdHUB_MONGO_URI;
+export const mongoURI = process.env.VIZHUB_MONGO_URI;
 export const mongoDatabaseName = 'vizhub';
 export const redisHost = process.env.VIZHUB_REDIS_HOST;
