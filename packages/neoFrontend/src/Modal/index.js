50a5e3e1d306fb3f7b8ee0166d35091297738dde
Oleksandr Poplavskyi <oleksandrp@seekingalpha.com>
2020-12-13 19:26:29 +0200

    fix undesired modal close on text selection
    Closes datavis-tech/vizhub-issue-tracker/issues/592

--- packages/neoFrontend/src/Modal/index.js
+++ packages/neoFrontend/src/Modal/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { CloseSVG } from '../svg';
-import { Wrapper, Box, CloseIcon, Message } from './styles';
-
-const stopPropagation = (event) => event.stopPropagation();
+import { FullscreenOverlay, Box, CloseIcon, Message } from './styles';
 
 export const Modal = ({ children, onClose, closeButtonClassName }) => {
+  const handleClickOutsideBox = useCallback((event) => {
+    if(event.currentTarget === event.target) onClose();
+  }, [onClose])
   return (
-    <Wrapper onClick={onClose} onExit={onClose}>
-      <Box onClick={stopPropagation}>
+    // onClick can't be used.
+    // after text selection that finished outside Box component happens 
+    // click event is triggered in FullscreenOverlay and it can't be canceled
+    <FullscreenOverlay onMouseDown={handleClickOutsideBox}>
+      <Box>
         <CloseIcon
           onClick={onClose}
           rightmost={true}
@@ -18,7 +22,7 @@ export const Modal = ({ children, onClose, closeButtonClassName }) => {
         </CloseIcon>
         {children}
       </Box>
-    </Wrapper>
+    </FullscreenOverlay>
   );
 };
 
