f16ea8ab49ebdbee8f5dde224fb286423153f9fa
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-18 08:34:17 -0500

    Track views of terms page

--- packages/neoFrontend/src/pages/TermsPage/index.js
+++ packages/neoFrontend/src/pages/TermsPage/index.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { sendEvent } from '../../sendEvent';
 import marked from 'marked';
 import { NavBar } from '../../NavBar';
 import { Wrapper, Content, Copy, CopyWrapper } from '../styles';
 
 export const TermsPage = () => {
   const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    sendEvent('event.pageview.terms');
+  }, []);
 
   useEffect(() => {
     const fetchMarkdown = async () => {
