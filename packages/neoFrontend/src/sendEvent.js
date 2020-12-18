aaf3bd7514472a4794ffff1fcef66915296e6783
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-18 08:18:30 -0500

    Add convenience transformation for passing a single string to sendEvent

--- packages/neoFrontend/src/sendEvent.js
+++ packages/neoFrontend/src/sendEvent.js
@@ -1,6 +1,12 @@
 export const sendEvent = (eventIDs) => {
-  //console.log(typeof eventIDs);
-  //console.log('sendEvent: ', eventIDs);
+  if (typeof eventIDs === 'string') {
+    const values = eventIDs.split('.');
+    eventIDs = values.reduce((accumulator, value, i) => {
+      accumulator.push(`${values.slice(0, i + 1).join('.')}`);
+      return accumulator;
+    }, []);
+  }
+  // console.log('sendEvent: ' + JSON.stringify(eventIDs));
   fetch('/api/event/send', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
