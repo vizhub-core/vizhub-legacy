09df0d573cc2719c6b0fba4bbeb17f2c3a61a152
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-18 08:31:17 -0500

    Track views of search results page

--- packages/neoFrontend/src/pages/SearchResultsPage/index.js
+++ packages/neoFrontend/src/pages/SearchResultsPage/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LoadingScreen } from '../../LoadingScreen';
 import { Wrapper, Content, Centering } from '../styles';
-import { PageDataProvider } from './PageDataContext';
+import { sendEvent } from '../../sendEvent';
 import { NavBar } from '../../NavBar';
+import { PageDataProvider } from './PageDataContext';
 import { Vizzes } from './Vizzes';
 
-export const SearchResultsPage = () => (
-  <PageDataProvider fallback={<LoadingScreen />}>
-    <NavBar showSearch />
-    <Wrapper>
-      <Content>
-        <Centering>
-          <Vizzes />
-        </Centering>
-      </Content>
-    </Wrapper>
-  </PageDataProvider>
-);
+export const SearchResultsPage = () => {
+  useEffect(() => {
+    sendEvent('event.pageview.search');
+  }, []);
+
+  return (
+    <PageDataProvider fallback={<LoadingScreen />}>
+      <NavBar showSearch />
+      <Wrapper>
+        <Content>
+          <Centering>
+            <Vizzes />
+          </Centering>
+        </Content>
+      </Wrapper>
+    </PageDataProvider>
+  );
+};
