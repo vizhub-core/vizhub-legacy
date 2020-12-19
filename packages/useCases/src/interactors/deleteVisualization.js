05926784a357272f1c0c45e4cb43db064ae0c97f
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-19 07:56:29 -0500

    Track delete viz events

--- packages/useCases/src/interactors/deleteVisualization.js
+++ packages/useCases/src/interactors/deleteVisualization.js
@@ -1,11 +1,10 @@
 import { i18n } from 'vizhub-i18n';
-
-// Feature flag.
-const incrementForksCount = true;
+import { SendEvent } from './sendEvent';
 
 export class DeleteVisualization {
-  constructor({ visualizationGateway }) {
+  constructor({ visualizationGateway, eventRecordsGateway }) {
     this.visualizationGateway = visualizationGateway;
+    this.sendEvent = new SendEvent({ eventRecordsGateway });
   }
 
   async execute(requestModel) {
@@ -19,15 +18,21 @@ export class DeleteVisualization {
       id,
     });
 
-    if (visualization.info.owner !== userId) {
+    const vizOwner = visualization.info.owner;
+    if (vizOwner !== userId) {
       throw new Error(i18n('errorNotOwnerCantDelete'));
     }
 
-    if (incrementForksCount && visualization.info.forkedFrom != null) {
-      await this.visualizationGateway.decrementForksCount({
+    // No need to "await" this as we can return immediately.
+    if (visualization.info.forkedFrom != null) {
+      this.visualizationGateway.decrementForksCount({
         id: visualization.info.forkedFrom,
       });
     }
+
+    // No need to "await" this as we can return immediately.
+    const eventIDs = `event.interaction.viz.delete.owner:${vizOwner}`;
+    this.sendEvent.execute({ eventIDs });
 
     return await this.visualizationGateway.deleteVisualization({ id });
   }
