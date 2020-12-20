101dd4bb0fa83160d8b401523fd04e2febb9448f
Oleksandr Poplavskyi <stu.shurik@gmail.com>
2020-12-20 17:50:21 +0200

    add a vizhub section to vizzes` package.json
    - allow to override an imported package global name
    - allow to set path to umd a bundle of an imported package
    - take packages from the npm in a "exported" vizz installation

---
+++ packages/presenters/src/packageJson.js
@@ -0,0 +1,28 @@
+import { getText } from './accessors';
+
+const EMPTY_PKG_JSON = { dependencies: {}, vizhub: {} };
+export const packageJSON = (files) => {
+  const packageJsonText = getText(files, 'package.json');
+  try {
+    const pkg = packageJsonText
+      ? JSON.parse(packageJsonText)
+      : EMPTY_PKG_JSON;
+    return pkg;
+  } catch (error) {
+    console.log(error);
+    return EMPTY_PKG_JSON;
+  }
+};
+
+export const dependencies = files => packageJSON(files).dependencies;
+
+export const vizhubLibraries = files => {
+  const vizhubConfig = packageJSON(files).vizhub;
+  return vizhubConfig ? vizhubConfig.libraries : {}
+}
+
+export const dependencySource = ({name, version}, libraries) => {
+  const path = libraries[pkg] ? libraries[pkg].path || '' : '';
+  // unpkg uses file from unpkg or main field when no file specifid in url
+  return `https://unpkg.com/${name}@${version}${path}`
+}
