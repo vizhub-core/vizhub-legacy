3b658c34d4459c5f935d82bc69718452c1b15e97
Curran <curran.kelleher@gmail.com>
2020-12-21 18:09:04 -0500

    Refactor stats page

--- packages/neoFrontend/src/pages/VizHubStatsPage/RecordViz/index.js
+++ packages/neoFrontend/src/pages/VizHubStatsPage/RecordViz/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { utcDay } from 'd3-time';
-import { utcFormat } from 'd3-time-format';
 import { scaleUtc, scaleLinear } from 'd3-scale';
 import { extent } from 'd3-array';
 import { area, curveStep } from 'd3-shape';
@@ -10,22 +8,26 @@ const width = 500;
 const height = 90;
 const maxEntries = 10;
 
-const Chart = ({ timeseries, interval, format, maxValue }) => {
+const Chart = ({ timeseries, maxValue, format, d3TimeInterval }) => {
   const now = new Date();
-  const endDate = interval.ceil(now);
-  const startDate = interval.offset(now, -maxEntries);
-  const dates = interval.range(startDate, endDate);
+  const endDate = d3TimeInterval.ceil(now);
+  const startDate = d3TimeInterval.offset(now, -maxEntries);
+  const dates = d3TimeInterval.range(startDate, endDate);
   const data = dates.map((date) => ({
     date,
     value: timeseries[format(date)] || 0,
   }));
+
   const xScale = scaleUtc(extent(dates), [0, width]);
+
   const yScale = scaleLinear([0, maxValue], [height, 0]);
+
   const areaGenerator = area()
     .x((d) => xScale(d.date))
     .y1((d) => yScale(d.value))
     .y0(yScale(0))
     .curve(curveStep);
+
   return (
     <SVG width={width} height={height}>
       <path d={areaGenerator(data)} />
@@ -33,17 +35,20 @@ const Chart = ({ timeseries, interval, format, maxValue }) => {
   );
 };
 
-export const RecordViz = ({ record, maxValue }) => {
+export const RecordViz = ({ record, maxValue, interval }) => {
+  const { format, recordKey, d3TimeInterval } = interval;
+  const timeseries = record[recordKey];
+
   return (
     <Wrapper>
       <div>
         {record.id} ({record.all.all})
       </div>
       <Chart
-        timeseries={record.days}
-        interval={utcDay}
-        format={utcFormat('%Y-%m-%d')}
+        timeseries={timeseries}
         maxValue={maxValue}
+        format={format}
+        d3TimeInterval={d3TimeInterval}
       />
     </Wrapper>
   );
