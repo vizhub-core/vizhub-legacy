f808d22415c95c524ba1506a5dc6b5a5cc3b8492
Curran <curran.kelleher@gmail.com>
2020-12-22 16:16:18 -0500

    Update bundle when .svelte or .json files change

--- packages/neoFrontend/src/pages/VizPage/RunContext/changesJS.js
+++
@@ -1,5 +0,0 @@
-import { createChangesChecker } from './createChangesChecker';
-
-// Returns true if the given op changes a .js file
-// that is not bundle.js.
-export const changesJS = createChangesChecker('.js');
