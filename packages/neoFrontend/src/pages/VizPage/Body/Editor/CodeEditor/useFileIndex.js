1347d3300c8d11ed4969d41fea31a5d23489c819
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-31 15:11:08 -0500

    Squash newly appearing linter warnings.

--- packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/useFileIndex.js
+++ packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/useFileIndex.js
@@ -1,6 +1,9 @@
-import { useCallback } from 'react';
+import { useMemo } from 'react';
 import { getVizFileIndex } from 'vizhub-presenters';
 import { useValue } from '../../../../../useValue';
 
 export const useFileIndex = (viz$, activeFile) =>
-  useValue(viz$, useCallback(getVizFileIndex(activeFile), [activeFile]));
+  useValue(
+    viz$,
+    useMemo(() => getVizFileIndex(activeFile), [activeFile])
+  );
