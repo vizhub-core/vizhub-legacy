e4e3a7b646a58603e21690d04518993cb4aa5620
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-31 09:21:22 -0500

    Add create viz button to home page

--- packages/neoFrontend/src/pages/HomePage/Sort/index.js
+++ packages/neoFrontend/src/pages/HomePage/Sort/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { VizzesSortForm } from '../../../VizzesGrid/VizzesSortForm';
+import { Button } from '../../../Button';
 import { Container, Header } from './styles';
 
 export const Sort = (props) => {
   return (
     <Container>
-      <Header>VizHub Community</Header>
+      <Header>
+        <Link to="/create-viz">
+          <Button>Create a Viz</Button>
+        </Link>
+      </Header>
       <VizzesSortForm {...props} />
     </Container>
   );
