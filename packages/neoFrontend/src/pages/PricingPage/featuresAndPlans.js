c254a76d59eb75e81fc9a74f9da207d7ce49b2f9
Oleksandr Poplavskyi <stu.shurik@gmail.com>
2021-01-23 12:23:44 +0200

    Develop new pricing page content
    closes datavis-tech/vizhub-issue-tracker/issues/706

--- packages/neoFrontend/src/pages/PricingPage/featuresAndPlans.js
+++ packages/neoFrontend/src/pages/PricingPage/featuresAndPlans.js
@@ -1,7 +1,10 @@
+import { showProPlan, showNeoPricing } from '../../featureFlags'
+
 export const FREE = 'free';
 export const BASIC = 'basic';
+export const PRO = 'pro';
 
-export const features = [
+const oldFeatures = [
   {
     title: 'Private Vizzes',
     description: 'Make your vizzes private to you and your collaborators only.',
@@ -33,6 +36,59 @@ export const features = [
   },
 ];
 
+const neoFeatures = [
+  {
+    title: 'Vizzes with HTML, CSS & JavaScript',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'ES6 Modules',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Export',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Package.json',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'React with JSX',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Real-Time Broadcast',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Embedding',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Snippet Embedding',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Real-Time Collaboration 2 editors',
+    plans: { [FREE]: true, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Private Vizzes',
+    plans: { [FREE]: false, [BASIC]: true, [PRO]: true },
+  },
+  {
+    title: 'Unlimited Collaborators',
+    plans: { [FREE]: false, [BASIC]: false, [PRO]: true },
+  },
+  {
+    title: 'Whitelabel Embedding',
+    plans: { [FREE]: false, [BASIC]: false, [PRO]: true },
+  },
+]
+
+export const features = showNeoPricing ? neoFeatures : oldFeatures;
+
 export const plans = [
   { id: FREE, label: 'Free' },
   {
@@ -40,4 +96,9 @@ export const plans = [
     label: 'Basic',
     subtext: ['$4 / month'],
   },
-];
+  showProPlan &&   {
+    id: PRO,
+    label: 'Pro',
+    subtext: ['$12 / month'],
+  },
+].filter(Boolean);
