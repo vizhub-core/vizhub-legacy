8f348908d026194477c07a9a07281d7696b132c9
Curran <curran.kelleher@gmail.com>
2021-06-23 11:58:41 -0400

    Iterate pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,9 @@
 module.exports = {
   apps: [
     {
+      name: 'VizHub v3 Beta',
       script: 'npm start',
       watch: '.',
     },
   ],
-
-  deploy: {
-    production: {
-      key: '/home/curran/Dropbox/nv.pem',
-      user: 'ubuntu',
-      host: 'beta.vizhub.com',
-      ref: 'origin/main',
-      repo: 'git@github.com:curran/vizhub-v3.git',
-      path: '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-    },
-  },
 };
