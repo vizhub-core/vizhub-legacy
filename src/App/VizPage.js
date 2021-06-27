9e22337e895f8f0c8811aad6b17a4eb27370273e
Curran <curran.kelleher@gmail.com>
2021-06-27 08:23:10 -0400

    Start on content styling

--- src/App/VizPage.js
+++ src/App/VizPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Navigation } from './Navigation';
+
+const Content = ({ children }) => <div className="content">{children}</div>;
 
 export const VizPage = ({ title, sanitizedDescriptionHTML }) => {
   return (
     <>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedDescriptionHTML }}></div>
+      <Navigation />
+      <Content>
+        <h1>{title}</h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: sanitizedDescriptionHTML }}
+        ></div>
+      </Content>
     </>
   );
 };
