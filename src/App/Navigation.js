a2547eea51c1688532db0998bcc5fefa68b47ea4
Curran <curran.kelleher@gmail.com>
2021-06-27 08:13:31 -0400

    Implement dark theme concept

--- src/App/Navigation.js
+++ src/App/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { LogoSVG } from '../svg/LogoSVG';
 
 export const Navigation = () => (
-  <div className="navigation">
+  <div className="navigation" data-theme="dark">
     <LogoSVG />
   </div>
 );
