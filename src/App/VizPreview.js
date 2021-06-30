9784fd71f75c591713e52f2382920b84b7ce25bd
Curran <curran.kelleher@gmail.com>
2021-06-30 06:53:29 -0400

    Iterate viz preview

--- src/App/VizPreview.js
+++ src/App/VizPreview.js
@@ -6,10 +6,16 @@ const ContentContainer = classed('content-container');
 const Thumbnail = classed('thumbnail');
 const LastUpdatedDate = classed('last-updated-date');
 const Title = classed('title');
+const MetaContainer = classed('meta-container');
+const AuthorAvatarImage = classed('author-avatar-image');
 
 const urlBase = 'https://vizhub.com';
 const thumbnailURL = (id) =>
   `url(${urlBase}/api/visualization/thumbnail/${id}.png)`;
+
+// Use 's=180' because that's what GitHub uses all over the place
+// for small avatars, so they are more likely to be cached.
+const avatarUrl = (user) => user.avatarUrl + '&s=180';
 
 export const VizPreview = ({ vizInfo, ownerUser }) => {
   const { id, title } = vizInfo;
@@ -27,6 +33,15 @@ export const VizPreview = ({ vizInfo, ownerUser }) => {
         <LastUpdatedDate>January 10, 2020</LastUpdatedDate>
         <Title>{title}</Title>
       </ContentContainer>
+      <MetaContainer>
+        {ownerUser ? (
+          <>
+            {' '}
+            <AuthorAvatarImage src={avatarUrl(ownerUser)} />{' '}
+            {ownerUser.userName}
+          </>
+        ) : null}
+      </MetaContainer>
     </Wrapper>
   );
 };
