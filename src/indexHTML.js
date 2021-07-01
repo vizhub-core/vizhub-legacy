66175ac38ced400054d6667330d9a309a4d22b8f
Curran <curran.kelleher@gmail.com>
2021-07-01 12:49:52 -0400

    Iterate meta tags

--- src/indexHTML.js
+++ src/indexHTML.js
@@ -20,8 +20,32 @@ const d3Require = '1.2.4';
 // TODO get oembed working
 // <link rel="alternate" type="application/json+oembed" href="https://vizhub.com/oembed?url=https://vizhub.com/" title="VizHub - data visualization platform"/>
 
+const defaultMeta = {
+  url: 'https://vizhub.com',
+  title: 'VizHub - data visualization platform',
+  description:
+    'Learn to code interactive graphics &amp; data visualization with Web technologies!',
+  image: 'https://vizhub.com/images/unfurl-logo.png',
+  siteName: 'VizHub',
+
+  // See https://ogp.me/#types
+  // TODO use "article" for vizzes
+  // TODO use "profile" for user profiles
+  type: 'website',
+};
+
+// TODO i18n, translations
+const locale = 'en-US';
+
 // TODO tweak meta information per page.
-export const indexHTML = ({ title, rootHTML, page, pageProps }) => `<html>
+export const indexHTML = ({
+  title,
+  rootHTML,
+  page,
+  pageProps,
+  meta = defaultMeta,
+}) => `<!DOCTYPE html>
+<html lang="${locale}">
   <head>
     <meta charset="utf-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -33,21 +57,14 @@ export const indexHTML = ({ title, rootHTML, page, pageProps }) => `<html>
     <link rel="manifest" href="manifest.json" crossorigin="use-credentials"/>
     <meta name="viewport" content="width=device-width">
     <link href="/styles.css" rel="stylesheet">
-
-    <meta name="description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
-    <meta name="twitter:url" content="https://vizhub.com/"/>
+    <meta property="og:url" content="${meta.url}"/>
+    <meta property="og:title" content="${meta.title}"/>
+    <meta property="og:description" content="${meta.description}"/>
+    <meta property="og:image" content="${meta.image}"/>
+    <meta property="og:site_name" content="${meta.siteName}"/>
+    <meta property="og:locale" content="${locale}" />
+    <meta property="og:type" content="${meta.type}"/>
     <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:site" content="@datavis_tech"/>
-    <meta name="twitter:title" content="VizHub - data visualization platform"/>
-    <meta name="twitter:description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
-    <meta name="twitter:image" content="https://vizhub.com/images/unfurl-logo.png"/>
-    <meta name="twitter:domain" content="vizhub.com"/>
-    <meta property="og:url" content="https://vizhub.com/"/>
-    <meta property="og:title" content="VizHub - data visualization platform"/>
-    <meta property="og:description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
-    <meta property="og:image" content="https://vizhub.com/images/unfurl-logo.png"/>
-    <meta property="og:site_name" content="VizHub"/>
-    <meta property="og:type" content="article"/>
   </head>
   <body>
     <div id="root">${rootHTML}</div>
