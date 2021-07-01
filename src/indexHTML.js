455f534c5fed0809c093000ab268694148b900ee
Curran <curran.kelleher@gmail.com>
2021-07-01 07:54:23 -0400

    Add meta tags

--- src/indexHTML.js
+++ src/indexHTML.js
@@ -16,17 +16,38 @@ const d3Require = '1.2.4';
 
 // TODO get favicon to work by moving built files into public/build
 // and including stuff in public/ in the git repo.
+
+// TODO get oembed working
+// <link rel="alternate" type="application/json+oembed" href="https://vizhub.com/oembed?url=https://vizhub.com/" title="VizHub - data visualization platform"/>
+
+// TODO tweak meta information per page.
 export const indexHTML = ({ title, rootHTML, page, pageProps }) => `<html>
   <head>
     <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
     <title>${title}</title>
     <link rel="shortcut icon" href="favicon.ico" />
     <link rel="manifest" href="manifest.json" crossorigin="use-credentials"/>
     <meta name="viewport" content="width=device-width">
     <link href="/styles.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+    <meta name="description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
+    <meta name="twitter:url" content="https://vizhub.com/"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@datavis_tech"/>
+    <meta name="twitter:title" content="VizHub - data visualization platform"/>
+    <meta name="twitter:description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
+    <meta name="twitter:image" content="https://vizhub.com/images/unfurl-logo.png"/>
+    <meta name="twitter:domain" content="vizhub.com"/>
+    <meta property="og:url" content="https://vizhub.com/"/>
+    <meta property="og:title" content="VizHub - data visualization platform"/>
+    <meta property="og:description" content="Learn to code interactive graphics &amp; data visualization with Web technologies!"/>
+    <meta property="og:image" content="https://vizhub.com/images/unfurl-logo.png"/>
+    <meta property="og:site_name" content="VizHub"/>
+    <meta property="og:type" content="article"/>
   </head>
   <body>
     <div id="root">${rootHTML}</div>
