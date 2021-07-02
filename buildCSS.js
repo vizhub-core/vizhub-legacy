b727ba06884ff25be28fc4f715644a9d17235c7c
Curran <curran.kelleher@gmail.com>
2021-07-02 07:31:12 -0400

    Reorganize build

--- buildCSS.js
+++ buildCSS.js
@@ -1,8 +1,9 @@
 const { renderSync } = require('sass');
 const { writeFileSync } = require('fs');
 
+// TODO minify for production build.
 console.log('Building CSS...');
 writeFileSync(
-  './public/styles.css',
+  './public/build/styles.css',
   renderSync({ file: 'src/styles/index.scss' }).css.toString()
 );
