2557d966d0afca2404b5a3f102f2159ee818eb06
Curran <curran.kelleher@gmail.com>
2021-07-03 14:28:31 -0400

    Change the service worker so it does nothing

--- src/client/serviceWorker.js
+++ src/client/serviceWorker.js
@@ -1,33 +1,40 @@
 // Inspired by https://github.com/mdn/pwa-examples/blob/master/js13kpwa/sw.js
 
-const cacheName = 'vizhub-v1';
-const contentToCache = ['/', '/build/client.js', '/build/styles.css'];
+// TODO add caching when it's clear why we need it.
+//const cacheName = 'vizhub-v1';
+//const contentToCache = ['/', '/build/client.js', '/build/styles.css'];
 
 // Installing Service Worker
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install');
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })()
-  );
+  // event.waitUntil(
+  //   (async () => {
+  //     console.log('[Service Worker] Caching all: app shell and content');
+  //     const cache = await caches.open(cacheName);
+  //     await cache.addAll(contentToCache);
+  //   })()
+  // );
 });
 
 // Fetching content using Service Worker
 // TODO experiment with serving viz content files here
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  const { url } = request;
+
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) return r;
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+      console.log(
+        `[Service Worker] Opportunity to fetch resource: ${e.request.url}`
+      );
+      //const r = await caches.match(e.request);
+      //console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      //if (r) return r;
+      //const response = await fetch(e.request);
+      //const cache = await caches.open(cacheName);
+      //console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      //cache.put(e.request, response.clone());
+      //return response;
     })()
   );
 });
