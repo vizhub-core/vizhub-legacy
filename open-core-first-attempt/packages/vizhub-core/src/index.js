752bf73ed8a7ae7726036bb9fb992cd1e8afa05d
Curran <curran.kelleher@gmail.com>
2021-07-09 09:21:20 -0400

    Got express app to start

--- open-core-first-attempt/packages/vizhub-core/src/index.js
+++ open-core-first-attempt/packages/vizhub-core/src/index.js
@@ -1,3 +1,12 @@
-export const server = () => {
-  console.log('TODO make the server');
+import express from 'express';
+
+export const server = (plugins) => {
+  const app = express();
+  const port = 8080;
+
+  console.log('TODO interface with plugins: ', plugins);
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 };
