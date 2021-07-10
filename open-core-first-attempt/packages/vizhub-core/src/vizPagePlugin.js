1a0e96f44d71021186f407d4b03ce83f6a6e8a58
Curran <curran.kelleher@gmail.com>
2021-07-10 07:13:23 -0400

    Refactor viz page plugin

--- open-core-first-attempt/packages/vizhub-core/src/vizPagePlugin.js
+++ open-core-first-attempt/packages/vizhub-core/src/vizPagePlugin.js
@@ -1,20 +1,42 @@
+const getSnapshot = (shareDBConnection, collectionName) => (id) =>
+  new Promise((resolve, reject) => {
+    // See https://github.com/share/sharedb/blob/master/examples/counter-json1/server.js
+    const shareDBDoc = shareDBConnection.get(collectionName, iid);
+    shareDBDoc.fetch((error) => {
+      if (error) {
+        return reject(error);
+      }
+      if (shareDBDoc.type === null) {
+        return resolve(null);
+      }
+      const { version, data, type } = shareDBDoc;
+      const snapshot = { v: version, data, type };
+      resolve(snapshot);
+    });
+  });
+
 export const vizPagePlugin = () => ({
   extendServer: (expressApp, shareDBConnection) => {
-    expressApp.get('/:userName/:vizId', async (req, res) => {
-      const { userName, vizId } = req.params;
+    const getVizInfoSnapshot = getSnapshot(shareDBConnection, 'documentInfo');
 
-      // See https://github.com/share/sharedb/blob/master/examples/counter-json1/server.js
-      const shareDBDoc = shareDBConnection.get('documentInfo', vizId);
-      shareDBDoc.fetch((error) => {
-        if (error) {
-          return res.send('TODO error page ' + error);
+    expressApp.get('/:userName/:vizId', async (req, res) => {
+      const { vizId } = req.params;
+
+      try {
+        const snapshot = await getVizInfoSnapshot(vizId);
+        if (snapshot === null) {
+          res.send('TODO 404 not found page. need to log in?');
         }
-        if (shareDBDoc.type === null) {
-          return res.send('TODO not found page');
-        }
-        const snapshot = { v: doc.version, data: doc.data, type: doc.type };
+
+        // TODO leverage ingestSnapshot in frontend.
+        // TODO SSR React
+        // TODO SSR React-Router
         res.send(JSON.stringify(snapshot));
-      });
+      } catch (error) {
+        // Should never happen, but if it does, surface the error clearly.
+        console.log(error);
+        res.send(error.toString?.());
+      }
     });
   },
 });
