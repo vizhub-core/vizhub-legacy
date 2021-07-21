45d182af20c75f00640f86a3535ffbc8ff22e55a
Curran <curran.kelleher@gmail.com>
2021-07-21 18:04:42 -0400

    Document entry points

--- open-core-first-attempt/packages/vizhub-core/server.js
+++ open-core-first-attempt/packages/vizhub-core/server.js
@@ -1,2 +1,5 @@
+// This file is here as a package entry point, to support
+// tree shaking for independent builds. For example:
+// import { ... } from 'vizhub-core/server';
 export { server } from './src/server';
 export { vizPageServerPlugin } from './src/plugins/vizPagePlugin/server';
