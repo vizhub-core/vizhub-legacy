a73a20dcd141af2738e0ab09b5743fd838a0c244
Curran <curran.kelleher@gmail.com>
2021-07-21 20:24:13 -0400

    Introduce isClient

--- open-core-first-attempt/packages/vizhub-core/src/client/index.js
+++ open-core-first-attempt/packages/vizhub-core/src/client/index.js
@@ -1,6 +1,9 @@
 import { App } from '../App';
 import { decodePageData } from '../pageData';
 import { getPages } from '../getPages';
+import { setIsClient } from '../isClient';
+
+setIsClient();
 
 export const client = (plugins) => {
   const pageData = decodePageData(window.pageData);
--- open-core-first-attempt/packages/vizhub-core/src/plugins/vizPagePlugin/VizPage.js
+++ open-core-first-attempt/packages/vizhub-core/src/plugins/vizPagePlugin/VizPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { isClient } from '../../isClient';
 
 export const VizPage = ({ vizInfoSnapshot }) => {
+  console.log('VizPage: isClient === ' + isClient);
   // TODO introduce context that instantiates a VizInfo
   // entity - static with no ShareDB if server rendered,
   // dynamic with ingestSnapshot if client rendered.
