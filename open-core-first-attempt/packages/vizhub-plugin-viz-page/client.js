a769a6243d0f8cd9e04b6995899b6907ae9c202b
Curran <curran.kelleher@gmail.com>
2021-07-31 15:56:56 -0400

    Make file names consistent with exports

--- open-core-first-attempt/packages/vizhub-plugin-viz-page/client.js
+++ open-core-first-attempt/packages/vizhub-plugin-viz-page/client.js
@@ -1,4 +1,4 @@
 // This file is here as a package entry point, to support
 // tree shaking for independent builds. For example:
 // import { ... } from 'vizhub-plugin-viz-page/client';
-export { vizPageClientPlugin } from './src/client';
+export { vizPageClientPlugin } from './src/vizPageClientPlugin';
