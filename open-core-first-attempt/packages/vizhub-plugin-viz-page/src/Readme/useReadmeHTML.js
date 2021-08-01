a2f53a38619bed81acc8ebb99bc82be68e4a09cd
Curran <curran.kelleher@gmail.com>
2021-08-01 12:58:01 -0400

    Only parse server rendered markdown once.

--- open-core-first-attempt/packages/vizhub-plugin-viz-page/src/Readme/useReadmeHTML.js
+++ open-core-first-attempt/packages/vizhub-plugin-viz-page/src/Readme/useReadmeHTML.js
@@ -10,12 +10,18 @@ const loadNodeModules = () => ({
 });
 const { marked, filterXSS } = isClient ? {} : loadNodeModules();
 
+// Parse the innerHTML _only once_ on module load,
+// not every time getInitialReadmeHTML is called.
+const serverRenderedMarkdown = isClient
+  ? document.getElementById('readme').innerHTML
+  : null;
+
 const getInitialReadmeHTML = (vizContent) =>
   isClient
     ? // If we're in the client,
       // grab the server-rendered HTML to use for initial hydration,
       // before the Web Worker that renders Markdown client side has loaded.
-      document.getElementById('readme').innerHTML
+      serverRenderedMarkdown
     : // If we're on the server,
       // render Markdown synchronously.
       renderREADME(getFileText(vizContent, 'README.md'), marked, filterXSS);
