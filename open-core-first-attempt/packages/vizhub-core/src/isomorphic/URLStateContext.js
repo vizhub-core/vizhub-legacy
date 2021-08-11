31abec13d6534e2f59b7b33d976c0d862d790f3a
Curran <curran.kelleher@gmail.com>
2021-08-11 17:34:06 -0400

    Craft correct query

--- open-core-first-attempt/packages/vizhub-core/src/isomorphic/URLStateContext.js
+++ open-core-first-attempt/packages/vizhub-core/src/isomorphic/URLStateContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useMemo, useCallback } from 'react';
 import { isClient } from './isClient';
 
 // Inspired by:
@@ -11,7 +11,7 @@ const urlStateConfig = {
   edit: {
     defaultValue: false,
     parse: (d) => true,
-    stringify: (d) => d,
+    stringify: (d) => (d ? '' : undefined),
   },
 };
 
@@ -34,33 +34,65 @@ const getQuery = () => {
 
 const initialQuery = isClient ? getQuery() : null;
 
-const useURLState = (urlStateConfig, urlStateReducer, ssrQuery) => {
+// Removes unnecessary equals symbols from stringified query.
+// From https://stackoverflow.com/questions/64316665/is-there-any-way-to-remove-equal-sign-at-the-only-key-parameter-on-the-query-str
+const removeEmptyEquals = (str) => str.replace(/=$|=(?=&)/g, '');
+
+const useURLState = (config, reducer, ssrQuery) => {
   const [query, setQuery] = useState(isClient ? initialQuery : ssrQuery);
 
-  const urlState = {};
-  for (const [key, { defaultValue, parse }] of Object.entries(urlStateConfig)) {
-    const value = query[key];
-    urlState[key] = value === undefined ? defaultValue : parse(value);
-  }
+  const urlState = useMemo(() => {
+    const state = {};
+    for (const [key, keyConfig] of Object.entries(config)) {
+      const { defaultValue, parse } = keyConfig;
+      const value = query[key];
+      state[key] = value === undefined ? defaultValue : parse(value);
+    }
+    return state;
+  }, [config, query]);
+
   console.log(urlState);
 
-  //  const urlDispatch = useCallback(
-  //    (action) => {
-  //history.pushState(null, null, "?"+queryParams.toString());
-  //..reducer(urlState, action)
-  //},
-  //    [urlState, reducer]
-  //  );
+  const urlDispatch = useCallback(
+    (action) => {
+      const newURLState = reducer(urlState, action);
+      const urlSearchParams = new URLSearchParams();
+      for (const [key, keyConfig] of Object.entries(config)) {
+        const { defaultValue, stringify } = keyConfig;
+        urlSearchParams.set(key, stringify(newURLState[key]));
+      }
+      history.pushState(
+        null,
+        null,
+        '?' + removeEmptyEquals(urlSearchParams.toString())
+      );
+      // TODO setQuery
+    },
+    [urlState, reducer]
+  );
 
-  //  return [urlState, dispatch];
-  return urlState;
+  return [urlState, urlDispatch];
 };
 
 export const URLStateContextProvider = ({ children, query }) => {
-  const urlState = useURLState(urlStateConfig, urlStateReducer, query);
+  const [urlState, urlDispatch] = useURLState(
+    urlStateConfig,
+    urlStateReducer,
+    query
+  );
+
+  const isEditorOpen = urlState.edit;
+  const setIsEditorOpen = useCallback(
+    (isEditorOpen) => {
+      urlDispatch({ type: 'setEdit', edit: isEditorOpen });
+    },
+    [urlDispatch]
+  );
+
+  const value = { isEditorOpen, setIsEditorOpen };
 
   return (
-    <URLStateContext.Provider value={urlState}>
+    <URLStateContext.Provider value={value}>
       {children}
     </URLStateContext.Provider>
   );
