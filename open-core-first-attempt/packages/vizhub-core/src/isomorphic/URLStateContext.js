a8ed1ba93847ce419cb6b1466c9a52f7c9df5ff8
Curran <curran.kelleher@gmail.com>
2021-08-11 16:56:55 -0400

    Work out initial URL state

--- open-core-first-attempt/packages/vizhub-core/src/isomorphic/URLStateContext.js
+++ open-core-first-attempt/packages/vizhub-core/src/isomorphic/URLStateContext.js
@@ -1,18 +1,19 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { isClient } from './isClient';
+
+// Inspired by:
+// https://github.com/stamen/pleth/blob/master/packages/examples/src/pleth/useURLState.js
+// https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
 
 export const URLStateContext = createContext();
 
 const urlStateConfig = {
   edit: {
     defaultValue: false,
-    parse: (d) => d !== null,
+    parse: (d) => true,
     stringify: (d) => d,
   },
 };
-
-// TODO move this state into the URL.
-// https://github.com/stamen/pleth/blob/master/packages/examples/src/pleth/useURLState.js
-// https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
 
 const urlStateReducer = (state, action) => {
   switch (action.type) {
@@ -23,25 +24,41 @@ const urlStateReducer = (state, action) => {
   }
 };
 
-const useURLState = (urlStateConfig, urlStateReducer, query) => {
-  const searchParams = query
-    ? // Handle server side rendering.
-      new Map(Object.entries(query))
-    : // Handle client side rendering.
-      new URL(window.location).searchParams;
+const getQuery = () => {
+  const query = {};
+  for (const [key, value] of new URL(window.location).searchParams) {
+    query[key] = value;
+  }
+  return query;
+};
+
+const initialQuery = isClient ? getQuery() : null;
+
+const useURLState = (urlStateConfig, urlStateReducer, ssrQuery) => {
+  const [query, setQuery] = useState(isClient ? initialQuery : ssrQuery);
 
   const urlState = {};
-  for (const [key, config] of Object.entries(urlStateConfig)) {
-    const { defaultValue, parse } = config;
-    const value = searchParams.get(key);
+  for (const [key, { defaultValue, parse }] of Object.entries(urlStateConfig)) {
+    const value = query[key];
     urlState[key] = value === undefined ? defaultValue : parse(value);
   }
   console.log(urlState);
+
+  //  const urlDispatch = useCallback(
+  //    (action) => {
+  //history.pushState(null, null, "?"+queryParams.toString());
+  //..reducer(urlState, action)
+  //},
+  //    [urlState, reducer]
+  //  );
+
+  //  return [urlState, dispatch];
   return urlState;
 };
 
 export const URLStateContextProvider = ({ children, query }) => {
   const urlState = useURLState(urlStateConfig, urlStateReducer, query);
+
   return (
     <URLStateContext.Provider value={urlState}>
       {children}
