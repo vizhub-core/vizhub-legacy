182607309139cf45292125c52b3a7d8a1f5e7f81
Curran <curran.kelleher@gmail.com>
2021-12-02 08:56:59 -0500

    Use Bootstrap Nav for file list

--- vizhub3/src/EditorSidebar.js
+++ vizhub3/src/EditorSidebar.js
@@ -1,16 +1,27 @@
 import { classed } from './classed';
+import { Nav } from 'react-bootstrap';
 
 const Wrapper = classed('editor-sidebar');
-const Items = classed('editor-items', 'ul');
-const Item = classed('editor-item clickable', 'li');
+const Items = classed('editor-items');
+const Item = classed('editor-item clickable');
 
 export const EditorSidebar = () => {
   return (
     <Wrapper>
-      <Items role="listbox">
-        <Item role="option">index.js</Item>
-        <Item role="option">styles.css</Item>
-      </Items>
+      <Nav defaultActiveKey="link-1" className="flex-column editor-items">
+        <Nav.Link eventKey="link-1" className="editor-item clickable">
+          Link
+        </Nav.Link>
+        <Nav.Link eventKey="link-2" className="editor-item clickable">
+          Link 2
+        </Nav.Link>
+      </Nav>
     </Wrapper>
+    //    <Wrapper>
+    //      <Items role="listbox">
+    //        <Item role="option" tabindex="0">index.js</Item>
+    //        <Item role="option" tabindex="0">styles.css</Item>
+    //      </Items>
+    //    </Wrapper>
   );
 };
