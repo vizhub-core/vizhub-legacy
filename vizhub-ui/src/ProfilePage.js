f47cdd30014bdbf6005763775f78cc4f6adb9b3a
Curran <curran.kelleher@gmail.com>
2021-12-07 14:23:46 -0500

    Implement profile design for user avatar.

--- vizhub-ui/src/ProfilePage.js
+++ vizhub-ui/src/ProfilePage.js
@@ -4,13 +4,25 @@ import { VizPreview } from './VizPreview';
 import { classed } from './classed';
 
 const Wrapper = classed('profile-page');
+const FullName = classed('profile-page-full-name');
+const UserName = classed('profile-page-user-name');
 const VizPreviewCollection = classed('viz-preview-collection');
 
 export const ProfilePage = () => (
   <Wrapper>
     <Navigation />
     <Container className="mt-3 mb-3">
-      <Image className="profile-avatar" src="https://github.com/mdo.png" roundedCircle />
+      <div className="d-flex mb-3">
+        <Image
+          className="profile-page-avatar me-3"
+          src="https://github.com/mdo.png"
+          roundedCircle
+        />
+        <div className="d-flex flex-column justify-content-center">
+          <FullName>Full Name</div>
+          <UserName>username</div>
+        </div>
+      </div>
       <VizPreviewCollection>
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((d) => (
           <VizPreview
