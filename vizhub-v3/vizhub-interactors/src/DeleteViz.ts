d5c2ffd2f107254f1d2977352bd87d8d2ee95446
Curran <curran.kelleher@gmail.com>
2022-01-11 00:26:26 -0500

    Implement DeleteViz

---
+++ vizhub-v3/vizhub-interactors/src/DeleteViz.ts
@@ -0,0 +1,21 @@
+import { VizId, VizInfo, VizContent, UserId, Timestamp } from 'vizhub-entities';
+import { Gateways } from './Gateways';
+
+export const DeleteViz = (gateways: Gateways) => {
+  const { getVizInfo, saveVizInfo, deleteVizInfo, deleteVizContent, getForks } =
+    gateways;
+
+  return async (vizId: VizId): Promise<void> => {
+    const vizInfo = await getVizInfo(vizId);
+    const { forkedFrom } = vizInfo;
+
+    // For each fork forked from the viz to be deleted,
+    // update its forkedFrom reference to the viz that the
+    // viz to be deleted was forked from (tree node removal).
+    const forks: Array<VizInfo> = await getForks(vizId);
+    const updateForkedFrom = (fork) => saveVizInfo({ ...fork, forkedFrom });
+    await Promise.all(forks.map(updateForkedFrom));
+
+    await Promise.all([deleteVizInfo(vizId), deleteVizContent(vizId)]);
+  };
+};
