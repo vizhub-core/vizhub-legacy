52e3e9805158236bb8150abaea64c321270d8fcb
Curran <curran.kelleher@gmail.com>
2023-12-21 14:14:40 -0500

    Add V3 transition announcement

--- vizhub-v2/packages/neoFrontend/src/pages/HomePage/Banner/index.js
+++ vizhub-v2/packages/neoFrontend/src/pages/HomePage/Banner/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { isMobile } from "../../../mobileMods";
+import styled from "styled-components";
 import {
   Wrapper,
   Gradient,
